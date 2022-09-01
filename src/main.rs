a18b999b645d5e3d095ff7dc9fcf21597fe0313d
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-09-02 00:42:38 +0200

    Refactor into struct

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,15 @@ use std::{
     io::{self, BufRead},
 };
 
+use reqwest::Client;
 use scraper::{ElementRef, Html, Selector};
 
-async fn fetch_document(url: &str) -> Result<Html, Box<dyn std::error::Error>> {
-    let resp = reqwest::get(url).await?.text().await?;
+struct TUCAN {
+    client: Client,
+}
 
-    Ok(Html::parse_document(&resp))
+fn s(selector: &str) -> Selector {
+    Selector::parse(selector).unwrap()
 }
 
 fn link_by_text<'a>(document: &'a Html, text: &str) -> &'a str {
@@ -21,161 +24,183 @@ fn link_by_text<'a>(document: &'a Html, text: &str) -> &'a str {
         .unwrap()
 }
 
-fn s(selector: &str) -> Selector {
-    Selector::parse(selector).unwrap()
-}
-
 fn element_by_selector<'a>(document: &'a Html, selector: &str) -> Option<ElementRef<'a>> {
     document.select(&s(selector)).next()
 }
 
-async fn handle_veranstaltung(document: &Html) {
-    let name = element_by_selector(&document, "h1").unwrap();
+impl TUCAN {
+    async fn fetch_document(&self, url: &str) -> Result<Html, Box<dyn std::error::Error>> {
+        let resp = self.client.get(url).send().await?.text().await?;
 
-    println!("Name: {}", name.inner_html().trim());
-}
+        Ok(Html::parse_document(&resp))
+    }
 
-#[async_recursion::async_recursion(?Send)]
-async fn traverse_module_list(document: &Html) -> Result<(), Box<dyn std::error::Error>> {
-    //println!("{}", document.root_element().html());
+    async fn handle_veranstaltung(&self, document: &Html) {
+        let name = element_by_selector(&document, "h1").unwrap();
 
-    let list = element_by_selector(&document, "#auditRegistration_list");
+        println!("Name: {}", name.inner_html().trim());
+    }
 
-    match list {
-        Some(list) => {
-            for child in list.select(&s("li")) {
-                println!("{}", child.value().attr("title").unwrap());
+    #[async_recursion::async_recursion(?Send)]
+    async fn traverse_module_list(
+        &self,
+        document: &Html,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        //println!("{}", document.root_element().html());
 
-                let child_url = child
-                    .select(&s(r#"a[href]"#))
+        let list = element_by_selector(&document, "#auditRegistration_list");
+
+        match list {
+            Some(list) => {
+                for child in list.select(&s("li")) {
+                    println!("{}", child.value().attr("title").unwrap());
+
+                    let child_url = child
+                        .select(&s(r#"a[href]"#))
+                        .next()
+                        .unwrap()
+                        .value()
+                        .attr("href")
+                        .unwrap();
+
+                    //println!("{}", child_url);
+
+                    let document = self
+                        .fetch_document(&format!("https://www.tucan.tu-darmstadt.de/{}", child_url))
+                        .await?;
+
+                    self.traverse_module_list(&document).await?;
+                }
+            }
+            None => {
+                for child in document
+                    .select(&s(r#"table[class="nb eventTable"]"#))
                     .next()
                     .unwrap()
-                    .value()
-                    .attr("href")
-                    .unwrap();
+                    .select(&s(r#"a[name="eventLink"]"#))
+                {
+                    println!("{}", child.inner_html());
 
-                //println!("{}", child_url);
+                    let child_url = child.value().attr("href").unwrap();
 
-                let document =
-                    fetch_document(&format!("https://www.tucan.tu-darmstadt.de/{}", child_url))
+                    //println!("{}", child_url);
+
+                    let document = self
+                        .fetch_document(&format!("https://www.tucan.tu-darmstadt.de/{}", child_url))
                         .await?;
 
-                traverse_module_list(&document).await?;
+                    self.handle_veranstaltung(&document).await;
+                }
             }
         }
-        None => {
-            for child in document
-                .select(&s(r#"table[class="nb eventTable"]"#))
-                .next()
-                .unwrap()
-                .select(&s(r#"a[name="eventLink"]"#))
-            {
-                println!("{}", child.inner_html());
-
-                let child_url = child.value().attr("href").unwrap();
-
-                //println!("{}", child_url);
-
-                let document =
-                    fetch_document(&format!("https://www.tucan.tu-darmstadt.de/{}", child_url))
-                        .await?;
-
-                handle_veranstaltung(&document).await;
-            }
-        }
+        Ok(())
     }
-    Ok(())
+
+    async fn start(&self) -> Result<(), Box<dyn std::error::Error>> {
+        let username = env::args().nth(1).unwrap();
+
+        let password = rpassword::prompt_password("TUCAN password: ").unwrap();
+
+        let params: [(&str, &str); 10] = [
+            ("usrname", &username),
+            ("pass", &password),
+            ("APPNAME", "CampusNet"),
+            ("PRGNAME", "LOGINCHECK"),
+            (
+                "ARGUMENTS",
+                "clino,usrname,pass,menuno,menu_type,browser,platform",
+            ),
+            ("clino", "000000000000001"),
+            ("menuno", "000344"),
+            ("menu_type", "classic"),
+            ("browser", ""),
+            ("platform", ""),
+        ];
+        let client = reqwest::Client::new();
+        let res_headers = client
+            .post("https://www.tucan.tu-darmstadt.de/scripts/mgrqispi.dll")
+            .form(&params)
+            .send()
+            .await?;
+
+        println!("{:?}", res_headers);
+
+        let res = res_headers.text().await?;
+
+        let document = Html::parse_document(&res);
+        println!("{}", document.root_element().html());
+
+        return Ok(());
+
+        let document = self
+            .fetch_document("https://www.tucan.tu-darmstadt.de/")
+            .await?;
+
+        let redirect_url = &element_by_selector(&document, r#"meta[http-equiv="refresh"]"#)
+            .unwrap()
+            .value()
+            .attr("content")
+            .unwrap()[7..];
+
+        let document = self
+            .fetch_document(&format!(
+                "https://www.tucan.tu-darmstadt.de/{}",
+                redirect_url
+            ))
+            .await?;
+
+        let redirect_url = element_by_selector(&document, r#"h2 a[href]"#)
+            .unwrap()
+            .value()
+            .attr("href")
+            .unwrap();
+
+        let document = self
+            .fetch_document(&format!(
+                "https://www.tucan.tu-darmstadt.de/{}",
+                redirect_url
+            ))
+            .await?;
+
+        let vorlesungsverzeichnis_link = link_by_text(&document, "Vorlesungsverzeichnis (VV)");
+
+        let document = self
+            .fetch_document(&format!(
+                "https://www.tucan.tu-darmstadt.de/{}",
+                vorlesungsverzeichnis_link
+            ))
+            .await?;
+
+        let aktuelles_vorlesungsverzeichnis_link =
+            link_by_text(&document, "Aktuell - Wintersemester 2022/23");
+
+        let document = self
+            .fetch_document(&format!(
+                "https://www.tucan.tu-darmstadt.de/{}",
+                aktuelles_vorlesungsverzeichnis_link
+            ))
+            .await?;
+
+        let informatik_link = link_by_text(&document, " FB20 - Informatik ");
+
+        let document = self
+            .fetch_document(&format!(
+                "https://www.tucan.tu-darmstadt.de/{}",
+                informatik_link
+            ))
+            .await?;
+
+        self.traverse_module_list(&document).await?;
+
+        Ok(())
+    }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let username = env::args().nth(1).unwrap();
+    let tucan = TUCAN {
+        client: reqwest::Client::builder().cookie_store(true).build()?,
+    };
 
-    let password = rpassword::prompt_password("TUCAN password: ").unwrap();
-
-    let params: [(&str, &str); 10] = [
-        ("usrname", &username),
-        ("pass", &password),
-        ("APPNAME", "CampusNet"),
-        ("PRGNAME", "LOGINCHECK"),
-        (
-            "ARGUMENTS",
-            "clino,usrname,pass,menuno,menu_type,browser,platform",
-        ),
-        ("clino", "000000000000001"),
-        ("menuno", "000344"),
-        ("menu_type", "classic"),
-        ("browser", ""),
-        ("platform", ""),
-    ];
-    let client = reqwest::Client::new();
-    let res_headers = client
-        .post("https://www.tucan.tu-darmstadt.de/scripts/mgrqispi.dll")
-        .form(&params)
-        .send()
-        .await?;
-
-    println!("{:?}", res_headers);
-
-    let res = res_headers.text().await?;
-
-    let document = Html::parse_document(&res);
-    println!("{}", document.root_element().html());
-
-    return Ok(());
-
-    let document = fetch_document("https://www.tucan.tu-darmstadt.de/").await?;
-
-    let redirect_url = &element_by_selector(&document, r#"meta[http-equiv="refresh"]"#)
-        .unwrap()
-        .value()
-        .attr("content")
-        .unwrap()[7..];
-
-    let document = fetch_document(&format!(
-        "https://www.tucan.tu-darmstadt.de/{}",
-        redirect_url
-    ))
-    .await?;
-
-    let redirect_url = element_by_selector(&document, r#"h2 a[href]"#)
-        .unwrap()
-        .value()
-        .attr("href")
-        .unwrap();
-
-    let document = fetch_document(&format!(
-        "https://www.tucan.tu-darmstadt.de/{}",
-        redirect_url
-    ))
-    .await?;
-
-    let vorlesungsverzeichnis_link = link_by_text(&document, "Vorlesungsverzeichnis (VV)");
-
-    let document = fetch_document(&format!(
-        "https://www.tucan.tu-darmstadt.de/{}",
-        vorlesungsverzeichnis_link
-    ))
-    .await?;
-
-    let aktuelles_vorlesungsverzeichnis_link =
-        link_by_text(&document, "Aktuell - Wintersemester 2022/23");
-
-    let document = fetch_document(&format!(
-        "https://www.tucan.tu-darmstadt.de/{}",
-        aktuelles_vorlesungsverzeichnis_link
-    ))
-    .await?;
-
-    let informatik_link = link_by_text(&document, " FB20 - Informatik ");
-
-    let document = fetch_document(&format!(
-        "https://www.tucan.tu-darmstadt.de/{}",
-        informatik_link
-    ))
-    .await?;
-
-    traverse_module_list(&document).await?;
-
-    Ok(())
+    tucan.start().await
 }
