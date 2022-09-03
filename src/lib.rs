7f6f093c23c2a7dc0d2292d266a257258f259ffb
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-09-03 16:24:57 +0200

    Allow scraping and rerunning

--- src/lib.rs
+++ src/lib.rs
@@ -11,10 +11,10 @@ use sqlx::{
 };
 use tokio::sync::Semaphore;
 
-struct Tucan {
-    client: Client,
-    semaphore: Semaphore,
-    pool: Pool<Sqlite>,
+pub struct Tucan {
+    pub client: Client,
+    pub semaphore: Semaphore,
+    pub pool: Pool<Sqlite>,
 }
 
 fn s(selector: &str) -> Selector {
@@ -186,50 +186,8 @@ impl Tucan {
         Ok(())
     }
 
-    async fn start(&self) -> Result<(), Box<dyn std::error::Error>> {
-        let username = env::args().nth(1).unwrap();
-
-        let password = env::args().nth(2).unwrap();
-        //let password = rpassword::prompt_password("TUCAN password: ").unwrap();
-
-        let params: [(&str, &str); 10] = [
-            ("usrname", &username),
-            ("pass", &password),
-            ("APPNAME", "CampusNet"),
-            ("PRGNAME", "LOGINCHECK"),
-            (
-                "ARGUMENTS",
-                "clino,usrname,pass,menuno,menu_type,browser,platform",
-            ),
-            ("clino", "000000000000001"),
-            ("menuno", "000344"),
-            ("menu_type", "classic"),
-            ("browser", ""),
-            ("platform", ""),
-        ];
-        let res_headers = self
-            .client
-            .post("https://www.tucan.tu-darmstadt.de/scripts/mgrqispi.dll")
-            .form(&params)
-            .send()
-            .await?;
-
-        let redirect_url = &format!(
-            "https://www.tucan.tu-darmstadt.de{}",
-            &res_headers.headers().get("refresh").unwrap().to_str()?[7..]
-        );
-
-        res_headers.text().await?;
-
+    pub async fn start(&self, redirect_url: &str) -> Result<(), Box<dyn std::error::Error>> {
         let document = self.fetch_document(redirect_url).await?;
-
-        let cnt = sqlx::query!(
-            "INSERT INTO entrypoints (entrypoint_url) VALUES (?)",
-            redirect_url
-        )
-        .execute(&self.pool)
-        .await?;
-        assert_eq!(cnt.rows_affected(), 1);
 
         let redirect_url = &element_by_selector(&document, r#".redirect h2 a[href]"#)
             .unwrap()
@@ -284,24 +242,4 @@ impl Tucan {
         }
         Ok(())
     }
-}
-
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let database_url = dotenvy::var("DATABASE_URL")?;
-    let pool = SqlitePoolOptions::new()
-        .max_connections(1)
-        .test_before_acquire(false)
-        .connect_with(SqliteConnectOptions::from_str(&database_url)?.create_if_missing(true))
-        .await?;
-
-    sqlx::migrate!().run(&pool).await?;
-
-    let tucan = Tucan {
-        pool,
-        client: reqwest::Client::builder().cookie_store(true).build()?,
-        semaphore: Semaphore::new(10), // risky
-    };
-
-    tucan.start().await
 }
