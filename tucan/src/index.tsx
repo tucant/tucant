8effe378441a5fba44605284bc385a445507ed91
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-09-05 13:31:21 +0200

    Setup react project with router and material ui

--- tucan/src/index.tsx
+++ tucan/src/index.tsx
@@ -1,15 +1,23 @@
+import CssBaseline from '@mui/material/CssBaseline';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from './routes/login';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <CssBaseline enableColorScheme />
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="login" element={<Login />} />
+    </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
---
+++ tucan/src/routes/login.tsx
@@ -0,0 +1,10 @@
+import Button from '@mui/material/Button';
+
+export default function Login() {
+  return (
+    <>
+    <Button variant="contained">Login</Button>
+    
+      </>
+  );
+}
