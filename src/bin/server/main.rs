aab69f071ccf5db24298b3192b35494b3a02e198
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-09-05 21:26:49 +0200

    Sample login endpoint

--- src/bin/server/main.rs
+++ src/bin/server/main.rs
@@ -1,47 +1,37 @@
 mod csrf_middleware;
 
-use std::future::{ready, Ready};
-
 use actix_cors::Cors;
-use actix_web::dev::{Response, Service, ServiceRequest, ServiceResponse, Transform};
-use actix_web::error::ErrorForbidden;
-use actix_web::{get, http, post, web, App, Either, Error, HttpResponse, HttpServer, Responder};
+use actix_web::{post, web, App, HttpServer, Responder};
 use csrf_middleware::CsrfMiddleware;
-use futures::future::{ok, LocalBoxFuture};
-use futures::FutureExt;
-use reqwest::header::{HeaderName, HeaderValue};
+use serde::{Deserialize, Serialize};
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+#[derive(Deserialize)]
+struct Login {
+    username: String,
+    password: String,
 }
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
+#[derive(Serialize)]
+struct LoginResult {
+    success: bool,
 }
 
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+#[post("/login")]
+async fn echo(login: web::Json<Login>) -> actix_web::Result<impl Responder> {
+    Ok(web::Json(LoginResult {
+        success: login.username == "hi"
+    }))
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        let cors = Cors::default()
-            .allowed_origin("http://localhost:3000/")
-            .allowed_methods(vec!["GET", "POST"])
-            .allowed_headers(vec![http::header::AUTHORIZATION, http::header::ACCEPT])
-            .allowed_header(http::header::CONTENT_TYPE)
-            .allowed_header(HeaderName::from_static("x-csrf-protection"))
-            .max_age(3600);
+        let cors = Cors::default().allow_any_method().allow_any_header().allowed_origin("http://localhost:3000");
 
         App::new()
             .wrap(cors)
             .wrap(CsrfMiddleware {})
-            .service(hello)
             .service(echo)
-            .route("/hey", web::get().to(manual_hello))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
