2223cd8c97d4fe60fc77e0c0c79bb753b6cf9441
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-09-05 23:40:43 +0200

    Persist sessions

--- src/bin/server/main.rs
+++ src/bin/server/main.rs
@@ -5,13 +5,20 @@ mod csrf_middleware;
 use std::{fmt::Display, time::Duration};
 
 use actix_cors::Cors;
-use actix_identity::{IdentityMiddleware, Identity, config::IdentityMiddlewareBuilder};
-use actix_session::{SessionMiddleware, storage::CookieSessionStore};
-use actix_web::{post, web, App, HttpServer, Responder, cookie::Key, get, HttpRequest, HttpResponse, error::ErrorUnauthorized};
+use actix_identity::{config::IdentityMiddlewareBuilder, Identity, IdentityMiddleware};
+use actix_session::{storage::CookieSessionStore, SessionMiddleware};
+use actix_web::HttpMessage;
+use actix_web::{
+    cookie::Key, error::ErrorUnauthorized, get, post, web, App, HttpRequest, HttpResponse,
+    HttpServer, Responder,
+};
 use csrf_middleware::CsrfMiddleware;
 use serde::{Deserialize, Serialize};
+use tokio::{
+    fs::{self, OpenOptions},
+    io::AsyncWriteExt,
+};
 use tucan_scraper::tucan::Tucan;
-use actix_web::HttpMessage;
 
 #[derive(Debug)]
 struct MyError {
@@ -24,8 +31,7 @@ impl Display for MyError {
     }
 }
 
-impl actix_web::error::ResponseError for MyError {
-}
+impl actix_web::error::ResponseError for MyError {}
 
 impl From<anyhow::Error> for MyError {
     fn from(err: anyhow::Error) -> MyError {
@@ -49,9 +55,7 @@ async fn login(request: HttpRequest, login: web::Json<Login>) -> Result<impl Res
     let tucan = Tucan::new().await?;
     tucan.login(&login.username, &login.password).await?;
     Identity::login(&request.extensions(), login.username.to_string().into()).unwrap();
-    Ok(web::Json(LoginResult {
-        success: true
-    }))
+    Ok(web::Json(LoginResult { success: true }))
 }
 
 #[post("/logout")]
@@ -82,19 +86,40 @@ async fn registration(user: Option<Identity>) -> Result<impl Responder, MyError>
     }
 }
 
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let secret_key = Key::generate();
+    let random_secret_key = Key::generate();
+
+    let file = OpenOptions::new()
+        .create_new(true)
+        .write(true)
+        .append(true)
+        .open("sessions.key")
+        .await;
+    if let Ok(mut file) = file {
+        file.write(random_secret_key.master()).await?;
+        drop(file)
+    }
+
+    let secret_key_raw = fs::read("sessions.key").await?;
+    let secret_key = Key::derive_from(&secret_key_raw);
 
     HttpServer::new(move || {
-        let cors = Cors::default().supports_credentials().allow_any_method().allow_any_header().allowed_origin("http://localhost:3000");
+        let cors = Cors::default()
+            .supports_credentials()
+            .allow_any_method()
+            .allow_any_header()
+            .allowed_origin("http://localhost:3000");
 
         App::new()
-            .wrap(IdentityMiddleware::builder().visit_deadline(Some(Duration::from_secs(24*3600))).build())
+            .wrap(
+                IdentityMiddleware::builder()
+                    .visit_deadline(Some(Duration::from_secs(24 * 3600)))
+                    .build(),
+            )
             .wrap(SessionMiddleware::new(
                 CookieSessionStore::default(),
-                secret_key.clone()
+                secret_key.clone(),
             ))
             .wrap(CsrfMiddleware {})
             .wrap(cors)
