b16127cc412044d69dff6b59acf5f0de36ee8af2
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-09-07 01:06:13 +0200

    Implement module menu navigation

--- tucan/src/routes/Modules.tsx
+++ tucan/src/routes/Modules.tsx
@@ -4,6 +4,7 @@ import LinearProgress from "@mui/material/LinearProgress";
 import List from "@mui/material/List";
 import Typography from "@mui/material/Typography";
 import { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import { RouterLink } from "../MiniDrawer";
 import InitialFetch from "./InitialFetch";
 
@@ -11,12 +12,13 @@ export default function Registration() {
   const [data, setData] = useState<any>(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string|null>(null);
+  const location = useLocation();
 
   useEffect(() => {
     const getData = async () => {
       try {
         const response = await fetch(
-          `http://localhost:8080${window.location.pathname}`,
+          `http://localhost:8080${location.pathname}`,
           {
             credentials: "include"
           }
@@ -37,7 +39,7 @@ export default function Registration() {
       }  
     }
     getData()
-  }, [])
+  }, [location])
 
   return (
     <>
@@ -49,7 +51,7 @@ export default function Registration() {
       {loading && <LinearProgress />}
       {error && <Alert severity="error">{error}</Alert>}
       <List>
-        { data != null && data.map((e: [string, string]) => <RouterLink to={`/modules/${e[1]}/`} text={e[0]}></RouterLink>)
+        { data != null && data.map((e: [string, string]) => <RouterLink to={`${location.pathname}${e[1]}/`} text={e[0]}></RouterLink>)
 
 
         }
