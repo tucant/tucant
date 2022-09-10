2666aa3b0ce4cb101f2fa5f16587734c0c367960
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-09-10 02:11:50 +0200

    The idea

--- migrations/20220902230519_initial.sql
+++ migrations/20220902230519_initial.sql
@@ -1,7 +1,11 @@
 PRAGMA foreign_keys = ON;
 
+-- TODO FIXME maybe this shows that the parent is not actually unique because
+-- some menu parts are integrated at different places
+-- if that's the case we need to fix the definition here
 CREATE TABLE module_menu (
     tucan_id TEXT NOT NULL PRIMARY KEY,
+    tucan_last_checked DATETIME NOT NULL,
     name TEXT NOT NULL,
     normalized_name TEXT NOT NULL,
     parent TEXT REFERENCES module_menu (tucan_id)
@@ -16,6 +20,7 @@ CREATE TABLE modules (
     content TEXT NOT NULL
 );
 
+-- Looking at the comment above maybe this is not necessary
 CREATE TABLE module_menu_module (
     module_menu_id TEXT NOT NULL REFERENCES module_menu (tucan_id),
     module_id TEXT NOT NULL REFERENCES modules (tucan_id),
