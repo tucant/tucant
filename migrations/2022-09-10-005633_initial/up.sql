1eccb08ff5c3c992516bf16052c730775815f920
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-09-18 23:17:51 +0200

    Try arrays as primary key to better store that value?

--- migrations/2022-09-10-005633_initial/up.sql
+++ migrations/2022-09-10-005633_initial/up.sql
@@ -1,6 +1,6 @@
 
 CREATE TABLE modules (
-    tucan_id TEXT NOT NULL PRIMARY KEY,
+    tucan_id INTEGER[] NOT NULL PRIMARY KEY,
     tucan_last_checked TIMESTAMP WITH TIME ZONE NOT NULL,
     title TEXT NOT NULL,
     module_id TEXT NOT NULL,
@@ -12,16 +12,16 @@ CREATE TABLE modules (
 -- some menu parts are integrated at different places
 -- if that's the case we need to fix the definition here
 CREATE TABLE module_menu (
-    tucan_id TEXT NOT NULL PRIMARY KEY,
+    tucan_id INTEGER[] NOT NULL PRIMARY KEY,
     tucan_last_checked TIMESTAMP WITH TIME ZONE NOT NULL,
     name TEXT NOT NULL,
     normalized_name TEXT NOT NULL,
-    parent TEXT REFERENCES module_menu (tucan_id)
+    parent INTEGER[] REFERENCES module_menu (tucan_id)
 );
 
 -- Looking at the comment above maybe this is not necessary
 CREATE TABLE module_menu_module (
-    module_menu_id TEXT NOT NULL REFERENCES module_menu (tucan_id),
-    module_id TEXT NOT NULL REFERENCES modules (tucan_id),
+    module_menu_id INTEGER[] NOT NULL REFERENCES module_menu (tucan_id),
+    module_id INTEGER[] NOT NULL REFERENCES modules (tucan_id),
     PRIMARY KEY (module_menu_id, module_id)
 );
--- src/schema.rs
+++ src/schema.rs
@@ -2,24 +2,24 @@
 
 diesel::table! {
     module_menu (tucan_id) {
-        tucan_id -> Text,
+        tucan_id -> Array<Nullable<Int4>>,
         tucan_last_checked -> Timestamptz,
         name -> Text,
         normalized_name -> Text,
-        parent -> Nullable<Text>,
+        parent -> Nullable<Array<Nullable<Int4>>>,
     }
 }
 
 diesel::table! {
     module_menu_module (module_menu_id, module_id) {
-        module_menu_id -> Text,
-        module_id -> Text,
+        module_menu_id -> Array<Nullable<Int4>>,
+        module_id -> Array<Nullable<Int4>>,
     }
 }
 
 diesel::table! {
     modules (tucan_id) {
-        tucan_id -> Text,
+        tucan_id -> Array<Nullable<Int4>>,
         tucan_last_checked -> Timestamptz,
         title -> Text,
         module_id -> Text,
@@ -31,4 +31,8 @@ diesel::table! {
 diesel::joinable!(module_menu_module -> module_menu (module_menu_id));
 diesel::joinable!(module_menu_module -> modules (module_id));
 
-diesel::allow_tables_to_appear_in_same_query!(module_menu, module_menu_module, modules,);
+diesel::allow_tables_to_appear_in_same_query!(
+    module_menu,
+    module_menu_module,
+    modules,
+);
