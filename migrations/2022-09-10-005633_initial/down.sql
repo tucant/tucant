23a1adf14ca90f985b14444465e912087d1564f3
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-09-27 00:09:25 +0200

    Clean up database migrations

--- migrations/2022-09-10-005633_initial/down.sql
+++ migrations/2022-09-10-005633_initial/down.sql
@@ -1,13 +1,15 @@
+DROP INDEX courses_idx;
 DROP INDEX modules_idx;
 
-DROP TEXT SEARCH CONFIGURATION tucan;
-
-DROP TEXT SEARCH DICTIONARY english_hunspell;
-DROP TEXT SEARCH DICTIONARY german_hunspell;
-
+DROP TABLE module_courses;
+DROP TABLE courses_unfinished;
 DROP TABLE users_studies;
 DROP TABLE users;
 DROP TABLE module_menu_tree;
 DROP TABLE module_menu_module;
 DROP TABLE module_menu_unfinished;
 DROP TABLE modules_unfinished;
+
+DROP TEXT SEARCH CONFIGURATION tucan;
+DROP TEXT SEARCH DICTIONARY english_hunspell;
+DROP TEXT SEARCH DICTIONARY german_hunspell;
