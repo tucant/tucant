308d28c3b3ff51d89ddce6e83817c917604dd7dd
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-10-02 00:26:29 +0200

    Allow lazily loading modules and courses

--- backend-rust/src/bin/server/s_course.rs
+++ backend-rust/src/bin/server/s_course.rs
@@ -2,40 +2,43 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
+use std::io::ErrorKind;
+
 use crate::MyError;
 use actix_session::Session;
 use actix_web::post;
 use actix_web::web::Json;
 
 use actix_web::web::Data;
-use diesel::ExpressionMethods;
-use diesel::QueryDsl;
-use diesel_async::RunQueryDsl;
-use tucant::{models::Course, schema::courses_unfinished, tucan::Tucan};
+
+
+
+use tucant::tucan_user::TucanSession;
+use tucant::url::Coursedetails;
+use tucant::{models::Course, tucan::Tucan};
 use tucant_derive::ts;
 
 #[ts]
 #[post("/course")]
 pub async fn course(
-    _: Session,
+    session: Session,
     tucan: Data<Tucan>,
     input: Json<String>,
 ) -> Result<Json<Course>, MyError> {
-    let mut connection = tucan.pool.get().await?;
+    match session.get::<TucanSession>("session").unwrap() {
+        Some(session) => {
+            let binary_path = base64::decode(input.as_bytes()).unwrap();
 
-    let result = courses_unfinished::table
-        .filter(courses_unfinished::tucan_id.eq(base64::decode(input.as_bytes()).unwrap()))
-        .select((
-            courses_unfinished::tucan_id,
-            courses_unfinished::tucan_last_checked,
-            courses_unfinished::title,
-            courses_unfinished::course_id,
-            courses_unfinished::sws,
-            courses_unfinished::content,
-            courses_unfinished::done,
-        ))
-        .get_result::<Course>(&mut connection)
-        .await?;
+            let tucan = tucan.continue_session(session).await.unwrap();
 
-    Ok(Json(result))
+            let result = tucan
+                .course(Coursedetails {
+                    id: binary_path.clone(),
+                })
+                .await?;
+
+            Ok(Json(result))
+        }
+        None => Err(std::io::Error::new(ErrorKind::Other, "no session!").into()),
+    }
 }
--- backend-rust/src/schema.rs
+++ backend-rust/src/schema.rs
@@ -1,7 +1,3 @@
-// SPDX-FileCopyrightText: The tucant Contributors
-//
-// SPDX-License-Identifier: AGPL-3.0-or-later
-
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
