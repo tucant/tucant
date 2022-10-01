00dc8e9e855c2bd3a5830d1ecaf4f4b7c65ccdd8
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-10-01 15:28:19 +0200

    Formatting

--- backend-rust-derive/src/lib.rs
+++ backend-rust-derive/src/lib.rs
@@ -1,11 +1,8 @@
 use proc_macro2::{Ident, TokenStream};
 use quote::{format_ident, quote, ToTokens};
 use syn::{
-    parse::Nothing,
-    parse_macro_input,
-    spanned::Spanned,
-    visit::{Visit},
-    Error, Item, ItemEnum, ItemFn, Pat, PatIdent, PatType, Meta, NestedMeta, Lit, DeriveInput, Data, DataStruct, DataEnum,
+    parse::Nothing, parse_macro_input, spanned::Spanned, visit::Visit, Data, DataEnum, DataStruct,
+    DeriveInput, Error, Item, ItemEnum, ItemFn, Lit, Meta, NestedMeta, Pat, PatIdent, PatType,
 };
 
 // RUSTFLAGS="-Z macro-backtrace" cargo test
@@ -21,7 +18,9 @@ impl<'ast> Visit<'ast> for InnermostTypeVisitor {
         &mut self,
         i: &'ast syn::AngleBracketedGenericArguments,
     ) {
-        if let Some(v) = i.args.first() { self.visit_generic_argument(v) }
+        if let Some(v) = i.args.first() {
+            self.visit_generic_argument(v)
+        }
     }
 
     fn visit_type_tuple(&mut self, i: &'ast syn::TypeTuple) {
@@ -29,77 +28,68 @@ impl<'ast> Visit<'ast> for InnermostTypeVisitor {
     }
 }
 
-
-    fn handle_item_fn(node: &ItemFn) -> TokenStream {
-        let return_type = match node.sig.output {
-            syn::ReturnType::Default => format_ident!("void").to_token_stream(),
-            syn::ReturnType::Type(_, ref path) => {
-                let mut innermost_type_visitor = InnermostTypeVisitor(None);
-                innermost_type_visitor.visit_type(path);
-                innermost_type_visitor.0.unwrap()
-            }
-        };
-
-        let actix_macro = node.attrs.iter().find(|attr| {
-            attr.path.get_ident().map(Ident::to_string) == Some("get".to_string())
-            || attr.path.get_ident().map(Ident::to_string) == Some("post".to_string())
-        });
-
-        if actix_macro.is_none() {
-            return  Error::new_spanned(
-                    node,
-                    r#"could not find actix get or post attribute macro"#,
-                )
-                .to_compile_error()
-            
+fn handle_item_fn(node: &ItemFn) -> TokenStream {
+    let return_type = match node.sig.output {
+        syn::ReturnType::Default => format_ident!("void").to_token_stream(),
+        syn::ReturnType::Type(_, ref path) => {
+            let mut innermost_type_visitor = InnermostTypeVisitor(None);
+            innermost_type_visitor.visit_type(path);
+            innermost_type_visitor.0.unwrap()
         }
-        let url_path = actix_macro.unwrap().parse_meta().unwrap();
-        let url_path = match url_path {
-            Meta::List(meta_list) => {
-                match meta_list.nested.iter().next() {
-                    Some(NestedMeta::Lit(Lit::Str(str))) => {
-                        str.value()
+    };
+
+    let actix_macro = node.attrs.iter().find(|attr| {
+        attr.path.get_ident().map(Ident::to_string) == Some("get".to_string())
+            || attr.path.get_ident().map(Ident::to_string) == Some("post".to_string())
+    });
+
+    if actix_macro.is_none() {
+        return Error::new_spanned(node, r#"could not find actix get or post attribute macro"#)
+            .to_compile_error();
+    }
+    let url_path = actix_macro.unwrap().parse_meta().unwrap();
+    let url_path = match url_path {
+        Meta::List(meta_list) => match meta_list.nested.iter().next() {
+            Some(NestedMeta::Lit(Lit::Str(str))) => str.value(),
+            _ => panic!(),
+        },
+        _ => panic!(),
+    };
+
+    let arg_type = node
+        .sig
+        .inputs
+        .iter()
+        .filter_map(|arg| match arg {
+            syn::FnArg::Receiver(_) => None,
+            syn::FnArg::Typed(PatType { pat, ty, .. }) => {
+                if let Pat::Ident(PatIdent { ident, .. }) = &**pat {
+                    if *ident == "input" || *ident == "_input" {
+                        let mut innermost_type_visitor = InnermostTypeVisitor(None);
+                        innermost_type_visitor.visit_type(ty);
+                        return Some(innermost_type_visitor.0.unwrap().to_token_stream());
                     }
-                    _ => panic!()
                 }
+                None
             }
-            _ => panic!()
-        };
+        })
+        .next();
 
-        let arg_type = node
-            .sig
-            .inputs
-            .iter()
-            .filter_map(|arg| match arg {
-                syn::FnArg::Receiver(_) => None,
-                syn::FnArg::Typed(PatType { pat, ty, .. }) => {
-                    if let Pat::Ident(PatIdent { ident, .. }) = &**pat {
-                        if *ident == "input" || *ident == "_input" {
-                            let mut innermost_type_visitor = InnermostTypeVisitor(None);
-                            innermost_type_visitor.visit_type(ty);
-                            return Some(innermost_type_visitor.0.unwrap().to_token_stream());
-                        }
-                    }
-                    None
+    if let Some(arg_type) = arg_type {
+        let name = &node.sig.ident;
+        let name_string = node.sig.ident.to_string();
+        return quote! {
+            #node
+
+            impl tucant::typescript::Typescriptable for #name {
+                fn name() -> String {
+                    #name_string.to_string()
                 }
-            })
-            .next();
 
-        if let Some(arg_type) = arg_type {
-            let name = &node.sig.ident;
-            let name_string = node.sig.ident.to_string();
-            return quote! {
-                #node
-
-                impl tucant::typescript::Typescriptable for #name {
-                    fn name() -> String {
-                        #name_string.to_string()
-                    }
-
-                    fn code() -> ::std::collections::HashSet<String> {
-                        let mut result = ::std::collections::HashSet::from(["export async function ".to_string() + &<#name as tucant::typescript::Typescriptable>::name() + "(input: " + &<#arg_type as tucant::typescript::Typescriptable>::name() + ")"
-                        + ": Promise<" + &<#return_type as tucant::typescript::Typescriptable>::name() + "> {" +
-                        r#"
+                fn code() -> ::std::collections::HashSet<String> {
+                    let mut result = ::std::collections::HashSet::from(["export async function ".to_string() + &<#name as tucant::typescript::Typescriptable>::name() + "(input: " + &<#arg_type as tucant::typescript::Typescriptable>::name() + ")"
+                    + ": Promise<" + &<#return_type as tucant::typescript::Typescriptable>::name() + "> {" +
+                    r#"
     const response = await fetch("http://localhost:8080"# + #url_path + r#"", {
     credentials: "include",
     method: "POST",
@@ -110,28 +100,27 @@ impl<'ast> Visit<'ast> for InnermostTypeVisitor {
     body: JSON.stringify(input),
     });
     return await response.json() as "# + &<#return_type as tucant::typescript::Typescriptable>::name() +
-"\n}"]);
-                        result.extend(<#arg_type as tucant::typescript::Typescriptable>::code());
-                        result.extend(<#return_type as tucant::typescript::Typescriptable>::code());       
-                        result                
-                    }
+        "\n}"]);
+                    result.extend(<#arg_type as tucant::typescript::Typescriptable>::code());
+                    result.extend(<#return_type as tucant::typescript::Typescriptable>::code());
+                    result
                 }
             }
-        } else {
-            return Error::new(
-                    node.sig.inputs.span(),
-                    r#"name one of the parameters `input` or `_input`"#,
-                )
-                .to_compile_error()
-                    }
+        };
+    } else {
+        return Error::new(
+            node.sig.inputs.span(),
+            r#"name one of the parameters `input` or `_input`"#,
+        )
+        .to_compile_error();
     }
-
+}
 
 fn typescriptable_impl(input: DeriveInput) -> TokenStream {
     let name = &input.ident;
-        let name_string = input.ident.to_string();
+    let name_string = input.ident.to_string();
 
-        let (members, members_code) = match &input.data {
+    let (members, members_code) = match &input.data {
             Data::Struct(DataStruct { fields: syn::Fields::Named(fields_named), .. }) => {
                 fields_named.named.iter().map(|field| {
                     let ident_string = field.ident.as_ref().unwrap().to_string();
@@ -192,23 +181,23 @@ fn typescriptable_impl(input: DeriveInput) -> TokenStream {
             _ => panic!()
         };
 
-        quote! {
-            #input
+    quote! {
+        #input
 
-            impl tucant::typescript::Typescriptable for #name {
-                fn name() -> String {
-                    #name_string.to_string()
-                }
+        impl tucant::typescript::Typescriptable for #name {
+            fn name() -> String {
+                #name_string.to_string()
+            }
 
-                fn code() -> ::std::collections::HashSet<String> {
-                    let mut result = ::std::collections::HashSet::from(["type ".to_string() + &#name::name() + " = {\n"
-                    #members
-                    + "}"]);
-                    #members_code
-                    result
-                }
+            fn code() -> ::std::collections::HashSet<String> {
+                let mut result = ::std::collections::HashSet::from(["type ".to_string() + &#name::name() + " = {\n"
+                #members
+                + "}"]);
+                #members_code
+                result
             }
         }
+    }
 }
 
 #[proc_macro_attribute]
@@ -228,7 +217,6 @@ pub fn typescriptable(input: proc_macro::TokenStream) -> proc_macro::TokenStream
 
 #[cfg(test)]
 mod tests {
-    
 
     #[test]
     fn it_works() {}
