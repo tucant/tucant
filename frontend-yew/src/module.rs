00e9cbf9ff74e6e6f05040080be8387f39d7fdde
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-10-08 21:24:31 +0200

    Formatting

--- frontend-yew/src/module.rs
+++ frontend-yew/src/module.rs
@@ -1,7 +1,7 @@
 use gloo_net::http::Request;
-use tucant::models::{ModuleResponse};
+use tucant::models::ModuleResponse;
 use yew::prelude::*;
-use yew::suspense::{use_future};
+use yew::suspense::use_future;
 
 #[function_component(Content)]
 fn content() -> HtmlResult {
