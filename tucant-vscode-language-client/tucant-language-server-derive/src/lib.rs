15621d01a8b2032277ead9b0c02dbccd99d8930a
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-10-11 22:05:42 +0200

    More progress

--- tucant-vscode-language-client/tucant-language-server-derive/src/lib.rs
+++ tucant-vscode-language-client/tucant-language-server-derive/src/lib.rs
@@ -8,6 +8,72 @@ use syn::{parse::Nothing, parse_macro_input, Error, LitStr, token::Brace, Expr};
 
 // this is manually extracted from the metaModel.schema.json (but we should probably generate this at some point)
 // well it contains insufficient information e.g. no default for proposed so we need to probably do this manually
+
+#[derive(Serialize, Deserialize, Debug)]
+enum BaseTypes {
+    #[serde(rename = "URI")]
+    Uri,
+    #[serde(rename = "DocumentUri")]
+    DocumentUri,
+    #[serde(rename = "integer")]
+    Integer,
+    #[serde(rename = "uinteger")]
+    UnsignedInteger,
+    #[serde(rename = "decimal")]
+    Decimal,
+    #[serde(rename = "RegExp")]
+    RegExp,
+    #[serde(rename = "string")]
+    String,
+    #[serde(rename = "boolean")]
+    Boolean,
+    #[serde(rename = "null")]
+    Null
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+#[serde(deny_unknown_fields)]
+/// Represents a base type like `string` or `DocumentUri`.
+/// kind = "base"
+struct BaseType {
+    name: BaseTypes
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+#[serde(deny_unknown_fields)]
+/// Represents a boolean literal type (e.g. `kind: true`).
+/// kind = "booleanLiteral"
+struct BooleanLiteralType {
+    value: bool
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(untagged)]
+enum StringOrNumber {
+    String(String),
+    Number(i64),
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+#[serde(deny_unknown_fields)]
+/// Defines an enumeration entry.
+struct EnumerationEntry {
+    /// An optional documentation.
+    documentation: Option<String>,
+    /// The name of the enum item.
+    name: String,
+    /// Whether this is a proposed enumeration entry. If omitted, the enumeration entry is final.
+    #[serde(default)]
+    proposed: bool,
+    /// Since when (release number) this enumeration entry is available. Is undefined if not known.
+    since: String,
+    /// The value.
+    value: StringOrNumber
+}
+
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 #[serde(deny_unknown_fields)]
