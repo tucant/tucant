be2b420857c3aa179b2656bd0f564d16730dedc2
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-10-11 22:44:07 +0200

    More progress

--- tucant-vscode-language-client/tucant-language-server-derive/src/lib.rs
+++ tucant-vscode-language-client/tucant-language-server-derive/src/lib.rs
@@ -6,8 +6,34 @@ use serde::{Deserialize, Serialize};
 use serde_json::Value;
 use syn::{parse::Nothing, parse_macro_input, Error, LitStr, token::Brace, Expr};
 
+// Try https://crates.io/crates/schemafy
+
 // this is manually extracted from the metaModel.schema.json (but we should probably generate this at some point)
 // well it contains insufficient information e.g. no default for proposed so we need to probably do this manually
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+#[serde(deny_unknown_fields)]
+/// Represents an `and`type (e.g. TextDocumentParams & WorkDoneProgressParams`).
+/// kind = "and"
+struct AndType {
+    items: Vec<Type>,
+}
+
+/// Represents an array type (e.g. `TextDocument[]`).
+/// kind = "array"
+struct ArrayType {
+    element: Type,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+#[serde(deny_unknown_fields)]
+/// Represents a base type like `string` or `DocumentUri`.
+/// kind = "base"
+struct BaseType {
+    name: BaseTypes
+}
 
 #[derive(Serialize, Deserialize, Debug)]
 enum BaseTypes {
@@ -34,19 +60,33 @@ enum BaseTypes {
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 #[serde(deny_unknown_fields)]
-/// Represents a base type like `string` or `DocumentUri`.
-/// kind = "base"
-struct BaseType {
-    name: BaseTypes
+/// Represents a boolean literal type (e.g. `kind: true`).
+/// kind = "booleanLiteral"
+struct BooleanLiteralType {
+    value: bool
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 #[serde(deny_unknown_fields)]
-/// Represents a boolean literal type (e.g. `kind: true`).
-/// kind = "booleanLiteral"
-struct BooleanLiteralType {
-    value: bool
+/// Defines an enumeration.
+struct Enumeration {
+    /// An optional documentation.
+    documentation: Option<String>,
+    /// The name of the enumeration.
+    name: String,
+    /// Whether this is a proposed enumeration. If omitted, the enumeration is final.
+    #[serde(default)]
+    proposed: bool,
+    /// Since when (release number) this enumeration is available. Is undefined if not known.
+    since: Option<String>,
+    /// Whether the enumeration supports custom values (e.g. values which are not part of the set defined in `values`). If omitted no custom values are supported.
+    supports_custom_values: bool,
+    /// The type of the elements.
+    #[serde(rename = "type")]
+    _type: EnumerationType,
+    /// The enum values.
+    values: Vec<EnumerationEntry>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -138,6 +178,41 @@ struct ReferenceType {
 /// kind = "stringLiteral"
 struct StringLiteralType {
     value: String
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(tag = "kind")]
+enum Type {
+    BaseType(BaseType),
+    StringLiteralType(StringLiteralType),
+    IntegerLiteralType(IntegerLiteralType),
+    BooleanLiteralType(BooleanLiteralType)
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+enum TypeKind {
+    #[serde(rename = "base")]
+    Base,
+    #[serde(rename = "reference")]
+    Reference,
+    #[serde(rename = "array")]
+    Array,
+    #[serde(rename = "map")]
+    Map,
+    #[serde(rename = "and")]
+    And,
+    #[serde(rename = "or")]
+    Or,
+    #[serde(rename = "tuple")]
+    Tuple,
+    #[serde(rename = "literal")]
+    Literal,
+    #[serde(rename = "stringLiteral")]
+    StringLiteral,
+    #[serde(rename = "integerLiteral")]
+    IntegerLiteral,
+    #[serde(rename = "booleanLiteral")]
+    BooleanLiteral,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
