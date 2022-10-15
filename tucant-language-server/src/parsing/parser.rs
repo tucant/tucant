e012abe8215e80b3a7edad4df1669d1cdbe22ea8
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-10-15 16:10:05 +0200

    Looks like its more or less working

--- tucant-language-server/src/parsing/parser.rs
+++ tucant-language-server/src/parsing/parser.rs
@@ -1,47 +1,87 @@
 use std::fmt::Debug;
 
+use once_cell::sync::Lazy;
+use regex::Regex;
+
 #[derive(Clone, Copy)]
-pub struct Span<T: Debug> {
+pub struct Span<'a, T: Debug> {
     inner: T,
-    start: usize,
-    line_start: usize,
-    column_start: usize,
-    end: usize,
-    line_end: usize,
-    column_end: usize,
+    full_string: &'a str, // TODO this could become a ref to a struct for the whole file with name info etc
+    string: &'a str,
 }
 
-impl<'a> Debug for Span<&'a str> {
+#[derive(Debug)]
+pub struct Error<'a> {
+    location: Span<'a, ()>,
+    reason: &'static str,
+}
+
+fn offset_to_line_column<'a, T: Debug>(span: &Span<'a, T>, string: &str) -> (usize, usize) {
+    span.full_string[..(string.as_ptr() as usize-span.full_string.as_ptr() as usize)]
+        .lines()
+        .enumerate()
+        .last()
+        .map_or((0, 0), |(index, last_line)| (index, last_line.len()))
+}
+
+impl<'a> Debug for Span<'a, ()> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let start_pos = offset_to_line_column(self, self.string);
+        let end_pos = offset_to_line_column(self, &self.string[self.string.len()..]);
         write!(
             f,
             "location:{}:{} - location:{}:{}\n{}",
-            self.line_start,
-            self.column_start,
-            self.line_end,
-            self.column_end,
-            &self.inner[self.start..self.end]
+            start_pos.0,
+            start_pos.1,
+            end_pos.0,
+            end_pos.1,
+            &self.string
         )
     }
 }
 
-impl<'a> Span<&'a str> {
+impl<'a> Span<'a, ()> {
     fn new(input: &'a str) -> Self {
-        let last_line_pos = input
-            .lines()
-            .enumerate()
-            .last()
-            .map_or((0, 0), |(index, last_line)| (index, last_line.len()));
         Self {
-            inner: input,
-            start: 0,
-            line_start: 0,
-            column_start: 0,
-            end: input.len(),
-            line_end: last_line_pos.0,
-            column_end: last_line_pos.1,
+            inner: (),
+            full_string: input,
+            string: input,
         }
     }
+}
+
+impl<'a> Into<&'a str> for Span<'a, ()> {
+    fn into(self) -> &'a str {
+        &self.string
+    }
+}
+
+// only support raw strings
+fn parse_string<'a>(input: Span<&'a str>) {
+
+}
+
+// https://doc.rust-lang.org/book/ch08-02-strings.html
+fn parse_number<'a>(input: Span<'a, ()>) -> Result<i64, Error> {
+   let input_str: &'a str = input.into();
+   let end_of_numbers = input_str.char_indices().skip_while(|(_, character)| {
+    character.is_ascii_digit()
+   }).map(|(offset, _)| offset).next().unwrap_or(input_str.len());
+   let number_str = &input_str[0..end_of_numbers];
+   number_str.parse::<i64>().map_err(|_| {
+    Error {
+        location: Span {
+            inner: (),
+            full_string: input.full_string,
+            string: number_str
+        },
+        reason: "Failed to parse as number",
+    }
+   })
+}
+
+fn parse_atom<'a>(input: Span<&'a str>) {
+
 }
 
 fn init() {
@@ -55,7 +95,39 @@ fn test_parse_number() {
     /*let span = Span::new(r#"
     (this is an (epic awesome great) "test" 5)
     "#);*/
-    let _span = Span::new(r#"test fdsf"#);
+    let span = Span::new(r#"notanumber"#);
+    let number = parse_number(span);
+    println!("{:?}", number);
 
-    let _span = Span::new(r#"notest fdsf"#);
+    let span = Span::new(r#"3notendingwithanumber"#);
+    let number = parse_number(span);
+    println!("{:?}", number);
+
+    let span = Span::new(r#"3"#);
+    let number = parse_number(span);
+    println!("{:?}", number);
+
+    let span = Span::new(r#"3z9"#);
+    let number = parse_number(span);
+    println!("{:?}", number);
+
+    let span = Span::new(r#"3546z945"#);
+    let number = parse_number(span);
+    println!("{:?}", number);
+
+    let span = Span::new(r#"345345"#);
+    let number = parse_number(span);
+    println!("{:?}", number);
+
+    let span = Span::new(r#"345345sdfasd"#);
+    let number = parse_number(span);
+    println!("{:?}", number);
+
+    let span = Span::new(r#"n32otanumber"#);
+    let number = parse_number(span);
+    println!("{:?}", number);
+
+    let span = Span::new(r#"70708777897986976707598759785978698752otanumber"#);
+    let number = parse_number(span);
+    println!("{:?}", number);
 }
