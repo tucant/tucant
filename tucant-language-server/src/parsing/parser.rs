9d31ca971f1a615a1084682fd261c03d0c9c0e50
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-10-15 16:36:50 +0200

    Still a bit buggy

--- tucant-language-server/src/parsing/parser.rs
+++ tucant-language-server/src/parsing/parser.rs
@@ -24,14 +24,14 @@ fn offset_to_line_column<'a, T: Debug>(span: &Span<'a, T>, string: &str) -> (usi
         .map_or((0, 0), |(index, last_line)| (index, last_line.len()))
 }
 
-impl<'a> Debug for Span<'a, ()> {
+impl<'a, T: Debug> Debug for Span<'a, T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let start_pos = offset_to_line_column(self, self.string);
         let end_pos = offset_to_line_column(self, &self.string[self.string.len()..]);
         write!(
             f,
-            "location:{}:{} - location:{}:{}\n{}",
-            start_pos.0, start_pos.1, end_pos.0, end_pos.1, &self.string
+            "location:{}:{} - location:{}:{}\n{}\nvalue:{:?}",
+            start_pos.0, start_pos.1, end_pos.0, end_pos.1, &self.string, self.inner
         )
     }
 }
@@ -76,10 +76,22 @@ fn parse_string<'a>(input: Span<'a, ()>) -> Result<(Span<&'a str>, Span<'a, ()>)
     };
     match it
         .skip_while(|(_, character)| *character != '"')
+        .skip(1)
         .map(|(offset, _)| offset)
         .next()
     {
-        Some(_) => todo!(),
+        Some(offset) => {
+            let (str_str, rest_str) = input_str.split_at(offset);
+            Ok((Span {
+                inner: str_str.trim_matches('"'),
+                full_string: input.full_string,
+                string: str_str,
+            }, Span {
+                inner: (),
+                full_string: input.full_string,
+                string: rest_str,
+            }))
+        },
         None => Err(Error {
             location: input,
             reason: r#"Unterminated string literal"#,
@@ -96,8 +108,8 @@ fn parse_number<'a>(input: Span<'a, ()>) -> Result<(Span<i64>, Span<'a, ()>), Er
         .map(|(offset, _)| offset)
         .next()
         .unwrap_or(input_str.len()); // TODO FIXME different error message
-    let number_str = &input_str[0..end_of_numbers];
-    Ok(Span {
+    let (number_str, rest_str) = input_str.split_at(end_of_numbers);
+    Ok((Span {
         inner: number_str.parse::<i64>().map_err(|_| Error {
             location: Span {
                 inner: (),
@@ -108,7 +120,11 @@ fn parse_number<'a>(input: Span<'a, ()>) -> Result<(Span<i64>, Span<'a, ()>), Er
         })?,
         full_string: input.full_string,
         string: number_str,
-    })
+    }, Span {
+        inner: (),
+        full_string: input.full_string,
+        string: rest_str,
+    }))
 }
 
 fn parse_atom<'a>(input: Span<&'a str>) {}
@@ -159,4 +175,26 @@ fn test_parse_number() {
     let span = Span::new(r#"70708777897986976707598759785978698752otanumber"#);
     let number = parse_number(span);
     println!("{:?}", number);
+}
+
+// RUST_LOG=trace cargo watch -x 'test -- --nocapture test_parse_string'
+#[test]
+fn test_parse_string() {
+    init();
+
+    let span = Span::new(r#"notastring"#);
+    let string = parse_string(span);
+    println!("{:?}", string);
+
+    let span = Span::new(r#""unterminated"#);
+    let string = parse_string(span);
+    println!("{:?}", string);
+
+    let span = Span::new(r#""astring"jojo"#);
+    let string = parse_string(span);
+    println!("{:?}", string);
+
+    let span = Span::new(r#""astring""#);
+    let string = parse_string(span);
+    println!("{:?}", string);
 }
