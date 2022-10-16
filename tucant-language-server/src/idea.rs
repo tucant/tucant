7d9f11aa41eddab7858e9fdf576d5942e6b1800f
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-10-16 18:03:39 +0200

    More idea progress

--- tucant-language-server/src/idea.rs
+++ tucant-language-server/src/idea.rs
@@ -27,9 +27,23 @@ pub struct Handler {
     tx: mpsc::Sender<(CatRequest, Option<oneshot::Sender<CatResponse>>)>
 }
 
+#[derive(Debug)]
+pub enum Requests {
+    ElephantRequest(ElephantRequest),
+    CatRequest(CatRequest),
+    DogRequest(DogRequest),
+}
+
+#[derive(Debug)]
+pub enum Responses {
+    ElephantResponse(ElephantResponse),
+    CatResponse(CatResponse),
+    DogResponse(DogResponse),
+}
+
 // concurrent by default
 impl Handler {
-    async fn handle_sending(mut rx: mpsc::Receiver<(CatRequest, Option<oneshot::Sender<CatResponse>>)>) -> mpsc::Receiver<(CatRequest, Option<oneshot::Sender<CatResponse>>)> {
+    async fn handle_sending(mut rx: mpsc::Receiver<(CatRequest, Option<oneshot::Sender<CatResponse>>)>) -> (mpsc::Receiver<(CatRequest, Option<oneshot::Sender<CatResponse>>)>, Option<(String, oneshot::Sender<CatResponse>)>) {
         println!("Handle sending start");
         if let Some((req, res)) = rx.recv().await {
             if let Some(res) = res {
@@ -38,29 +52,33 @@ impl Handler {
                     .take(30)
                     .map(char::from)
                     .collect();
-                //pending_requests.insert(rand_string, res);
-
                 // TODO actually send
+                println!("Handle sending end");
+                (rx, Some((rand_string, res)))
             } else {
                 // TODO actually send (also while sending we should be able to receive)
+                println!("Handle sending end");
+                (rx, None)
             }
+        } else {
+            // TODO FIXME
+            println!("DONE!!!!");
+            (rx, None)
         }
-        println!("Handle sending end");
-        rx
     }
 
-    async fn handle_receiving(self: Arc<Self>) {
+    async fn handle_receiving(self: Arc<Self>) -> (String, Requests) {
         println!("handle receiving start");
         let request = self.clone().retrieve_next().await;
         let id = "1337".to_string();
 
         let cloned_self = self.clone();
         spawn(async move {
-            cloned_self.handle_dog(DogRequest).await;
+            cloned_self.handle(Requests::DogRequest(DogRequest)).await;
         });
 
-        //pending_requests.remove(&id).unwrap().send(request).unwrap();
         println!("handle receiving end");
+        (id, request)
     }
 
     // https://smallcultfollowing.com/babysteps/blog/2022/06/13/async-cancellation-a-case-study-of-pub-sub-in-mini-redis/
@@ -78,7 +96,7 @@ impl Handler {
         };
         let self_arc = Arc::new(the_self);
 
-        let mut pending_requests: HashMap<String, oneshot::Sender<CatResponse>> = HashMap::new();
+        let mut pending_requests: HashMap<String, oneshot::Sender<Requests>> = HashMap::new();
 
         // I think this still does not work when they're cancelled?
         let mut receive_handler = self_arc.clone().handle_receiving();
@@ -88,22 +106,29 @@ impl Handler {
 
         loop {
             tokio::select! {
-                res = &mut receive_handler => {
+                (id, request) = &mut receive_handler => {
+                    pending_requests.remove(&id).unwrap().send(request).unwrap();
                     receive_handler.set(self_arc.clone().handle_receiving());
                 }
-                rx = &mut send_handler => {
+                (rx, hashmap_insert) = &mut send_handler => {
+                    if let Some((id, res)) = hashmap_insert {
+                        pending_requests.insert(id, res);
+                    }
                     send_handler.set(Handler::handle_sending(rx));
                 }
             }
         }
     }
 
-    pub async fn handle_elephant(&self, request: ElephantRequest) -> ElephantResponse {
-        todo!()
-    }
-
-    pub async fn handle_dog(&self, request: DogRequest) -> DogResponse {
+    pub async fn handle(&self, request: Requests) {
         self.send_cat_request(CatRequest).await;
+
+        //request.send_response(DogResponse);
+        /*
+        internally:
+        self.tx.send((DogResponse, None)).await.unwrap();
+        */
+
         todo!()
     }
 
@@ -114,7 +139,7 @@ impl Handler {
         rx.await.unwrap()
     }
 
-    pub async fn retrieve_next(&self) -> CatResponse {
+    pub async fn retrieve_next(&self) -> Requests {
         todo!()
     }
 }
