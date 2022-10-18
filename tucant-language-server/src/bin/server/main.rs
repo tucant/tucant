44e5e0863568f3621ba4d937a95ac85ccc16f943
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-10-19 00:07:17 +0200

    Remove all that boxed shit

--- tucant-language-server/src/bin/server/main.rs
+++ tucant-language-server/src/bin/server/main.rs
@@ -160,18 +160,18 @@ impl Server {
             let start_pos = error.location.start_line_column();
             let end_pos = error.location.end_line_column();
 
-            vec![Box::new(Diagnostic {
-                range: Box::new(Range {
-                    start: Box::new(Position {
+            vec![Diagnostic {
+                range: Range {
+                    start: Position {
                         line: start_pos.0.try_into().unwrap(),
                         character: start_pos.1.try_into().unwrap(),
-                    }),
-                    end: Box::new(Position {
+                    },
+                    end: Position {
                         line: end_pos.0.try_into().unwrap(),
                         character: end_pos.1.try_into().unwrap(),
-                    }),
-                }),
-                severity: Some(Box::new(DiagnosticSeverity::Error)),
+                    },
+                },
+                severity: Some(DiagnosticSeverity::Error),
                 code: None,
                 code_description: None,
                 source: Some("tucant".to_string()),
@@ -179,22 +179,18 @@ impl Server {
                 tags: None,
                 related_information: None,
                 data: None,
-            })]
+            }]
         } else {
             vec![]
         };
 
-        let response = 
-            TextDocumentPublishDiagnosticsNotification {
-                jsonrpc: "2.0".to_string(),
-                params: Box::new(PublishDiagnosticsParams {
+        let response = PublishDiagnosticsParams {
                     uri,
                     version: Some(version),
                     diagnostics,
-                }),
-            };
+        };
 
-        self.send_something::<TextDocumentPublishDiagnosticsNotification>(response).await;
+        self.send_something::<TextDocumentPublishDiagnosticsNotification>(response).await?;
 
         Ok(())
     }
@@ -240,12 +236,12 @@ impl Server {
             jsonrpc: "2.0".to_string(),
             id: request.id,
             result: Some(
-                He98ccfdc940d4c1fa4b43794669192a12c560d6457d392bc00630cb4::Variant0(Box::new(
+                He98ccfdc940d4c1fa4b43794669192a12c560d6457d392bc00630cb4::Variant0(
                     SemanticTokens {
                         result_id: None,
                         data: result,
                     },
-                )),
+                ),
             ),
         };
 
@@ -306,7 +302,7 @@ impl Server {
             .clone();
 
         for change in notification.params.content_changes.iter() {
-            match &**change {
+            match change {
         tucant_language_server_derive_output::H25fd6c7696dff041d913d0a9d3ce2232683e5362f0d4c6ca6179cf92::Variant0(incremental_changes) => {
             let start_offset = line_column_to_offset(&document, incremental_changes.range.start.line.try_into().unwrap(), incremental_changes.range.start.character.try_into().unwrap());
             let end_offset = line_column_to_offset(&document, incremental_changes.range.end.line.try_into().unwrap(), incremental_changes.range.end.character.try_into().unwrap());
@@ -327,7 +323,7 @@ impl Server {
         println!("change notification");
 
         if notification.params.content_changes.len() == 1 {
-            match &*(notification.params.content_changes[0]) {
+            match notification.params.content_changes[0] {
         tucant_language_server_derive_output::H25fd6c7696dff041d913d0a9d3ce2232683e5362f0d4c6ca6179cf92::Variant0(ref incremental_changes) => {
             let _start_offset = line_column_to_offset(&document, incremental_changes.range.start.line.try_into().unwrap(), incremental_changes.range.start.character.try_into().unwrap());
             let _end_offset = line_column_to_offset(&document, incremental_changes.range.end.line.try_into().unwrap(), incremental_changes.range.end.character.try_into().unwrap());
@@ -335,27 +331,27 @@ impl Server {
             let response = WorkspaceApplyEditRequest {
                 jsonrpc: "2.0".to_string(),
                 id: tucant_language_server_derive_output::StringOrNumber::String("1337".to_string()), // TODO FIXME
-                params: Box::new(ApplyWorkspaceEditParams {
+                params: ApplyWorkspaceEditParams {
                     label: Some("insert matching paren".to_string()),
-                    edit: Box::new(WorkspaceEdit {
+                    edit: WorkspaceEdit {
                         changes: None,
                         document_changes: Some(vec![
-                            H1332ceed95c3cca3c02eed7277ac86fcb37ac84398216e85560c37bf::Variant0(Box::new(TextDocumentEdit {
-                                text_document: Box::new(OptionalVersionedTextDocumentIdentifier {
-                                    variant0: Box::new(TextDocumentIdentifier { uri: notification.params.text_document.variant0.uri.clone() }),
+                            H1332ceed95c3cca3c02eed7277ac86fcb37ac84398216e85560c37bf::Variant0(TextDocumentEdit {
+                                text_document: OptionalVersionedTextDocumentIdentifier {
+                                    variant0: TextDocumentIdentifier { uri: notification.params.text_document.variant0.uri.clone() },
                                     version: Hf7dce6b26d9e110d906dc3150d7d569f6983091049d0e763bb4a5cec::Variant0(notification.params.text_document.version)
-                                }),
+                                },
                                 edits: vec![
-                                    Hbc05edec65fcb6ecb06a32c6c6bd742b6b3682f1da78657cd86b8f05::Variant0(Box::new(TextEdit {
-                                        range: Box::new(Range { start: Box::new(Position { line: incremental_changes.range.end.line, character: incremental_changes.range.end.character }), end: Box::new(Position { line: incremental_changes.range.end.line, character: incremental_changes.range.end.character }) }),
+                                    Hbc05edec65fcb6ecb06a32c6c6bd742b6b3682f1da78657cd86b8f05::Variant0(TextEdit {
+                                        range: Range { start: Position { line: incremental_changes.range.end.line, character: incremental_changes.range.end.character }, end: Position { line: incremental_changes.range.end.line, character: incremental_changes.range.end.character } },
                                         new_text: r#"""#.to_string()
-                                    }))
+                                    })
                                 ]
-                            }))
+                            })
                         ]),
                         change_annotations: None,
-                    }),
-                }),
+                    },
+                },
             };
 
         },
@@ -387,10 +383,10 @@ impl Server {
         let notification =
             WindowShowMessageNotification {
                 jsonrpc: "2.0".to_string(),
-                params: Box::new(ShowMessageParams {
-                    r#type: Box::new(MessageType::Error),
+                params: ShowMessageParams {
+                    r#type: MessageType::Error,
                     message: "This is a test error".to_string(),
-                }),
+                },
             };
 
         Ok(())
@@ -402,16 +398,16 @@ impl Server {
             jsonrpc: "2.0".to_string(),
             id: request.id,
             error: None,
-            result: Some(Box::new(InitializeResult {
-                capabilities: Box::new(ServerCapabilities {
+            result: Some(InitializeResult {
+                capabilities: ServerCapabilities {
                     position_encoding: None,
-                    text_document_sync: Some(H1e2267041560020dc953eb5d9d8f0c194de0f657a1193f66abeab062::Variant0(Box::new(TextDocumentSyncOptions {
+                    text_document_sync: Some(H1e2267041560020dc953eb5d9d8f0c194de0f657a1193f66abeab062::Variant0(TextDocumentSyncOptions {
                         open_close: Some(true),
                         will_save: None,
                         will_save_wait_until: None,
-                        change: Some(Box::new(TextDocumentSyncKind::Incremental)),
+                        change: Some(TextDocumentSyncKind::Incremental),
                         save: None, // TODO FIXME
-                    }))),
+                    })),
                     notebook_document_sync: None,
                     completion_provider: None,/*Some(Box::new(CompletionOptions {
                         variant0: Box::new(WorkDoneProgressOptions { work_done_progress: None }),
@@ -446,17 +442,17 @@ impl Server {
                     execute_command_provider: None,
                     call_hierarchy_provider: None,
                     linked_editing_range_provider: None,
-                    semantic_tokens_provider: Some(Hb33d389f4db33e188f5f7289bda48f700ee05a6244701313be32e552::Variant0(Box::new(SemanticTokensOptions {
-                        legend: Box::new(SemanticTokensLegend {
+                    semantic_tokens_provider: Some(Hb33d389f4db33e188f5f7289bda48f700ee05a6244701313be32e552::Variant0(SemanticTokensOptions {
+                        legend: SemanticTokensLegend {
                             token_types: vec!["string".to_string(), "number".to_string(), "type".to_string()],
                             token_modifiers: vec![],
-                        }),
-                        variant0: Box::new(WorkDoneProgressOptions {
+                        },
+                        variant0: WorkDoneProgressOptions {
                             work_done_progress: None,
-                        }),
+                        },
                         range: Some(H3424688d17603d45dbf7bc9bc9337e660ef00dd90b070777859fbf1e::Variant0(false)),
                         full: Some(H560683c9a528918bcd8e6562ca5d336a5b02f2a471cc7f47a6952222::Variant0(true)),
-                    }))),
+                    })),
                     moniker_provider: None,
                     type_hierarchy_provider: None,
                     inline_value_provider: None,
@@ -464,9 +460,9 @@ impl Server {
                     diagnostic_provider: None,
                     workspace: None,
                     experimental: None
-                }),
+                },
                 server_info: Some(H07206713e0ac2e546d7755e84916a71622d6302f44063c913d615b41 { name: "TUCaN't".to_string(), version: Some("0.0.1".to_string()) })
-            }))
+            })
         };
 
         Ok(())
