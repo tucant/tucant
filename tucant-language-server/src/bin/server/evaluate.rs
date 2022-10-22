306b25a84879502640da7a3e9d4c83a8b7bcc53e
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-10-23 01:13:24 +0200

    Lifetime juggling

--- tucant-language-server/src/bin/server/evaluate.rs
+++ tucant-language-server/src/bin/server/evaluate.rs
@@ -3,8 +3,8 @@ use std::fmt::Debug;
 use crate::parser::{Span, Ast, parse_root};
 use anyhow::anyhow;
 
-pub trait Object: Debug {
-    fn call<'a>(&self, args: &[Span<'a, Ast<'a>>]) -> anyhow::Result<Rc<dyn Object + 'a>> {
+pub trait Object<'a>: Debug {
+    fn call(&self, context: &mut Vec<(String, Rc<dyn Object<'a> + 'a>)>, args: &[Span<'a, Ast<'a>>]) -> anyhow::Result<Rc<dyn Object<'a> + 'a>> {
         Err(anyhow!("not yet implemented"))
     }
 }
@@ -12,14 +12,14 @@ pub trait Object: Debug {
 #[derive(Debug)]
 pub struct IntegerType(i64);
 
-impl Object for IntegerType {
+impl<'a> Object<'a> for IntegerType {
 
 }
 
 #[derive(Debug)]
 pub struct StringType(String);
 
-impl Object for StringType {
+impl<'a> Object<'a> for StringType {
 }
 
 #[derive(Debug)]
@@ -28,15 +28,22 @@ pub struct Lambda<'a> {
     body: Span<'a, Ast<'a>>
 }
 
-impl<'a> Object for Lambda<'a> {
-    
+impl<'a> Object<'a> for Lambda<'a> {
+    fn call(&self, context: &mut Vec<(String, Rc<dyn Object<'a> + 'a>)>, args: &[Span<'a, Ast<'a>>]) -> anyhow::Result<Rc<dyn Object<'a> + 'a>> {
+        let [variable_value]: &[Span<'a, Ast<'a>>; 1] = args.try_into()?;
+        let arg_value = evaluate_with_context(context, variable_value.clone())?;
+        context.push((self.variable.clone(), arg_value));
+        let return_value = evaluate_with_context(context, self.body.clone());
+        context.pop();
+        return_value
+    }
 }
 
 #[derive(Debug)]
 pub struct DefineLambda;
 
-impl Object for DefineLambda {
-    fn call<'a>(&self, args: &[Span<'a, Ast<'a>>]) -> anyhow::Result<Rc<dyn Object + 'a>> {
+impl<'a> Object<'a> for DefineLambda {
+    fn call(&self, context: &mut Vec<(String, Rc<dyn Object<'a> + 'a>)>, args: &[Span<'a, Ast<'a>>]) -> anyhow::Result<Rc<dyn Object<'a> + 'a>> {
         let [variable, body]: &[Span<'a, Ast<'a>>; 2] = args.try_into()?;
         let variable = match variable.inner {
             Ast::Identifier(identifier) => identifier,
@@ -49,18 +56,18 @@ impl Object for DefineLambda {
     }
 }
 
-pub fn evaluate<'a>(value: Span<'a, Ast<'a>>) -> anyhow::Result<Rc<dyn Object + 'a>> {
-    let context: Vec<(String, Rc<dyn Object>)> = vec![
+pub fn evaluate<'a>(value: Span<'a, Ast<'a>>) -> anyhow::Result<Rc<dyn Object<'a> + 'a>> {
+    let mut context: Vec<(String, Rc<dyn Object>)> = vec![
         ("lambda".to_string(), Rc::new(DefineLambda))
     ];
-    evaluate_with_context(context, value)
+    evaluate_with_context(&mut context, value)
 }
 
-fn resolve_identifier<'a>(context: Vec<(String, Rc<dyn Object + 'a>)>, identifier: &str) -> anyhow::Result<Rc<dyn Object + 'a>> {
+fn resolve_identifier<'a>(context: &mut Vec<(String, Rc<dyn Object<'a> + 'a>)>, identifier: &str) -> anyhow::Result<Rc<dyn Object<'a> + 'a>> {
     context.iter().rev().find(|(ident, _)| identifier == ident).map(|(ident, value)| value).ok_or(anyhow!("could not find identifier {}", identifier)).cloned()
 }
 
-pub fn evaluate_with_context<'a>(context: Vec<(String, Rc<dyn Object + 'a>)>, value: Span<'a, Ast<'a>>) -> anyhow::Result<Rc<dyn Object + 'a>> {
+pub fn evaluate_with_context<'a>(context: &mut Vec<(String, Rc<dyn Object<'a> + 'a>)>, value: Span<'a, Ast<'a>>) -> anyhow::Result<Rc<dyn Object<'a> + 'a>> {
     match value.inner {
         Ast::Number(number) => Ok(Rc::new(IntegerType(number))),
         Ast::String(string) => Ok(Rc::new(StringType(string.to_string()))),
@@ -72,7 +79,7 @@ pub fn evaluate_with_context<'a>(context: Vec<(String, Rc<dyn Object + 'a>)>, va
                 Ast::List(_) => evaluate_with_context(context, callable.clone()),
                 _ => Err(anyhow!("can't call a string or number")),
             };
-            callable?.call(args)
+            callable?.call(context, args)
         },
     }
 }
@@ -105,6 +112,11 @@ fn test_primitives() {
 
     let result = evaluate(parse_root(Span::new(r#"
         (lambda 1 v)
+    "#)).unwrap().0);
+    println!("{:?}", result);
+
+    let result = evaluate(parse_root(Span::new(r#"
+        ((lambda v v) 1)
     "#)).unwrap().0);
     println!("{:?}", result);
 }
