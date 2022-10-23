36c044f0ae897c2d27f9b2a6511353a557d6ccb6
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-10-23 23:26:32 +0200

    Idea

--- tucant-language-server/src/bin/server/evaluate.rs
+++ tucant-language-server/src/bin/server/evaluate.rs
@@ -3,23 +3,58 @@ use std::fmt::Debug;
 use crate::parser::{Span, Ast, parse_root};
 use anyhow::anyhow;
 
-pub trait Object<'a>: Debug {
-    fn call(&self, context: &mut Vec<(String, Rc<dyn Object<'a> + 'a>)>, args: &[Span<'a, Ast<'a>>]) -> anyhow::Result<Rc<dyn Object<'a> + 'a>> {
+pub trait Value<'a>: Debug {
+    fn evaluate_call(self: Rc<Self>, context: &mut Vec<(String, Rc<dyn Value<'a> + 'a>)>, args: &[Span<'a, Ast<'a>>]) -> anyhow::Result<Rc<dyn Value<'a> + 'a>> {
+        Err(anyhow!("not yet implemented"))
+    }
+}
+
+pub trait Type<'a>: Debug {
+    fn typecheck_call(self: Rc<Self>, context: &mut Vec<(String, Rc<dyn Type<'a> + 'a>)>, args: &[Span<'a, Ast<'a>>]) -> anyhow::Result<Rc<dyn Type<'a> + 'a>> {
         Err(anyhow!("not yet implemented"))
     }
 }
 
 #[derive(Debug)]
-pub struct IntegerType(i64);
+pub struct IntegerValue(i64);
 
-impl<'a> Object<'a> for IntegerType {
+impl<'a> Value<'a> for IntegerValue {
+   
+}
 
+
+#[derive(Debug)]
+pub struct IntegerType(Option<i64>);
+
+impl<'a> Type<'a> for IntegerType {
 }
 
 #[derive(Debug)]
-pub struct StringType(String);
+pub struct StringValue(String);
 
-impl<'a> Object<'a> for StringType {
+impl<'a> Value<'a> for StringValue {
+}
+
+
+#[derive(Debug)]
+pub struct StringType(Option<String>);
+
+impl<'a> Type<'a> for StringType {
+}
+
+
+#[derive(Debug)]
+pub struct Add;
+
+impl<'a> Value<'a> for Add {
+    fn evaluate_call(self: Rc<Self>, context: &mut Vec<(String, Rc<dyn Value<'a> + 'a>)>, args: &[Span<'a, Ast<'a>>]) -> anyhow::Result<Rc<dyn Value<'a> + 'a>> {
+        let [left, right]: &[Span<'a, Ast<'a>>; 2] = args.try_into()?;
+        let left_value = evaluate_with_context(context, left.clone())?;
+        let right_value = evaluate_with_context(context, right.clone())?;
+        let return_value = evaluate_with_context(context, self.body.clone());
+        return_value
+    }
+
 }
 
 #[derive(Debug)]
@@ -28,8 +63,8 @@ pub struct Lambda<'a> {
     body: Span<'a, Ast<'a>>
 }
 
-impl<'a> Object<'a> for Lambda<'a> {
-    fn call(&self, context: &mut Vec<(String, Rc<dyn Object<'a> + 'a>)>, args: &[Span<'a, Ast<'a>>]) -> anyhow::Result<Rc<dyn Object<'a> + 'a>> {
+impl<'a> Value<'a> for Lambda<'a> {
+    fn evaluate_call(self: Rc<Self>, context: &mut Vec<(String, Rc<dyn Value<'a> + 'a>)>, args: &[Span<'a, Ast<'a>>]) -> anyhow::Result<Rc<dyn Value<'a> + 'a>> {
         let [variable_value]: &[Span<'a, Ast<'a>>; 1] = args.try_into()?;
         let arg_value = evaluate_with_context(context, variable_value.clone())?;
         context.push((self.variable.clone(), arg_value));
@@ -42,8 +77,8 @@ impl<'a> Object<'a> for Lambda<'a> {
 #[derive(Debug)]
 pub struct DefineLambda;
 
-impl<'a> Object<'a> for DefineLambda {
-    fn call(&self, context: &mut Vec<(String, Rc<dyn Object<'a> + 'a>)>, args: &[Span<'a, Ast<'a>>]) -> anyhow::Result<Rc<dyn Object<'a> + 'a>> {
+impl<'a> Value<'a> for DefineLambda {
+    fn evaluate_call(self: Rc<Self>, context: &mut Vec<(String, Rc<dyn Value<'a> + 'a>)>, args: &[Span<'a, Ast<'a>>]) -> anyhow::Result<Rc<dyn Value<'a> + 'a>> {
         let [variable, body]: &[Span<'a, Ast<'a>>; 2] = args.try_into()?;
         let variable = match variable.inner {
             Ast::Identifier(identifier) => identifier,
@@ -56,21 +91,45 @@ impl<'a> Object<'a> for DefineLambda {
     }
 }
 
-pub fn evaluate<'a>(value: Span<'a, Ast<'a>>) -> anyhow::Result<Rc<dyn Object<'a> + 'a>> {
-    let mut context: Vec<(String, Rc<dyn Object>)> = vec![
+pub fn evaluate<'a>(value: Span<'a, Ast<'a>>) -> anyhow::Result<Rc<dyn Value<'a> + 'a>> {
+    let mut context: Vec<(String, Rc<dyn Value>)> = vec![
         ("lambda".to_string(), Rc::new(DefineLambda))
     ];
     evaluate_with_context(&mut context, value)
 }
 
-fn resolve_identifier<'a>(context: &mut Vec<(String, Rc<dyn Object<'a> + 'a>)>, identifier: &str) -> anyhow::Result<Rc<dyn Object<'a> + 'a>> {
+pub fn typecheck<'a>(value: Span<'a, Ast<'a>>) -> anyhow::Result<Rc<dyn Value<'a> + 'a>> {
+    let mut context: Vec<(String, Rc<dyn Value>)> = vec![
+        ("lambda".to_string(), Rc::new(DefineLambda))
+    ];
+    evaluate_with_context(&mut context, value)
+}
+
+fn resolve_identifier<'a>(context: &mut Vec<(String, Rc<dyn Value<'a> + 'a>)>, identifier: &str) -> anyhow::Result<Rc<dyn Value<'a> + 'a>> {
     context.iter().rev().find(|(ident, _)| identifier == ident).map(|(ident, value)| value).ok_or(anyhow!("could not find identifier {}", identifier)).cloned()
 }
 
-pub fn evaluate_with_context<'a>(context: &mut Vec<(String, Rc<dyn Object<'a> + 'a>)>, value: Span<'a, Ast<'a>>) -> anyhow::Result<Rc<dyn Object<'a> + 'a>> {
+pub fn typecheck_with_context<'a>(context: &mut Vec<(String, Rc<dyn Type<'a> + 'a>)>, _type: Span<'a, Ast<'a>>) -> anyhow::Result<Rc<dyn Type<'a> + 'a>> {
+    match _type.inner {
+        Ast::Number(number) => Ok(Rc::new(IntegerType(Some(number)))),
+        Ast::String(string) => Ok(Rc::new(StringType(Some(string.to_string())))),
+        Ast::Identifier(identifier) => resolve_identifier(context, identifier),
+        Ast::List(elements) => {
+            let (callable, args) = elements.split_first().ok_or(anyhow!("can't call empty list"))?;
+            let callable = match callable.inner {
+                Ast::Identifier(identifier) => resolve_identifier(context, identifier),
+                Ast::List(_) => typecheck_with_context(context, callable.clone()),
+                _ => Err(anyhow!("can't call a string or number")),
+            };
+            callable?.call(context, args)
+        },
+    }
+}
+
+pub fn evaluate_with_context<'a>(context: &mut Vec<(String, Rc<dyn Value<'a> + 'a>)>, value: Span<'a, Ast<'a>>) -> anyhow::Result<Rc<dyn Value<'a> + 'a>> {
     match value.inner {
-        Ast::Number(number) => Ok(Rc::new(IntegerType(number))),
-        Ast::String(string) => Ok(Rc::new(StringType(string.to_string()))),
+        Ast::Number(number) => Ok(Rc::new(IntegerValue(number))),
+        Ast::String(string) => Ok(Rc::new(StringValue(string.to_string()))),
         Ast::Identifier(identifier) => resolve_identifier(context, identifier),
         Ast::List(elements) => {
             let (callable, args) = elements.split_first().ok_or(anyhow!("can't call empty list"))?;
