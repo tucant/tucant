ffdc444ee69b85f5ed205102f6ac537298547ea9
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-10-24 00:46:30 +0200

    Jo

--- tucant-language-server/src/bin/server/evaluate.rs
+++ tucant-language-server/src/bin/server/evaluate.rs
@@ -1,11 +1,20 @@
+use std::borrow::Cow;
 use std::{rc::Rc, any::Any};
 use std::fmt::Debug;
 use crate::parser::{Span, Ast, parse_root};
 use anyhow::anyhow;
 
+#[derive(Debug)]
+pub struct EvaluateError<'a> {
+    pub location: Option<Span<'a, ()>>,
+    pub reason: Cow<'static, str>,
+}
+
+pub type EvaluateResult<'a, V> = Result<V, EvaluateError<'a>>;
+
 pub trait Value<'a>: Debug {
-    fn evaluate_call(self: Rc<Self>, context: &mut Vec<(String, Rc<dyn Value<'a> + 'a>)>, args: &[Span<'a, Ast<'a>>]) -> anyhow::Result<Rc<dyn Value<'a> + 'a>> {
-        Err(anyhow!("not yet implemented"))
+    fn evaluate_call(self: Rc<Self>, context: &mut Vec<(String, Rc<dyn Value<'a> + 'a>)>, args: &[Span<'a, Ast<'a>>]) -> EvaluateResult<'a, Rc<dyn Value<'a> + 'a>> {
+        Err(EvaluateError { location: None, reason: format!("not yet implemented").into() }) // TODO FIXME add span information
     }
 
     fn downcast_integer_value(&self) -> Option<&IntegerValue> {
@@ -14,8 +23,8 @@ pub trait Value<'a>: Debug {
 }
 
 pub trait Type<'a>: Debug {
-    fn typecheck_call(self: Rc<Self>, context: &mut Vec<(String, Rc<dyn Type<'a> + 'a>)>, args: &[Span<'a, Ast<'a>>]) -> anyhow::Result<Rc<dyn Type<'a> + 'a>> {
-        Err(anyhow!("not yet implemented"))
+    fn typecheck_call(self: Rc<Self>, context: &mut Vec<(String, Rc<dyn Type<'a> + 'a>)>, args: &[Span<'a, Ast<'a>>]) -> EvaluateResult<'a, Rc<dyn Type<'a> + 'a>> {
+        Err(EvaluateError { location: None, reason: format!("not yet implemented").into() })
     }
 
     fn downcast_integer_type(&self) -> Option<&IntegerType> {
@@ -60,13 +69,13 @@ impl<'a> Type<'a> for StringType {
 pub struct AddLambdaValue;
 
 impl<'a> Value<'a> for AddLambdaValue {
-    fn evaluate_call(self: Rc<Self>, context: &mut Vec<(String, Rc<dyn Value<'a> + 'a>)>, args: &[Span<'a, Ast<'a>>]) -> anyhow::Result<Rc<dyn Value<'a> + 'a>> {
-        let [left, right]: &[Span<'a, Ast<'a>>; 2] = args.try_into()?;
+    fn evaluate_call(self: Rc<Self>, context: &mut Vec<(String, Rc<dyn Value<'a> + 'a>)>, args: &[Span<'a, Ast<'a>>]) -> EvaluateResult<'a, Rc<dyn Value<'a> + 'a>> {
+        let [left, right]: &[Span<'a, Ast<'a>>; 2] = args.try_into().or(Err(EvaluateError { location: None, reason: format!("expected exactly two arguments").into() }))?;
         let left_value = evaluate_with_context(context, left.clone())?;
         let right_value = evaluate_with_context(context, right.clone())?;
-        let left_value = left_value.downcast_integer_value().ok_or(anyhow!("expected integer type, got {:?}", left_value))?;
-        let right_value = right_value.downcast_integer_value().ok_or(anyhow!("expected integer type, got {:?}", right_value))?;
-        Ok(Rc::new(IntegerValue(left_value.0.checked_add(right_value.0).ok_or(anyhow!("integer overflow, adding {:?} and {:?}", left_value, right_value))?)))
+        let left_value = left_value.downcast_integer_value().ok_or(EvaluateError { location: None, reason: format!("expected integer type, got {:?}", left_value).into()})?;
+        let right_value = right_value.downcast_integer_value().ok_or(EvaluateError { location: None, reason: format!("expected integer type, got {:?}", right_value).into()})?;
+        Ok(Rc::new(IntegerValue(left_value.0.checked_add(right_value.0).ok_or(EvaluateError { location: None, reason: format!("integer overflow, adding {:?} and {:?}", left_value, right_value).into()})?)))
     }
 }
 
@@ -75,13 +84,13 @@ impl<'a> Value<'a> for AddLambdaValue {
 pub struct AddLambdaType;
 
 impl<'a> Type<'a> for AddLambdaType {
-    fn typecheck_call(self: Rc<Self>, context: &mut Vec<(String, Rc<dyn Type<'a> + 'a>)>, args: &[Span<'a, Ast<'a>>]) -> anyhow::Result<Rc<dyn Type<'a> + 'a>> {
-        let [left, right]: &[Span<'a, Ast<'a>>; 2] = args.try_into()?;
+    fn typecheck_call(self: Rc<Self>, context: &mut Vec<(String, Rc<dyn Type<'a> + 'a>)>, args: &[Span<'a, Ast<'a>>]) -> EvaluateResult<'a, Rc<dyn Type<'a> + 'a>> {
+        let [left, right]: &[Span<'a, Ast<'a>>; 2] = args.or(Err(EvaluateError { location: None, reason: format!("expected exactly two arguments").into() }))?;
         let left_value = typecheck_with_context(context, left.clone())?;
         let right_value = typecheck_with_context(context, right.clone())?;
-        let left_value = left_value.downcast_integer_type().ok_or(anyhow!("expected integer type, got {:?}", left_value))?;
-        let right_value = right_value.downcast_integer_type().ok_or(anyhow!("expected integer type, got {:?}", right_value))?;
-        Ok(Rc::new(IntegerType(left_value.0.and_then(|l| right_value.0.map(|r| l.checked_add(r).ok_or(anyhow!("integer overflow, adding {:?} and {:?}", left_value, right_value)))).transpose()?)))
+        let left_value = left_value.downcast_integer_type().ok_or(EvaluateError { location: None, reason: format!("expected integer type, got {:?}", left_value).into()})?;
+        let right_value = right_value.downcast_integer_type().ok_or(EvaluateError { location: None, reason: format!("expected integer type, got {:?}", right_value).into()})?;
+        Ok(Rc::new(IntegerType(left_value.0.and_then(|l| right_value.0.map(|r| l.checked_add(r).ok_or(EvaluateError { location: None, reason: format!("integer overflow, adding {:?} and {:?}", left_value, right_value).into()}))).transpose()?)))
     }
 }
 
@@ -92,7 +101,7 @@ pub struct LambdaValue<'a> {
 }
 
 impl<'a> Value<'a> for LambdaValue<'a> {
-    fn evaluate_call(self: Rc<Self>, context: &mut Vec<(String, Rc<dyn Value<'a> + 'a>)>, args: &[Span<'a, Ast<'a>>]) -> anyhow::Result<Rc<dyn Value<'a> + 'a>> {
+    fn evaluate_call(self: Rc<Self>, context: &mut Vec<(String, Rc<dyn Value<'a> + 'a>)>, args: &[Span<'a, Ast<'a>>]) -> EvaluateResult<'a, Rc<dyn Value<'a> + 'a>> {
         let [variable_value]: &[Span<'a, Ast<'a>>; 1] = args.try_into()?;
         let arg_value = evaluate_with_context(context, variable_value.clone())?;
         context.push((self.variable.clone(), arg_value));
@@ -109,7 +118,7 @@ pub struct LambdaType<'a> {
 }
 
 impl<'a> Type<'a> for LambdaType<'a> {
-    fn typecheck_call(self: Rc<Self>, context: &mut Vec<(String, Rc<dyn Type<'a> + 'a>)>, args: &[Span<'a, Ast<'a>>]) -> anyhow::Result<Rc<dyn Type<'a> + 'a>> {
+    fn typecheck_call(self: Rc<Self>, context: &mut Vec<(String, Rc<dyn Type<'a> + 'a>)>, args: &[Span<'a, Ast<'a>>]) -> EvaluateResult<'a, Rc<dyn Type<'a> + 'a>> {
         let [variable_value]: &[Span<'a, Ast<'a>>; 1] = args.try_into()?;
         let arg_value = typecheck_with_context(context, variable_value.clone())?;
         context.push((self.variable.clone(), arg_value));
@@ -123,7 +132,7 @@ impl<'a> Type<'a> for LambdaType<'a> {
 pub struct DefineLambdaValue;
 
 impl<'a> Value<'a> for DefineLambdaValue {
-    fn evaluate_call(self: Rc<Self>, context: &mut Vec<(String, Rc<dyn Value<'a> + 'a>)>, args: &[Span<'a, Ast<'a>>]) -> anyhow::Result<Rc<dyn Value<'a> + 'a>> {
+    fn evaluate_call(self: Rc<Self>, context: &mut Vec<(String, Rc<dyn Value<'a> + 'a>)>, args: &[Span<'a, Ast<'a>>]) -> EvaluateResult<'a, Rc<dyn Value<'a> + 'a>> {
         let [variable, body]: &[Span<'a, Ast<'a>>; 2] = args.try_into()?;
         let variable = match variable.inner {
             Ast::Identifier(identifier) => identifier,
@@ -140,7 +149,7 @@ impl<'a> Value<'a> for DefineLambdaValue {
 pub struct DefineLambdaType;
 
 impl<'a> Type<'a> for DefineLambdaType {
-    fn typecheck_call(self: Rc<Self>, context: &mut Vec<(String, Rc<dyn Type<'a> + 'a>)>, args: &[Span<'a, Ast<'a>>]) -> anyhow::Result<Rc<dyn Type<'a> + 'a>> {
+    fn typecheck_call(self: Rc<Self>, context: &mut Vec<(String, Rc<dyn Type<'a> + 'a>)>, args: &[Span<'a, Ast<'a>>]) -> EvaluateResult<'a, Rc<dyn Type<'a> + 'a>> {
         let [variable, body]: &[Span<'a, Ast<'a>>; 2] = args.try_into()?;
         let variable = match variable.inner {
             Ast::Identifier(identifier) => identifier,
@@ -153,7 +162,7 @@ impl<'a> Type<'a> for DefineLambdaType {
     }
 }
 
-pub fn evaluate<'a>(value: Span<'a, Ast<'a>>) -> anyhow::Result<Rc<dyn Value<'a> + 'a>> {
+pub fn evaluate<'a>(value: Span<'a, Ast<'a>>) -> EvaluateResult<'a, Rc<dyn Value<'a> + 'a>> {
     let mut context: Vec<(String, Rc<dyn Value>)> = vec![
         ("lambda".to_string(), Rc::new(DefineLambdaValue)),
         ("add".to_string(), Rc::new(AddLambdaValue)),
@@ -161,7 +170,7 @@ pub fn evaluate<'a>(value: Span<'a, Ast<'a>>) -> anyhow::Result<Rc<dyn Value<'a>
     evaluate_with_context(&mut context, value)
 }
 
-pub fn typecheck<'a>(value: Span<'a, Ast<'a>>) -> anyhow::Result<Rc<dyn Type<'a> + 'a>> {
+pub fn typecheck<'a>(value: Span<'a, Ast<'a>>) -> EvaluateResult<'a, Rc<dyn Type<'a> + 'a>> {
     let mut context: Vec<(String, Rc<dyn Type>)> = vec![
         ("lambda".to_string(), Rc::new(DefineLambdaType)),
         ("add".to_string(), Rc::new(AddLambdaType)),
@@ -169,11 +178,11 @@ pub fn typecheck<'a>(value: Span<'a, Ast<'a>>) -> anyhow::Result<Rc<dyn Type<'a>
     typecheck_with_context(&mut context, value)
 }
 
-fn resolve_identifier<'a, T: Clone>(context: &mut Vec<(String, T)>, identifier: &str) -> anyhow::Result<T> {
+fn resolve_identifier<'a, T: Clone>(context: &mut Vec<(String, T)>, identifier: &str) -> EvaluateResult<'a, T> {
     context.iter().rev().find(|(ident, _)| identifier == ident).map(|(ident, value)| value).ok_or(anyhow!("could not find identifier {}", identifier)).cloned()
 }
 
-pub fn typecheck_with_context<'a>(context: &mut Vec<(String, Rc<dyn Type<'a> + 'a>)>, _type: Span<'a, Ast<'a>>) -> anyhow::Result<Rc<dyn Type<'a> + 'a>> {
+pub fn typecheck_with_context<'a>(context: &mut Vec<(String, Rc<dyn Type<'a> + 'a>)>, _type: Span<'a, Ast<'a>>) -> EvaluateResult<'a, Rc<dyn Type<'a> + 'a>> {
     match _type.inner {
         Ast::Number(number) => Ok(Rc::new(IntegerType(Some(number)))),
         Ast::String(string) => Ok(Rc::new(StringType(Some(string.to_string())))),
@@ -190,7 +199,7 @@ pub fn typecheck_with_context<'a>(context: &mut Vec<(String, Rc<dyn Type<'a> + '
     }
 }
 
-pub fn evaluate_with_context<'a>(context: &mut Vec<(String, Rc<dyn Value<'a> + 'a>)>, value: Span<'a, Ast<'a>>) -> anyhow::Result<Rc<dyn Value<'a> + 'a>> {
+pub fn evaluate_with_context<'a>(context: &mut Vec<(String, Rc<dyn Value<'a> + 'a>)>, value: Span<'a, Ast<'a>>) -> EvaluateResult<'a, Rc<dyn Value<'a> + 'a>> {
     match value.inner {
         Ast::Number(number) => Ok(Rc::new(IntegerValue(number))),
         Ast::String(string) => Ok(Rc::new(StringValue(string.to_string()))),
