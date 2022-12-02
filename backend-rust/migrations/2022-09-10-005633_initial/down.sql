2036b19699c0be4f88bf0b7b73c1d55bb64ed2b0
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-12-02 23:39:51 +0100

    Database design idea

--- backend-rust/migrations/2022-09-10-005633_initial/down.sql
+++ backend-rust/migrations/2022-09-10-005633_initial/down.sql
@@ -2,21 +2,25 @@
 --
 -- SPDX-License-Identifier: AGPL-3.0-or-later
 
-DROP INDEX courses_idx;
-DROP INDEX modules_idx;
-
+DROP TABLE user_exams;
+DROP TABLE course_exams;
+DROP TABLE module_exams;
+DROP TABLE exams;
+DROP TABLE user_courses;
+DROP TABLE user_modules;
+DROP TABLE module_courses;
 DROP TABLE course_groups_events;
 DROP TABLE course_events;
 DROP TABLE course_groups_unfinished;
-
-DROP TABLE module_courses;
+DROP INDEX courses_idx;
 DROP TABLE courses_unfinished;
-DROP TABLE users_studies;
-DROP TABLE users;
+DROP TABLE sessions;
+DROP TABLE users_unfinished;
 DROP TABLE module_menu_module;
+DROP INDEX module_menu_unfinished_parent;
 DROP TABLE module_menu_unfinished;
+DROP INDEX modules_idx;
 DROP TABLE modules_unfinished;
-
 DROP TEXT SEARCH CONFIGURATION tucan;
 DROP TEXT SEARCH DICTIONARY english_hunspell;
 DROP TEXT SEARCH DICTIONARY german_hunspell;
