8f9598f4e1dbe10a84457dcd602e98d496abf7c8
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-12-12 01:01:03 +0100

    Jojo

--- frontend-react/vite.config.ts
+++ frontend-react/vite.config.ts
@@ -3,17 +3,21 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react({
-      //fastRefresh: false,
+    visualizer({
+      brotliSize: true,
+      gzipSize: true,
+      template: "sunburst",
+      open: true
     }),
-    visualizer({}),
   ],
+  build: {
+    sourcemap: true
+  },
   server: {
     // https://github.com/vitejs/vite/issues/3301
     proxy: {
@@ -21,8 +25,3 @@ export default defineConfig({
     },
   },
 });
-/*
-✓ 1008 modules transformed.
-dist/index.html                 0.76 KiB
-dist/assets/index.f34a4907.js   426.20 KiB / gzip: 137.37 KiB
-*/
