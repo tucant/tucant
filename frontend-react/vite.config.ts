f8406b1c301d8535aae62eba6142e5265338085e
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-12-27 22:19:39 +0100

    Fix vite config for react

--- frontend-react/vite.config.ts
+++ frontend-react/vite.config.ts
@@ -4,10 +4,12 @@
 
 import { defineConfig } from "vite";
 import { visualizer } from "rollup-plugin-visualizer";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    react(),
     visualizer({
       brotliSize: true,
       gzipSize: true,
