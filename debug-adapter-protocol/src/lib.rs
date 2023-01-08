06fd47db8ac5bf317430d40f3f2dc814a8dc5399
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2023-01-08 23:24:38 +0100

    Parser seems to work

--- debug-adapter-protocol/src/lib.rs
+++ debug-adapter-protocol/src/lib.rs
@@ -1,10 +1,66 @@
-mod debugAdapterProtocol;
-use crate::debugAdapterProtocol::get_debug_adapter_protocol_json;
-use syn::LitStr;
+use syn::punctuated::Punctuated;
+use syn::token::Colon;
+//mod debugAdapterProtocol;
+//use crate::debugAdapterProtocol::get_debug_adapter_protocol_json;
+use syn::parse::Parse;
+use syn::{braced, bracketed, token, LitInt, LitStr, Token};
+
+#[derive(Debug)]
+pub enum JSONValue {
+    String(LitStr),
+    Integer(LitInt),
+    Array((token::Bracket, Punctuated<JSONValue, token::Comma>)),
+    Object((token::Brace, Punctuated<KeyValue, token::Comma>)),
+}
+
+#[derive(Debug)]
+pub struct KeyValue {
+    key: LitStr,
+    colon: Token![:],
+    value: JSONValue,
+}
+
+impl Parse for KeyValue {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        Ok(Self {
+            key: input.parse()?,
+            colon: input.parse()?,
+            value: input.parse()?,
+        })
+    }
+}
+
+impl Parse for JSONValue {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        let lookahead = input.lookahead1();
+        if lookahead.peek(token::Brace) {
+            let content;
+            let brace = braced!(content in input);
+            Ok(JSONValue::Object((
+                brace,
+                content.parse_terminated(KeyValue::parse)?,
+            )))
+        } else if lookahead.peek(token::Bracket) {
+            let content;
+            let bracket = bracketed!(content in input);
+            Ok(JSONValue::Array((
+                bracket,
+                content.parse_terminated(JSONValue::parse)?,
+            )))
+        } else if lookahead.peek(LitStr) {
+            input.parse().map(Self::String)
+        } else if lookahead.peek(LitInt) {
+            input.parse().map(Self::Integer)
+        } else {
+            Err(lookahead.error())
+        }
+    }
+}
 
 pub fn parse() -> Result<(), syn::Error> {
-    let json = get_debug_adapter_protocol_json();
-    let json_value: LitStr = syn::parse2(quote::quote! { "hello" "world" })?;
+    //let json = get_debug_adapter_protocol_json();
+    let json_value: JSONValue = syn::parse2(quote::quote! { { "hello": { "test": ["world"] } } })?;
+    println!("{:#?}", json_value);
     Ok(())
 }
 
@@ -14,6 +70,6 @@ mod tests {
 
     #[test]
     fn it_works() {
-        parse().unwrap();
+        parse().unwrap()
     }
 }
