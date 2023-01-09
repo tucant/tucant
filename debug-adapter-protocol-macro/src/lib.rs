7d701a84a93368544410ab8d742d83a7a8c177fe
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2023-01-09 02:01:33 +0100

    Jo

--- debug-adapter-protocol-macro/src/lib.rs
+++ debug-adapter-protocol-macro/src/lib.rs
@@ -8,6 +8,10 @@
 
 // https://github.com/rust-lang/rfcs/pull/3200
 // https://github.com/rust-lang/rust/pull/82682
+// https://github.com/rust-lang/rustc-dev-guide/blob/master/src/diagnostics.md
+// https://users.rust-lang.org/t/how-to-provide-quickfix-recommendations-in-proc-macros/61582/1
+// https://internals.rust-lang.org/t/user-defined-quick-fixes/14926
+// https://github.com/rust-lang/rust/issues/54140#issuecomment-802701867
 
 use debug_adapter_protocol_macro_impl::{JSONSchema, JSONValue};
 use proc_macro::Span;
@@ -24,7 +28,7 @@ fn debug_adapter_protocol_macro_impl(
 
 #[proc_macro]
 pub fn debug_adapter_protocol_macro(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    Span::call_site().help("remove this").emit();
+    Span::call_site().help(r#"fdsf"#).emit();
 
     let input = parse_macro_input!(input as JSONValue);
 
