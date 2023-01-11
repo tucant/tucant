6320b08be68c954cff1cc68db8ee1f5514dd12e3
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2023-01-11 21:04:23 +0100

    Start generating code

--- debug-adapter-protocol-macro-impl/src/json_schema_codegen.rs
+++ debug-adapter-protocol-macro-impl/src/json_schema_codegen.rs
@@ -6,12 +6,95 @@ use crate::json_schema::JSONSchema;
 #[must_use]
 pub fn codegen(schema: JSONSchema) -> proc_macro2::TokenStream {
     let definitions = schema.definitions.into_iter().map(|definition| {
-        let name = format_ident!("{}", definition.key.value());
+        let name = format_ident!("r#{}", definition.key.value());
         let name = quote_spanned!(definition.key.span() => #name);
-        quote! {
-            pub struct #name {
+        let description = definition.value.description.map(|d| quote! {
+            #[doc = #d]
+        });
+        let title = definition.value.title.map(|t| quote! {
+            #[doc = #t]
+        });
+        let result = match definition.value.definition_type {
+            crate::json_schema::DefinitionType::AllOf(t) => {
 
-            }
+                quote! {
+                    pub struct #name {
+
+                    }
+                }
+            },
+            crate::json_schema::DefinitionType::OneOf(t) => {
+
+                quote! {
+                    pub struct #name {
+
+                    }
+                }
+            },
+            crate::json_schema::DefinitionType::Ref(t) => {
+
+                quote! {
+                    pub struct #name {
+
+                    }
+                }
+            },
+            crate::json_schema::DefinitionType::ObjectType(t) => {
+                let properties = t.properties.into_iter().map(|p| {
+                    let name = format_ident!("r#{}", p.key.value());
+                    let name = quote_spanned!(p.key.span() => #name);
+                    quote! {
+                        #name: ()
+                    }
+                });
+                quote! {
+                    pub struct #name {
+                        #(#properties),*
+                    }
+                }
+            },
+            crate::json_schema::DefinitionType::StringType(t) => {
+
+                quote! {
+                    pub struct #name {
+
+                    }
+                }
+            },
+            crate::json_schema::DefinitionType::ArrayType(t) => {
+
+                quote! {
+                    pub struct #name {
+
+                    }
+                }
+            },
+            crate::json_schema::DefinitionType::IntegerType(t) => {
+
+                quote! {
+                    pub struct #name {
+
+                    }
+                }
+            },
+            crate::json_schema::DefinitionType::DoubleType(t) => {
+
+                quote! {
+                    pub struct #name {
+
+                    }
+                }
+            },
+            crate::json_schema::DefinitionType::BooleanType(t) => {
+                quote! {
+                    type #name = bool
+                }
+            },
+        };
+        quote! {
+            #title
+            #description
+            #result
         }
     });
     quote! {
