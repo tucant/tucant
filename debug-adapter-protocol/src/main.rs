2dca4a70b10639e7251ac37681635d5d0dd34f97
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2023-01-14 23:52:04 +0100

    idea

--- debug-adapter-protocol/src/main.rs
+++ debug-adapter-protocol/src/main.rs
@@ -7,23 +7,81 @@
     clippy::module_name_repetitions
 )]
 use async_trait::async_trait;
+use futures_util::Sink;
+use futures_util::SinkExt;
 use futures_util::Stream;
+use futures_util::StreamExt;
+use json_rpc_server::run_json_rpc_server;
+use json_rpc_server::JsonRpcServer;
 use std::collections::HashMap;
 use std::sync::Arc;
-use std::sync::RwLock;
+use tokio::sync::{mpsc, oneshot, RwLock};
 
 out_dir_include::out_dir_include!("debug-adapter-protocol.rs");
 
-pub struct Server {
-    pm: ProtocolMessage,
+pub struct Server {}
+
+impl Server {
+    async fn handle_sending<
+        W: Sink<String, Error = anyhow::Error> + std::marker::Send + std::marker::Unpin,
+    >(
+        self: Arc<Self>,
+        mut sender: W,
+        mut rx: mpsc::Receiver<String>,
+    ) -> anyhow::Result<()> {
+        while let Some(result) = rx.recv().await {
+            sender.send(result).await?;
+        }
+
+        Ok(())
+    }
+
+    async fn handle_receiving<
+        R: Stream<Item = Result<String, anyhow::Error>> + std::marker::Send + std::marker::Unpin,
+    >(
+        self: Arc<Self>,
+        mut reader: R,
+    ) -> anyhow::Result<()> {
+        loop {
+            let request: () = serde_json::from_str(&reader.next().await.unwrap()?)?;
+
+            let cloned_self = self.clone();
+
+            todo!()
+        }
+    }
 }
 
-impl Server {}
+#[async_trait]
+impl JsonRpcServer for Server {
+    async fn run<
+        R: Stream<Item = Result<String, anyhow::Error>>
+            + std::marker::Unpin
+            + std::marker::Send
+            + 'static,
+        W: Sink<String, Error = anyhow::Error> + std::marker::Unpin + std::marker::Send + 'static,
+    >(
+        read: R,
+        write: W,
+    ) -> anyhow::Result<()> {
+        let (tx, rx) = mpsc::channel::<String>(3);
+
+        let arc_self = Arc::new(Self {});
+
+        let handle1 = tokio::spawn(arc_self.clone().handle_receiving(read));
+        let handle2 = tokio::spawn(arc_self.handle_sending(write, rx));
+
+        handle1.await??;
+        handle2.await??;
+
+        Ok(())
+    }
+}
 
 pub fn main() -> anyhow::Result<()> {
     tokio::runtime::Builder::new_multi_thread()
         .enable_all()
         .build()
         .unwrap()
-        .block_on(async { todo!() })
+        .block_on(async { run_json_rpc_server::<Server>().await })
 }
