6dc8f0e4807f413e2a0dc2141ff24174b85a065f
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2023-01-21 22:26:00 +0100

    it compiles

--- backend-rust/src/bin/test/main.rs
+++ backend-rust/src/bin/test/main.rs
@@ -7,24 +7,24 @@ use tucant::tucan::Tucan;
 // $HOME/.cargo/bin/diesel database reset && cargo run --bin test
 fn main() -> anyhow::Result<()> {
     tokio::runtime::Builder::new_current_thread()
-    .enable_all()
-    .build()
-    .unwrap()
-    .block_on(async { 
-        env_logger::init();
+        .enable_all()
+        .build()
+        .unwrap()
+        .block_on(async {
+            env_logger::init();
 
-        let tucan = Tucan::new()?;
-        let tucan = tucan
-            .login(
-                &std::env::var("TUCAN_USERNAME").unwrap(),
-                &std::env::var("TUCAN_PASSWORD").unwrap(),
-            )
-            .await?;
-    
-        let exams = tucan.my_exams().await?;
-    
-        println!("{exams:#?}");
-    
-        Ok(())
-    })
+            let tucan = Tucan::new()?;
+            let tucan = tucan
+                .login(
+                    &std::env::var("TUCAN_USERNAME").unwrap(),
+                    &std::env::var("TUCAN_PASSWORD").unwrap(),
+                )
+                .await?;
+
+            let exams = tucan.my_exams().await?;
+
+            println!("{exams:#?}");
+
+            Ok(())
+        })
 }
