6d273e596822bb55cbe104d9ad6a4beb89342df5
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2023-03-29 00:41:36 +0200

    jo

--- backend-rust/src/bin/base42/main.rs
+++ backend-rust/src/bin/base42/main.rs
@@ -8,7 +8,7 @@ use std::{
 };
 
 use base64::{
-    alphabet::{self, Alphabet},
+    alphabet::Alphabet,
     engine::{self, general_purpose},
     Engine,
 };
@@ -26,24 +26,30 @@ fn main() -> anyhow::Result<()> {
 
     for permutation in permutations_of(&tmp) {
         let alphabet = permutation.cloned().collect::<String>();
+        let custom: Alphabet = base64::alphabet::Alphabet::new(&alphabet).unwrap();
+
+        let engine: engine::GeneralPurpose =
+            engine::GeneralPurpose::new(&custom, general_purpose::NO_PAD);
+
         let res = (|| -> anyhow::Result<()> {
-            let custom: Alphabet = base64::alphabet::Alphabet::new(&alphabet).unwrap();
-
-            let engine: engine::GeneralPurpose =
-                engine::GeneralPurpose::new(&custom, general_purpose::NO_PAD);
-
             let file = File::open("base64.txt").unwrap();
-            // Read the file line by line, and return an iterator of the lines of the file.
             let lines = io::BufReader::new(file).lines();
 
             for line in lines {
                 let _ = engine.decode(line?.trim_end_matches("_"))?;
-                //stdout().write(&result)?;
             }
             Ok(())
         })();
         if res.is_ok() {
-            println!("{}", alphabet);
+            println!("\n\n\n\n{}\n\n", alphabet);
+            let file = File::open("base64.txt").unwrap();
+            let lines = io::BufReader::new(file).lines();
+
+            for line in lines {
+                let result = engine.decode(line?.trim_end_matches("_"))?;
+                stdout().write(&result)?;
+                println!("\n\n");
+            }
         }
     }
 
