eef98b3f1cc82e4e12b0c1da51ec15d5d8976064
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2023-04-04 20:42:01 +0200

    Calculate VV path

--- backend-rust/src/bin/server/utils.rs
+++ backend-rust/src/bin/server/utils.rs
@@ -1,15 +1,20 @@
-use std::collections::{HashMap, VecDeque};
+use std::{
+    collections::{HashMap, VecDeque},
+    hash::Hash,
+};
 
-use tucant::models::ModuleMenuPathPart;
+use tucant::models::PathLike;
 
-pub fn calculate_paths(path_to_root: &[ModuleMenuPathPart]) -> Vec<VecDeque<ModuleMenuPathPart>> {
-    let leaves = path_to_root.iter().take_while(|v| v.leaf);
+pub fn calculate_paths<TI: Eq + Hash, T: PathLike<TI> + Clone>(
+    path_to_root: &[T],
+) -> Vec<VecDeque<T>> {
+    let leaves = path_to_root.iter().take_while(|v| v.leaf());
 
     let nonleaves = path_to_root
         .iter()
         .rev()
-        .take_while(|v| !v.leaf)
-        .map(|v| (&v.tucan_id, v))
+        .take_while(|v| !v.leaf())
+        .map(|v| (v.tucan_id(), v))
         .collect::<HashMap<_, _>>();
 
     leaves
@@ -18,7 +23,7 @@ pub fn calculate_paths(path_to_root: &[ModuleMenuPathPart]) -> Vec<VecDeque<Modu
             let mut path = VecDeque::new();
             while let Some(curr) = current {
                 path.push_front(curr.to_owned().clone());
-                if let Some(parent) = &curr.parent {
+                if let Some(parent) = &curr.parent() {
                     current = nonleaves.get(&parent);
                 } else {
                     break;
