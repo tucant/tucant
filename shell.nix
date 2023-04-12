142a840e36c205c80bf3e8df47069bce07b514f9
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2023-04-12 21:04:43 +0200

    Small fixes

--- shell.nix
+++ shell.nix
@@ -4,12 +4,14 @@ mkShell {
   nativeBuildInputs = [
     bashInteractive
     cargo
+    cargo.rustc
     cargo.rustc.llvmPackages.clang
     cargo.rustc.llvmPackages.bintools
     rust-analyzer
     clippy
     rustfmt
     postgresql_15
+    nodejs
   ];
   RUST_BACKTRACE = 1;
 }
