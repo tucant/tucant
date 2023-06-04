222daca110a73393bab1b2a026fc183c81d0274c
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2023-06-04 21:43:11 +0200

    Update dependencies

--- tucant-language-server/fuzz_afl/fuzz_targets/fuzz_target_1.rs
+++ tucant-language-server/fuzz_afl/fuzz_targets/fuzz_target_1.rs
@@ -1,6 +1,9 @@
+#[cfg(fuzzing)]
 use afl::fuzz;
+#[cfg(fuzzing)]
 use tucant_language_server_fuzz_common::{magic, VecAction};
 
 fn main() {
+    #[cfg(fuzzing)]
     fuzz!(|input: VecAction| { magic(input) });
 }
