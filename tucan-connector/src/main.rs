bec2bf3f2ff007c4bf57fb2fdcfe7945c2d04632
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2024-10-04 23:02:57 +0200

    wip

--- tucan-connector/src/main.rs
+++ tucan-connector/src/main.rs
@@ -1,8 +1,6 @@
-pub mod streaming_http_parser;
-
+use encoding_rs::{Decoder, Encoding};
 use futures_util::TryStreamExt as _;
 use reqwest::{Client, ClientBuilder, Response};
-use streaming_http_parser::StreamingHttpParser;
 
 fn main() -> Result<(), TucanError> {
     tokio::runtime::Builder::new_multi_thread()
@@ -38,13 +36,9 @@ impl Tucan {
             .await?
             .error_for_status()?;
         println!("{resp:#?}");
-        let content = resp.bytes_stream();
-        let mut parser = StreamingHttpParser {
-            async_read: tokio_util::io::StreamReader::new(
-                content.map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e)),
-            ),
-        };
-        parser.doctype().await?;
+        let content = resp.text().await?;
+        println!("{content:#?}");
+
         Ok(Tucan { client })
     }
 }
