79dc773431f91591e76d6dfddb97d2b153410a32
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2024-10-04 21:30:17 +0200

    Make initial https request

--- tucan-connector/src/main.rs
+++ tucan-connector/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .unwrap()
+        .block_on(async_main())
+}
+
+
+async fn async_main() -> Result<(), Box<dyn std::error::Error>> {
+    let resp = reqwest::get("https://httpbin.org/ip")
+        .await?;
+    println!("{resp:#?}");
+    let content = resp.text().await?;
+    println!("{content:#?}");
+    Ok(())
 }
