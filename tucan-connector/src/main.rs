97ce0eee0d88c4c6fb8e073e286e019800c6d427
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2024-10-04 21:43:28 +0200

    Nicer error handling

--- tucan-connector/src/main.rs
+++ tucan-connector/src/main.rs
@@ -1,4 +1,6 @@
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+use reqwest::Response;
+
+fn main() -> Result<(), TucanError> {
     tokio::runtime::Builder::new_multi_thread()
         .enable_all()
         .build()
@@ -6,12 +8,30 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .block_on(async_main())
 }
 
-
-async fn async_main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp = reqwest::get("https://www.tucan.tu-darmstadt.de/")
-        .await?;
-    println!("{resp:#?}");
-    let content = resp.text().await?;
-    println!("{content:#?}");
+async fn async_main() -> Result<(), TucanError> {
+    let tucan = Tucan::new().await?;
     Ok(())
+}
+
+pub struct Tucan {}
+
+#[derive(thiserror::Error, Debug)]
+pub enum TucanError {
+    #[error("HTTP status code not 200")]
+    HttpNotOk(Response),
+    #[error("HTTP error {0:?}")]
+    Http(#[from] reqwest::Error),
+}
+
+impl Tucan {
+    pub async fn new() -> Result<Self, TucanError> {
+        let resp = reqwest::get("https://www.tucan.tu-darmstadt.de/").await?;
+        if resp.status() != 200 {
+            return Err(TucanError::HttpNotOk(resp));
+        }
+        println!("{resp:#?}");
+        let content = resp.text().await?;
+        println!("{content:#?}");
+        Ok(Tucan {})
+    }
 }
