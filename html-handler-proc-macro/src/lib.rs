4ae4095a97600aeea38bc17c699e6bbce511a229
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2024-10-05 21:01:36 +0200

    wip

--- html-handler-proc-macro/src/lib.rs
+++ html-handler-proc-macro/src/lib.rs
@@ -1,1 +1,16 @@
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
 
+#[proc_macro_derive(MyMacro)]
+pub fn my_macro(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    // Parse the input tokens into a syntax tree
+    let input = parse_macro_input!(input as DeriveInput);
+
+    // Build the output, possibly using quasi-quotation
+    let expanded = quote! {
+        // ...
+    };
+
+    // Hand the output tokens back to the compiler
+    proc_macro::TokenStream::from(expanded)
+}
