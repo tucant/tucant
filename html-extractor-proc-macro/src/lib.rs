a95c97385edce37932a3f6aa60d3ae153069ed70
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2024-10-05 22:41:25 +0200

    Parse first element

--- html-extractor-proc-macro/src/lib.rs
+++ html-extractor-proc-macro/src/lib.rs
@@ -1,19 +1,56 @@
 use quote::quote;
 use syn::{
     parse::{Parse, ParseStream},
-    parse2, parse_macro_input, DeriveInput, Ident, Token,
+    parse2, parse_macro_input,
+    punctuated::Punctuated,
+    DeriveInput, Ident, LitStr, Token,
 };
+
+struct HtmlAttribute {
+    ident: Punctuated<Ident, Token![-]>,
+    equals: Token![=],
+    value: LitStr,
+}
+
+impl Parse for HtmlAttribute {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let mut ident: Punctuated<Ident, Token![-]> = Punctuated::new();
+        ident.push_value(input.parse()?);
+        while input.peek(Token![-]) {
+            ident.push_punct(input.parse()?);
+            ident.push_value(input.parse()?);
+        }
+        let equals = input.parse()?;
+        let value = input.parse()?;
+        Ok(Self {
+            ident,
+            equals,
+            value,
+        })
+    }
+}
 
 struct HtmlElement {
     open_start: Token![<],
     element: Ident,
+    attributes: Vec<HtmlAttribute>,
+    open_end: Token![>],
 }
 
 impl Parse for HtmlElement {
     fn parse(input: ParseStream) -> syn::Result<Self> {
+        let open_start = input.parse()?;
+        let element = input.parse()?;
+        let mut attributes = Vec::new();
+        while !input.peek(Token!(>)) {
+            attributes.push(input.parse()?);
+        }
+        let open_end = input.parse()?;
         Ok(HtmlElement {
-            open_start: input.parse()?,
-            element: input.parse()?,
+            open_start,
+            element,
+            attributes,
+            open_end,
         })
     }
 }
