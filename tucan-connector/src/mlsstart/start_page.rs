ab6f10a8954a96e4dce2ae31f90b6e3f626d95af
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2024-10-09 22:16:11 +0200

    wip

--- tucan-connector/src/mlsstart/start_page.rs
+++ tucan-connector/src/mlsstart/start_page.rs
@@ -39,8 +39,6 @@ pub async fn after_login(client: &Client, login_response: LoginResponse) -> Resu
     );
     let html_handler = page_start(html_handler);
     html!(
-                            // this is different depending on the page
-
                         <li class="tree depth_1 linkItem branchLinkItem " title="Aktuelles" id="link000019">
                             <a  class="depth_1 link000019 navLink branchLink " href=aktuelles_url >"Aktuelles"</a>
                             <ul class="nav depth_2 linkItemContainer">
@@ -97,55 +95,26 @@ pub async fn after_login(client: &Client, login_response: LoginResponse) -> Resu
                             <ul class="nav depth_2 linkItemContainer">
                               <li class="intern depth_2 linkItem " title="Persönliche Daten" id="link000339"><a  class="depth_2 link000339 navLink " href={&format!("/scripts/mgrqispi.dll?APPNAME=CampusNet&PRGNAME=PERSADDRESS&ARGUMENTS=-N{id:015},-N000339,-A")} >"Persönliche Daten"</a></li>
                               <li class="intern depth_2 linkItem " title="Meine Dokumente" id="link000557"><a  class="depth_2 link000557 navLink " href={&format!("/scripts/mgrqispi.dll?APPNAME=CampusNet&PRGNAME=CREATEDOCUMENT&ARGUMENTS=-N{id:015},-N000557,")} >"Meine Dokumente"</a></li>
-                              <li class="intern depth_2 linkItem " title="Anträge" id="link000600"><a  class="depth_2 link000600 navLink " href="/scripts/mgrqispi.dll?APPNAME=CampusNet&PRGNAME=ACTION&ARGUMENTS=-Ab9U4mzVsE7G2cIagzX7inBDRns2GVhHrARIC3ERBiClU5FOMeKVvqLmeGarao7R9O4hNx0bOawKW6qqQ-60a3z8hhllXzLtzfeMb9liDg748XXOGmO7yEpbjqXG7sTVkZrFbOg~RdNglQ~QGFBEWeWA5abdewbrc8g__" >"Anträge"</a></li>
+                              <li class="intern depth_2 linkItem " title="Anträge" id="link000600"><a  class="depth_2 link000600 navLink " href=_url>"Anträge"</a></li>
                               <li class="intern depth_2 linkItem " title="Sperren" id="link000652"><a  class="depth_2 link000652 navLink " href={&format!("/scripts/mgrqispi.dll?APPNAME=CampusNet&PRGNAME=HOLDINFO&ARGUMENTS=-N{id:015},-N000652,")} >"Sperren"</a></li>
                             </ul>
                           </li>
                           <li class="tree depth_1 linkItem branchLinkItem " title="Bewerbung" id="link000441">
-                            <a  class="depth_1 link000441 navLink branchLink " href="/scripts/mgrqispi.dll?APPNAME=CampusNet&PRGNAME=EXTERNALPAGES&ARGUMENTS=-N{id:015},-N000441,-Abewerbung" >"Bewerbung"</a>
+                            <a  class="depth_1 link000441 navLink branchLink " href={&format!("/scripts/mgrqispi.dll?APPNAME=CampusNet&PRGNAME=EXTERNALPAGES&ARGUMENTS=-N{id:015},-N000441,-Abewerbung")} >"Bewerbung"</a>
                             <ul class="nav depth_2 linkItemContainer">
                               <li class="intern depth_2 linkItem " title="Herzlich Willkommen" id="link000442"><a  class="depth_2 link000442 navLink " href={&format!("/scripts/mgrqispi.dll?APPNAME=CampusNet&PRGNAME=EXTERNALPAGES&ARGUMENTS=-N{id:015},-N000442,-Abewerbung")} >"Herzlich Willkommen"</a></li>
-                              <li class="intern depth_2 linkItem " title="Meine Bewerbung" id="link000443"><a  class="depth_2 link000443 navLink " href="/scripts/mgrqispi.dll?APPNAME=CampusNet&PRGNAME=ACTION&ARGUMENTS=-ARpWJ595TPHb5Kw1T-3-2XTtQz8OAhYmDbF3kJsnagiy~Rhj5xhiWMgLXOaz3aOSpQJP6-KWDslOCw~lkCOVJ6X-7fX05Se0TCAghAOmtBxnnfghCs83ajaCJt7QaisVwZ~CXzW1LfEPnCEt94r~f19MeDXSCsulqH7Tbzrw_" >"Meine Bewerbung"</a></li>
+                              <li class="intern depth_2 linkItem " title="Meine Bewerbung" id="link000443"><a  class="depth_2 link000443 navLink " href=_url >"Meine Bewerbung"</a></li>
                               <li class="intern depth_2 linkItem " title="Meine Dokumente" id="link000444"><a  class="depth_2 link000444 navLink " href={&format!("/scripts/mgrqispi.dll?APPNAME=CampusNet&PRGNAME=CREATEDOCUMENT&ARGUMENTS=-N{id:015},-N000444,")} >"Meine Dokumente"</a></li>
                             </ul>
                           </li>
                           <li class="intern depth_1 linkItem " title="Hilfe" id="link000340"><a  class="depth_1 link000340 navLink " href={&format!("/scripts/mgrqispi.dll?APPNAME=CampusNet&PRGNAME=EXTERNALPAGES&ARGUMENTS=-N{id:015},-N000340,-Ahilfe%2Ehtml")} >"Hilfe"</a></li>
-                          </ul>
-
-
-                     </ul>_
+                          </ul>_
                   </div>_
                   <div id="pageHeadBottom_3" class="pageElementTop">_
                         <div id="pageHeadSwitchLang" class="pageElementRight">_
-                            <a href="/scripts/mgrqispi.dll?APPNAME=CampusNet&PRGNAME=CHANGELANGUAGE&ARGUMENTS=-N000000000000002,-N002" class="img img_LangEnglish pageElementLeft" title="English">"English"</a>_
+                            <a href=_wef class="img img_LangEnglish pageElementLeft" title="English">"English"</a>_
+                            <a href={&format!("/scripts/mgrqispi.dll?APPNAME=CampusNet&PRGNAME=LOGOUT&ARGUMENTS=-N{id:015},-N001")} id="logoutButton"    class="img img_arrowLogout logout" title="Abmelden">"Abmelden"</a>_
                         </div>_
-                        <form name="cn_loginForm" id="cn_loginForm" action="/scripts/mgrqispi.dll" method="post" class="pageElementRight">_
-                            <div>_
-                                <fieldset id="fieldSet_login">_
-                                    <legend>"Anmeldung"</legend>_
-                                    <div class="formRow nb">_
-                                        <div class="inputFieldLabel">_
-                                            <label for="field_user">"TU-ID:"</label>_
-                                            <input type="text" id="field_user" name="usrname" size="15" class="login" maxlength="255" accesskey="n" autofocus=""></input>_
-                                        </div>_
-                                        <div class="inputFieldLabel">_
-                                            <label for="field_pass">"Passwort:"</label>_
-                                            <input type="password" id="field_pass" name="pass" value="" size="15" class="login" maxlength="255" accesskey="p"></input>_
-                                        </div>_
-                                    </div>_
-                                </fieldset>_
-                                <input class="img img_arrowSubmit login_btn" type="submit" id="logIn_btn" value="Anmelden" onclick="return checkform('cn_loginForm','usrname:TU-ID,pass:Passwort','000000000000001');"></input>_
-                                <!--"416mrhkWvn83zXJacA3wOy6ZHvHNbAfVlkkb_PMmkEg"-->_
-                                <input name="APPNAME" type="hidden" value="CampusNet"></input>_
-                                <input name="PRGNAME" type="hidden" value="LOGINCHECK"></input>_
-                                <input name="ARGUMENTS" type="hidden" value="clino,usrname,pass,menuno,menu_type,browser,platform"></input>_
-                                <input name="clino" type="hidden" value="000000000000001"></input>_
-                                <input name="menuno" type="hidden" value="000344"></input>_
-                                <input name="menu_type" type="hidden" value="classic"></input>_
-                                <input name="browser" type="hidden" value=""></input>_
-                                <input name="platform" type="hidden" value=""></input>_
-                            </div>_
-                        </form>_
                     </div>_
               </div>_
     );
