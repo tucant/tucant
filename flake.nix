49939858ff5f64b0d91f5945ba3ca50a99e54112
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2024-10-15 21:20:13 +0200

    Add tucan injector

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ ];
+          };
+          lib = pkgs.lib;
+        in
+        {
+          devShells.default = pkgs.mkShell {
+            buildInputs = [ pkgs.bashInteractive pkgs.openssl ];
+
+            nativeBuildInputs = [ pkgs.pkg-config ];
+          };
+        }
+      );
+}
