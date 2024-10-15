5b703f35a92c4520b3ebd522417014295336c09b
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2024-10-15 22:17:33 +0200

    wip

--- tucan-injector/vite.config.js
+++ tucan-injector/vite.config.js
@@ -1,15 +1,18 @@
 import { defineConfig } from "vite";
 import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig({
     plugins: [
         wasm(),
+        topLevelAwait()
     ],
     build: {
         lib: {
+            name: "tucan_injector",
             entry: "./pkg/tucan_injector.js",
-            formats: ["es"]
+            formats: ["iife"]
         },
-        target: "esnext"
+        minify: false
     }
 });
