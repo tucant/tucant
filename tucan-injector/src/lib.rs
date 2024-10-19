ca53e708be08a91414d980db49260009409e5c6b
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2024-10-19 21:45:18 +0200

    Only remove styles on supported pages

--- tucan-injector/src/lib.rs
+++ tucan-injector/src/lib.rs
@@ -6,7 +6,11 @@ use tucan_connector::{
     registration::index::{anmeldung, AnmeldungRequest, AnmeldungResponse},
     Tucan,
 };
-use wasm_bindgen::{prelude::wasm_bindgen, JsCast as _};
+use wasm_bindgen::{
+    prelude::{wasm_bindgen, Closure},
+    JsCast as _,
+};
+use web_sys::{js_sys::Function, Node};
 use yew::{
     prelude::*,
     suspense::{self, SuspensionResult},
@@ -179,8 +183,47 @@ fn App() -> HtmlResult {
     })
 }
 
+fn inject() {
+    info!("Supported URL detected, injecting");
+
+    let window = web_sys::window().unwrap();
+    let document = window.document().unwrap();
+
+    let closure = Closure::<dyn Fn(Node)>::new(|element: Node| {
+        element
+            .parent_node()
+            .unwrap()
+            .remove_child(&element)
+            .unwrap();
+    });
+    document
+        .query_selector_all(r#"link[rel="stylesheet"]"#)
+        .unwrap()
+        .for_each(closure.as_ref().unchecked_ref())
+        .unwrap();
+    document
+        .query_selector_all(r#"style"#)
+        .unwrap()
+        .for_each(closure.as_ref().unchecked_ref())
+        .unwrap();
+    document
+        .query_selector_all(r#"script"#)
+        .unwrap()
+        .for_each(closure.as_ref().unchecked_ref())
+        .unwrap();
+    document
+        .query_selector_all(r#"[style]"#)
+        .unwrap()
+        .for_each(closure.as_ref().unchecked_ref())
+        .unwrap();
+
+    yew::Renderer::<App>::new().render();
+}
+
 #[wasm_bindgen(start)]
 fn start() {
+    std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+
     console_log::init().unwrap();
 
     let window = web_sys::window().unwrap();
@@ -198,8 +241,8 @@ fn start() {
 
     match prgname {
         None => {}
-        Some("EXTERNALPAGES") => {
-            yew::Renderer::<App>::new().render();
+        Some("REGISTRATION") => {
+            inject();
         }
         Some(_) => {}
     }
--- tucan-injector/src/main.js
+++ tucan-injector/src/main.js
@@ -1,13 +1,1 @@
 import "../wasm-bindgen/tucan_injector.js"
-
-document.querySelectorAll('link[rel="stylesheet"]')
-    .forEach(el => el.parentNode.removeChild(el));
-
-document.querySelectorAll('style')
-    .forEach(el => el.parentNode.removeChild(el));
-
-document.querySelectorAll('script')
-    .forEach(el => el.parentNode.removeChild(el));
-
-document.querySelectorAll('[style]')
-    .forEach(el => el.removeAttribute('style'));
