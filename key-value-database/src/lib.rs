8ea92b4d157bc20fc3acba5e63111dc65d9cd0ae
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2024-12-22 17:52:46 +0100

    Fix android (#442)

--- key-value-database/src/lib.rs
+++ key-value-database/src/lib.rs
@@ -24,13 +24,20 @@ impl Database {
         }
         #[cfg(not(target_arch = "wasm32"))]
         {
-            let database = sqlx::SqlitePool::connect(if cfg!(target_os = "android") {
-                "sqlite:///data/data/de.selfmade4u.tucant/files/data.db?mode=rwc"
+            let database = if cfg!(target_os = "android") {
+                tokio::fs::create_dir("/data/data/de.selfmade4u.tucant/files")
+                    .await
+                    .unwrap();
+                sqlx::SqlitePool::connect(
+                    "sqlite:///data/data/de.selfmade4u.tucant/files/data.db?mode=rwc",
+                )
+                .await
+                .unwrap()
             } else {
-                "sqlite://data.db?mode=rwc"
-            })
-            .await
-            .unwrap();
+                sqlx::SqlitePool::connect("sqlite://data.db?mode=rwc")
+                    .await
+                    .unwrap()
+            };
             sqlx::query(
                 "CREATE TABLE IF NOT EXISTS store (key TEXT PRIMARY KEY NOT NULL, value TEXT NOT \
                  NULL)",
