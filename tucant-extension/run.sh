faa94554383e6ec0e1f97263f09212091d83c908
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2024-12-25 19:27:30 +0100

    wip

--- tucant-extension/run.sh
+++ tucant-extension/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 cargo build --color always --target=wasm32-unknown-unknown
-wasm-bindgen --out-dir=wasm-bindgen ../target/wasm32-unknown-unknown/debug/tucan_injector.wasm
+wasm-bindgen --target web --out-dir=wasm-bindgen ../target/wasm32-unknown-unknown/debug/tucan_injector.wasm
 #npm run build
