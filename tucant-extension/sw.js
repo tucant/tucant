37f1aa330c3eb2a5fca0d2330650d7522ae1546a
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2024-12-25 18:54:06 +0100

    now we know it doesnt work

--- tucant-extension/sw.js
+++ tucant-extension/sw.js
@@ -1,1 +1,4 @@
-console.log("hello from service worker")
+self.addEventListener('fetch', function (event) {
+    event.respondWith(fetch(event.request));
+});
+// await navigator.serviceWorker.register("/sw.js")
