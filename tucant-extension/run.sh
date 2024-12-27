22e46031c5d3407071f4d1de6713598024b80dc0
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2024-12-27 13:19:36 +0100

    wip

--- tucant-extension/run.sh
+++ tucant-extension/run.sh
@@ -4,14 +4,14 @@ wasm-pack build --release
 
 ls -lh pkg/tucant_extension_bg.wasm
 
-wasm-pack build --profiling
+#wasm-pack build --profiling
 
-wasm2wat --output=pkg/tucant_extension_bg.wat pkg/tucant_extension_bg.wasm
+#wasm2wat --output=pkg/tucant_extension_bg.wat pkg/tucant_extension_bg.wasm
 
 #twiggy top pkg/tucant_extension_bg.wasm | less
 
 #cargo llvm-lines --release --target=wasm32-unknown-unknown
 
-cargo tree --target=wasm32-unknown-unknown
+#cargo tree --target=wasm32-unknown-unknown
 
-#-rw-r--r-- 1 moritz users 2,4M Dez 27 13:01 pkg/tucant_extension_bg.wasm
+#-rw-r--r-- 1 moritz users 1,8M Dez 27 13:14 pkg/tucant_extension_bg.wasm
