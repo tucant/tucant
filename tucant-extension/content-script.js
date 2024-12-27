9585d9ef2e4807838331f2237c7d22696c024472
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2024-12-27 17:30:54 +0100

    Fix

--- tucant-extension/content-script.js
+++ tucant-extension/content-script.js
@@ -1,1 +1,10 @@
-window.location.href = '/scripts/mgrqispi.dll?APPNAME=CampusNet&PRGNAME=MLSSTART&ARGUMENTS=-N205439515619234,-N000350,';
+let url = window.location.href;
+url = url.replaceAll(new RegExp("^https://www\\.tucan\\.tu-darmstadt\\.de/scripts/mgrqispi\\.dll\\?APPNAME=CampusNet&PRGNAME=CHANGELANGUAGE&ARGUMENTS=-N(\\d+),-N001$", "g"),
+    "https://www.tucan.tu-darmstadt.de/scripts/mgrqispi.dll?APPNAME=CampusNet&PRGNAME=MLSSTART&ARGUMENTS=-N$1,-N000019,");
+url = url.replaceAll(new RegExp("^https://www\\.tucan\\.tu-darmstadt\\.de/scripts/mgrqispi\\.dll\\?APPNAME=CampusNet&PRGNAME=CHANGELANGUAGE&ARGUMENTS=-N(\\d+),-N002$", "g"),
+    "https://www.tucan.tu-darmstadt.de/scripts/mgrqispi.dll?APPNAME=CampusNet&PRGNAME=MLSSTART&ARGUMENTS=-N$1,-N000350,");
+if (url !== window.location.href) {
+    window.location.href = url;
+} else {
+    console.log("NOT REDIRECTING")
+}
