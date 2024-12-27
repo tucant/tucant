66c9790ec18ba426f40ecdc6dedf935365e6e100
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2024-12-27 12:55:51 +0100

    smaller size?

--- tucant-extension/run.sh
+++ tucant-extension/run.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-cargo build --release --color always --target=wasm32-unknown-unknown
-wasm-bindgen --target web --out-dir=wasm-bindgen ../target/wasm32-unknown-unknown/release/tucant_extension.wasm
-#npm run build
-
-twiggy top ../target/wasm32-unknown-unknown/release/tucant_extension.wasm  | less
-
 wasm-pack build --profiling
 
 wasm2wat --output=pkg/tucant_extension_bg.wat pkg/tucant_extension_bg.wasm
+
+twiggy top pkg/tucant_extension_bg.wasm | less
+
+cargo llvm-lines --release --target=wasm32-unknown-unknown
