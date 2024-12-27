7a1e43079bf3f5de0285a89482d1bceba223fcf8
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2024-12-27 21:54:06 +0100

    detect login state

--- tucant-extension/background.js
+++ tucant-extension/background.js
@@ -27,7 +27,6 @@ chrome.webNavigation.onCommitted.addListener((details) => {
     );
     if (JSON.stringify(details.transitionQualifiers.sort()) === JSON.stringify(["server_redirect", "client_redirect"].sort()) && details.transitionType === "link") {
         const match = new RegExp("^https://www\\.tucan\\.tu-darmstadt\\.de/scripts/mgrqispi\\.dll\\?APPNAME=CampusNet&PRGNAME=MLSSTART&ARGUMENTS=-N(\\d+),-N000019,$", "g").exec(details.url);
-        console.log(match)
         if (match !== null) {
             console.log(`logged in with session id ${match[1]}`);
             chrome.action.setBadgeText({ text: "L" })
@@ -36,7 +35,19 @@ chrome.webNavigation.onCommitted.addListener((details) => {
             )
             chrome.action.setBadgeTextColor({ color: "red" });
         }
+
+        const logoutMatch = new RegExp("^https://www\\.tucan\\.tu-darmstadt\\.de/scripts/mgrqispi\\.dll\\?APPNAME=CampusNet&PRGNAME=EXTERNALPAGES&ARGUMENTS=-N000000000000001,-N000344,-Awelcome$", "g").exec(details.url);
+        if (logoutMatch !== null) {
+            console.log(`logged out`);
+            chrome.action.setBadgeText({ text: "" })
+            chrome.action.setBadgeBackgroundColor(
+                { color: 'green' }
+            )
+            chrome.action.setBadgeTextColor({ color: "red" });
+        }
     }
+
+
 }, { url: [{ urlPrefix: "https://www.tucan.tu-darmstadt.de" }] });
 
 // https://developer.chrome.com/docs/extensions/reference/api/action#badge
