34bcdf030f0defedf19dbfbbf3da115f9e0d1439
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2024-12-27 11:17:44 +0100

    wip

--- tucant-extension/content-script.js
+++ tucant-extension/content-script.js
@@ -1,3 +1,4 @@
 if (document.body.classList.contains("redirect")) {
+    console.log("REDIRECTING")
     window.location.href = (/** @type {HTMLAnchorElement} */ document.querySelector("h2 a")).href;
 }
