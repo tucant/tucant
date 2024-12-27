6340d3e2a88fb0638a2795f1f8c1ad9535a1bb81
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2024-12-27 18:55:55 +0100

    wip

--- tucant-yew/fixup.sh
+++ tucant-yew/fixup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+set -ex
 
-sed -n '/<script/,/<\/script>/p' dist/index.html > dist/test.js
-sed -i '/script/d' ./dist/test.js
-sed -i '/<script/,/<\/script>/d' dist/index.html
-sed -i 's/<\/title>/<\/title><script src="\/test.js"><\/script>/' dist/index.html
+sed -n '/<script/,/<\/script>/p' $TRUNK_STAGING_DIR/index.html > $TRUNK_STAGING_DIR/test.js
+sed -i '/script/d' $TRUNK_STAGING_DIR/test.js
+sed -i '/<script/,/<\/script>/d' $TRUNK_STAGING_DIR/index.html
+sed -i 's/<\/title>/<\/title><script src="\/test.js"><\/script>/' $TRUNK_STAGING_DIR/index.html
