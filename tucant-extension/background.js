3926e1def061bedab7c26df71549844deaaf3740
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2024-12-27 21:04:54 +0100

    wip

--- tucant-extension/background.js
+++ tucant-extension/background.js
@@ -17,3 +17,16 @@ chrome.declarativeNetRequest.updateDynamicRules({
     removeRuleIds: RULES.map(r => r.id),
     addRules: RULES,
 });
+
+chrome.webNavigation.onCommitted.addListener((details) => {
+    chrome.notifications.create({
+        type: 'basic',
+        iconUrl: 'icon.png',
+        title: 'page loaded',
+        message:
+            details.transitionQualifiers +
+            ' at ' +
+            details.transitionType +
+            ' milliseconds since the epoch.'
+    });
+}, { url: [{ urlPrefix: "https://www.tucan.tu-darmstadt.de" }] });
