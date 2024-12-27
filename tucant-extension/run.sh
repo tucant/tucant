4abdaea79291919229abc2f1f81300c68d7edc21
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2024-12-27 12:43:24 +0100

    debu gsymbols

--- tucant-extension/run.sh
+++ tucant-extension/run.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
-cargo build --color always --target=wasm32-unknown-unknown
-wasm-bindgen --target web --out-dir=wasm-bindgen ../target/wasm32-unknown-unknown/debug/tucan_injector.wasm
+cargo build --release --color always --target=wasm32-unknown-unknown
+wasm-bindgen --target web --out-dir=wasm-bindgen ../target/wasm32-unknown-unknown/release/tucant_extension.wasm
 #npm run build
+
+twiggy top ../target/wasm32-unknown-unknown/release/tucant_extension.wasm  | less
+
+wasm-pack build --profiling
+
+wasm2wat --output=pkg/tucant_extension_bg.wat pkg/tucant_extension_bg.wasm
