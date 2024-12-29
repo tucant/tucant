a650f7f52f942c17262eca8b42c010e067a2ad8f
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2024-12-29 22:53:10 +0100

    Fix

--- tucant-yew/fixup.sh
+++ tucant-yew/fixup.sh
@@ -5,3 +5,5 @@ sed -n '/<script type="module"/,/<\/script>/p' $TRUNK_STAGING_DIR/index.html > $
 sed -i.bak '/script/d' $TRUNK_STAGING_DIR/test.js
 sed -i.bak '/<script type="module"/,/<\/script>/d' $TRUNK_STAGING_DIR/index.html
 sed -i.bak "s@</title>@</title><script type=\"module\" src=\"${TRUNK_PUBLIC_URL}test.js\"></script>@" $TRUNK_STAGING_DIR/index.html
+rm $TRUNK_STAGING_DIR/test.js.bak
+rm $TRUNK_STAGING_DIR/index.html.bak
