df49debfd6421c295eaad0dbeb2b35c86da00f11
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2024-12-30 22:56:51 +0100

    wip

--- tucant-yew/src/main.rs
+++ tucant-yew/src/main.rs
@@ -1,9 +1,35 @@
 use tucant_yew::{login_response, App, AppProps, CurrentSession};
 use wasm_bindgen::prelude::wasm_bindgen;
 
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn error(msg: String);
+
+    fn alert(s: &str);
+
+    type Error;
+
+    #[wasm_bindgen(constructor)]
+    fn new() -> Error;
+
+    #[wasm_bindgen(structural, method, getter)]
+    fn stack(error: &Error) -> String;
+}
+
 #[wasm_bindgen(main)]
 async fn main() {
-    std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+    // From https://github.com/rustwasm/console_error_panic_hook, licensed under MIT and Apache 2.0
+    std::panic::set_hook(Box::new(|info| {
+        let mut msg = info.to_string();
+        msg.push_str("\n\nStack:\n\n");
+        let e = Error::new();
+        let stack = e.stack();
+        msg.push_str(&stack);
+        msg.push_str("\n\n");
+        //error(msg.clone());
+        alert(msg.as_str());
+    }));
 
     console_log::init().unwrap();
 
