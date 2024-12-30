77a71b061d88a0e5e2289b0f7a8bf9df1d4e7d9d
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2024-12-30 23:05:25 +0100

    stupid yew

--- tucant-yew/src/main.rs
+++ tucant-yew/src/main.rs
@@ -1,5 +1,7 @@
+use log::info;
 use tucant_yew::{login_response, App, AppProps, CurrentSession};
 use wasm_bindgen::prelude::wasm_bindgen;
+use yew::set_custom_panic_hook;
 
 #[wasm_bindgen]
 extern "C" {
@@ -20,18 +22,20 @@ extern "C" {
 #[wasm_bindgen(main)]
 async fn main() {
     // From https://github.com/rustwasm/console_error_panic_hook, licensed under MIT and Apache 2.0
-    std::panic::set_hook(Box::new(|info| {
+    set_custom_panic_hook(Box::new(|info| {
         let mut msg = info.to_string();
         msg.push_str("\n\nStack:\n\n");
         let e = Error::new();
         let stack = e.stack();
         msg.push_str(&stack);
         msg.push_str("\n\n");
-        //error(msg.clone());
+        error(msg.clone());
         alert(msg.as_str());
     }));
 
     console_log::init().unwrap();
+
+    info!("ewfwfwfefwf");
 
     let login_response = login_response().await;
 
