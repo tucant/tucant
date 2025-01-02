00ea8cb01094a7850b3b5ff752edd2524dbe41de
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2025-01-02 15:36:34 +0100

    interesting

--- tucant-yew/src/lib.rs
+++ tucant-yew/src/lib.rs
@@ -435,6 +435,75 @@ fn module_details(ModuleDetailsProps { module_details }: &ModuleDetailsProps) ->
     })
 }
 
+#[function_component(Navbar)]
+pub fn navbar() -> Html {
+    html! {
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">{ "Navbar" }</a>
+                <button
+                    class="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span class="navbar-toggler-icon" />
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">{ "Home" }</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">{ "Link" }</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a
+                                class="nav-link dropdown-toggle"
+                                href="#"
+                                role="button"
+                                data-bs-toggle="dropdown"
+                                aria-expanded="false"
+                            >
+                                { "Dropdown" }
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li>
+                                    <a class="dropdown-item" href="#">{ "Action" }</a>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item" href="#">{ "Another action" }</a>
+                                </li>
+                                <li>
+                                    <hr class="dropdown-divider" />
+                                </li>
+                                <li>
+                                    <a class="dropdown-item" href="#">{ "Something else here" }</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" aria-disabled="true">{ "Disabled" }</a>
+                        </li>
+                    </ul>
+                    <form class="d-flex" role="search">
+                        <input
+                            class="form-control me-2"
+                            type="search"
+                            placeholder="Search"
+                            aria-label="Search"
+                        />
+                        <button class="btn btn-outline-success" type="submit">{ "Search" }</button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+    }
+}
+
 #[autoprops]
 #[function_component(App)]
 pub fn app(initial_session: &Option<CurrentSession>) -> HtmlResult {
@@ -445,102 +514,7 @@ pub fn app(initial_session: &Option<CurrentSession>) -> HtmlResult {
             <style>{ include_str!("./bootstrap.min.css") }</style>
             <ContextProvider<Option<CurrentSession>> context={(*ctx).clone()}>
                 <HashRouter>
-                    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-                        <div class="container-fluid">
-                            <a class="navbar-brand" href="#">{ "Navbar" }</a>
-                            <button
-                                class="navbar-toggler"
-                                type="button"
-                                data-bs-toggle="collapse"
-                                data-bs-target="#navbarSupportedContent"
-                                aria-controls="navbarSupportedContent"
-                                aria-expanded="false"
-                                aria-label="Toggle navigation"
-                            >
-                                <span class="navbar-toggler-icon" />
-                            </button>
-                            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                                    <li class="nav-item">
-                                        <a class="nav-link active" aria-current="page" href="#">
-                                            { "Home" }
-                                        </a>
-                                    </li>
-                                    <li class="nav-item dropdown">
-                                        <a
-                                            class="nav-link dropdown-toggle"
-                                            href="#"
-                                            role="button"
-                                            data-bs-toggle="dropdown"
-                                            aria-expanded="false"
-                                        >
-                                            { "Aktuelles" }
-                                        </a>
-                                        <ul class="dropdown-menu">
-                                            <li>
-                                                <a class="dropdown-item" href="#">
-                                                    { "Aktuelles" }
-                                                </a>
-                                            </li>
-                                            <li>
-                                                <a class="dropdown-item" href="#">
-                                                    { "Nachrichten" }
-                                                </a>
-                                            </li>
-                                        </ul>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="#">{ "Link" }</a>
-                                    </li>
-                                    <li class="nav-item dropdown">
-                                        <a
-                                            class="nav-link dropdown-toggle"
-                                            href="#"
-                                            role="button"
-                                            data-bs-toggle="dropdown"
-                                            aria-expanded="false"
-                                        >
-                                            { "Dropdown" }
-                                        </a>
-                                        <ul class="dropdown-menu">
-                                            <li>
-                                                <a class="dropdown-item" href="#">{ "Action" }</a>
-                                            </li>
-                                            <li>
-                                                <a class="dropdown-item" href="#">
-                                                    { "Another action" }
-                                                </a>
-                                            </li>
-                                            <li>
-                                                <hr class="dropdown-divider" />
-                                            </li>
-                                            <li>
-                                                <a class="dropdown-item" href="#">
-                                                    { "Something else here" }
-                                                </a>
-                                            </li>
-                                        </ul>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link disabled" aria-disabled="true">
-                                            { "Disabled" }
-                                        </a>
-                                    </li>
-                                </ul>
-                                <form class="d-flex" role="search">
-                                    <input
-                                        class="form-control me-2"
-                                        type="search"
-                                        placeholder="Search"
-                                        aria-label="Search"
-                                    />
-                                    <button class="btn btn-outline-success" type="submit">
-                                        { "Search" }
-                                    </button>
-                                </form>
-                            </div>
-                        </div>
-                    </nav>
+                    <Navbar />
                     <Switch<Route> render={switch} />
                 </HashRouter>
             </ContextProvider<Option<CurrentSession>>>
