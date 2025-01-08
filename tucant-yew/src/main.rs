df9bacc9f8e9783d8d990a5ae5ef18ed38add86f
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2025-01-08 20:51:36 +0100

    Fix update

--- tucant-yew/src/main.rs
+++ tucant-yew/src/main.rs
@@ -1,6 +1,6 @@
 use js_sys::{global, Reflect};
 use log::info;
-use tucant_yew::{api_server::ApiServerTucan, direct::DirectTucan, App, AppProps};
+use tucant_yew::{App, AppProps};
 use wasm_bindgen::{prelude::wasm_bindgen, JsValue};
 use web_sys::window;
 use yew::set_custom_panic_hook;
@@ -42,7 +42,7 @@ async fn main() {
     #[cfg(feature = "direct")]
     if Reflect::get(&global(), &JsValue::from_str("chrome")).is_ok() {
         let login_response = tucant_yew::direct_login_response().await;
-        yew::Renderer::<App<DirectTucan>>::with_props(AppProps {
+        yew::Renderer::<App<tucant_yew::direct::DirectTucan>>::with_props(AppProps {
             initial_session: login_response,
         })
         .render();
@@ -50,7 +50,7 @@ async fn main() {
     #[cfg(feature = "api")]
     {
         let login_response = tucant_yew::api_login_response().await;
-        yew::Renderer::<App<ApiServerTucan>>::with_props(AppProps {
+        yew::Renderer::<App<tucant_yew::api_server::ApiServerTucan>>::with_props(AppProps {
             initial_session: login_response,
         })
         .render();
