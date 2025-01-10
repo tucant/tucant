b6279e0e55fae12f58d91fd7429747b41e9a02b1
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2025-01-10 22:18:09 +0100

    wip

--- tucant-yew/src/lib.rs
+++ tucant-yew/src/lib.rs
@@ -1,3 +1,4 @@
+use module_details::ModuleDetails;
 use navbar::Navbar;
 use registration::Registration;
 use std::ops::Deref;
@@ -18,6 +19,7 @@ pub mod navbar;
 pub mod api_server;
 #[cfg(feature = "direct")]
 pub mod direct;
+pub mod module_details;
 pub mod registration;
 pub mod tauri;
 
@@ -240,72 +242,6 @@ fn switch<TucanType: Tucan + 'static>(routes: Route) -> Html {
             }
         }
     }
-}
-
-#[derive(Properties, PartialEq)]
-pub struct ModuleDetailsProps {
-    module_details: ModuleDetailsRequest,
-}
-
-#[function_component(ModuleDetails)]
-fn module_details<TucanType: Tucan>(
-    ModuleDetailsProps { module_details }: &ModuleDetailsProps,
-) -> HtmlResult {
-    let data = use_state(|| None);
-    let loading = use_state(|| false);
-    let current_session =
-        use_context::<UseStateHandle<Option<LoginResponse>>>().expect("no ctx found");
-    {
-        let data = data.clone();
-        let loading = loading.clone();
-        use_effect_with(module_details.to_owned(), move |request| {
-            loading.set(true);
-            let request = request.clone();
-            let data = data.clone();
-            spawn_local(async move {
-                let response =
-                    TucanType::module_details(&current_session.deref().clone().unwrap(), request)
-                        .await
-                        .unwrap();
-                data.set(Some(response));
-                loading.set(false);
-            })
-        });
-    }
-
-    Ok(html! {
-        <div class="container">
-            { data.as_ref().map(|module| {
-                html!{
-                    <div>
-                        <h1>{ &module.module_id }</h1>
-
-                        <div>{ format!("Registered: {}", if module.registered { "Yes" } else { "No" }) }</div>
-
-                        <div>{ format!("Dozenten: {}", module.dozenten) }</div>
-
-                        <div>{ format!("Display in timetable: {}", module.display_in_timetable) }</div>
-
-                        <div>{ format!("Duration: {}", module.duration) }</div>
-
-                        <div>{ format!("Credits: {}", module.credits) }</div>
-
-                        <div>{ format!("Count of elective courses: {}", module.count_elective_courses) }</div>
-
-                        // TODO FIXME this is dangerous
-                        { Html::from_html_unchecked(module.description.join("\n").into()) }
-
-                    </div>
-                }
-            }).unwrap_or_else(|| html! { if *loading {
-                <div style="z-index: 10000" class="position-fixed top-50 start-50 translate-middle">
-                    <div class="spinner-grow" role="status">
-                        <span class="visually-hidden">{"Loading..."}</span>
-                    </div>
-                </div>
-            } }) }
-        </div>
-    })
 }
 
 #[derive(Properties, PartialEq)]
