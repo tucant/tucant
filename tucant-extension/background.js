c1650e9f512ae2ffa7f08170e88fa23cd0b0ad6a
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2025-01-11 00:02:46 +0100

    wip

--- tucant-extension/background.js
+++ tucant-extension/background.js
@@ -1,6 +1,7 @@
 console.log("background script")
 
 // maybe don't extract the cookie using the cookie api at all but instead use a content script to extract the info from the tucan page. then the id and cookie can't get out of sync.
+// and we can get the current value just with a reload of a tucan page and not a new login.
 // but also won't get auto-deleted. though we could store a hash of the cookie or so to compare if its still up to date
 
 chrome.runtime.onInstalled.addListener(() => {
