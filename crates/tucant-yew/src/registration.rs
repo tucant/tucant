7e25e45e667d9fe134470783e2ec6ef9be4073ad
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2025-03-30 14:49:26 +0200

    Add reload button

--- crates/tucant-yew/src/registration.rs
+++ crates/tucant-yew/src/registration.rs
@@ -7,7 +7,7 @@ use tucant_types::{
     registration::{AnmeldungRequest, AnmeldungResponse, RegistrationState},
 };
 use wasm_bindgen_futures::spawn_local;
-use yew::{Html, HtmlResult, Properties, UseStateHandle, function_component, html, use_context, use_effect_with, use_state};
+use yew::{Callback, Html, HtmlResult, MouseEvent, Properties, UseStateHandle, function_component, html, use_context, use_effect_with, use_state};
 use yew_router::{hooks::use_navigator, prelude::Link};
 
 use crate::{RcTucanType, Route};
@@ -31,6 +31,7 @@ pub fn registration<TucanType: Tucan + 'static>(AnmeldungRequestProps { registra
         let data = data.clone();
         let loading = loading.clone();
         let current_session = current_session.clone();
+        let tucan = tucan.clone();
         use_effect_with(registration.clone(), move |anmeldung_request| {
             if let Some(current_session) = (*current_session).to_owned() {
                 loading.set(true);
@@ -60,6 +61,39 @@ pub fn registration<TucanType: Tucan + 'static>(AnmeldungRequestProps { registra
             }
         });
     }
+
+    let reload = {
+        let current_session = current_session.clone();
+        let registration = registration.clone();
+        let data = data.clone();
+        let loading = loading.clone();
+        let current_session = current_session.clone();
+        let tucan = tucan.clone();
+        Callback::from(move |e: MouseEvent| {
+            if let Some(current_session) = (*current_session).to_owned() {
+                loading.set(true);
+                let anmeldung_request = registration.clone();
+                let data = data.clone();
+                let tucan = tucan.clone();
+                let loading = loading.clone();
+                spawn_local(async move {
+                    match tucan.0.anmeldung(current_session.clone(), RevalidationStrategy { max_age: 0, invalidate_dependents: Some(true) }, anmeldung_request.clone()).await {
+                        Ok(response) => {
+                            data.set(Ok(response));
+                            loading.set(false);
+                        }
+                        Err(error) => {
+                            data.set(Err(error.to_string()));
+                            loading.set(false);
+                        }
+                    }
+                })
+            } else {
+                data.set(Err("Not logged in".to_owned()));
+            }
+        })
+    };
+
     let navigator = use_navigator().unwrap();
 
     let data = match data.deref() {
@@ -68,6 +102,9 @@ pub fn registration<TucanType: Tucan + 'static>(AnmeldungRequestProps { registra
             return Ok(html! {
                 <div class="container">
                     <div class="alert alert-danger d-flex align-items-center mt-2" role="alert">
+                        // https://github.com/twbs/icons
+                        // The MIT License (MIT)
+                        // Copyright (c) 2019-2024 The Bootstrap Authors
                         <svg xmlns="http://www.w3.org/2000/svg" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" width="16" height="16" viewBox="0 0 16 16" role="img" aria-label="Error:">
                             <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
                         </svg>
@@ -86,7 +123,15 @@ pub fn registration<TucanType: Tucan + 'static>(AnmeldungRequestProps { registra
     #[expect(unused_parens)]
     Ok(html! {
         <div class="container">
-            <h2 class="text-center">{ "Registration" }</h2>
+            <h2 class="text-center">{ "Registration " }<button onclick={reload} type="button" class="btn btn-light">
+                // https://github.com/twbs/icons
+                // The MIT License (MIT)
+                // Copyright (c) 2019-2024 The Bootstrap Authors
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-clockwise" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2z"/>
+                    <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466"/>
+                </svg>
+            </button></h2>
             // TODO FIXME this is dangerous
             { Html::from_html_unchecked(data.additional_information.join("\n").into()) }
             <nav style="min-height: 5.5rem" aria-label="breadcrumb">
