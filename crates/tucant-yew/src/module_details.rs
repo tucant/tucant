80c44490cd6de043a83a98915bb2a873f5e94d2a
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2025-03-30 15:18:42 +0200

    Add button

--- crates/tucant-yew/src/module_details.rs
+++ crates/tucant-yew/src/module_details.rs
@@ -125,7 +125,15 @@ pub fn module_details<TucanType: Tucan + 'static>(ModuleDetailsProps { module_de
                             }
                             if module.registered {
                                 {" "}<span class="badge text-bg-secondary">{ "Angemeldet" }</span>
-                            }
+                            }{" "}<button onclick={reload} type="button" class="btn btn-light">
+                            // https://github.com/twbs/icons
+                            // The MIT License (MIT)
+                            // Copyright (c) 2019-2024 The Bootstrap Authors
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-clockwise" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2z"/>
+                                <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466"/>
+                            </svg>
+                        </button>
                         </h1>
 
                         <h2>{"Modulverantwortliche"}</h2>
