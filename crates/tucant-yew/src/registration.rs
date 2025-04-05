ebb32f4c0f815c6a8c1842e7e64a4c4e658167a3
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2025-04-05 19:13:15 +0200

    Formatting

--- crates/tucant-yew/src/registration.rs
+++ crates/tucant-yew/src/registration.rs
@@ -27,37 +27,59 @@ pub fn registration<TucanType: Tucan + 'static>(AnmeldungRequestProps { registra
                 <></>
             };
         }
-        yew::html! {
+        ::yew::html! {
             <div class="container">
-                <h2 class="text-center">{ "Registration " }<button onclick={reload} type="button" class="btn btn-light">
-                    // https://github.com/twbs/icons
-                    // The MIT License (MIT)
-                    // Copyright (c) 2019-2024 The Bootstrap Authors
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-clockwise" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2z"/>
-                        <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466"/>
-                    </svg>
-                </button></h2>
-                <nav style="min-height: 5.5rem" aria-label="breadcrumb">
-                    <ol class="breadcrumb">
-                        { data.path.iter().map(|entry| {
-                            yew::html!{<li class="breadcrumb-item"><Link<Route> to={Route::Registration { registration: entry.1.clone()}}>{entry.0.clone()}</Link<Route>></li>}
-                            }).collect::<Html>() }
-                    </ol>
-                </nav>
-                // TODO FIXME this is dangerous
-                { Html::from_html_unchecked(data.additional_information.join("\n").into()) }
-                <h2 class="text-center">{ "Submenus" }</h2>
-                <ul class="list-group">
-                    { data.submenus.iter().map(|entry| {
-                            yew::html!{<Link<Route> to={Route::Registration { registration: entry.1.clone()}} classes="list-group-item list-group-item-action">{ format!("{}", entry.0) }</Link<Route>>}
-                        }).collect::<Html>() }
-                </ul>
-                <h2 class="text-center">{ "Modules and courses" }</h2>
-                <ul class="list-group">
-                    { data.entries.iter().map(|entry| {
-                            let module = entry.module.as_ref();
-                            yew::html!{
+                <h2 class="text-center">
+                    { "Registration " }
+                    <button onclick={reload} type="button" class="btn btn-light">
+                        // https://github.com/twbs/icons
+                        // The MIT License (MIT)
+                        // Copyright (c) 2019-2024 The Bootstrap Authors
+
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-clockwise" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2z" />
+                                <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466" />
+                                </svg>
+                            </button>
+                        </h2>
+                        <nav style="min-height: 5.5rem" aria-label="breadcrumb">
+                            <ol class="breadcrumb">
+                                {
+                data.path
+                    .iter()
+                    .map(|entry| {
+                        yew::html!{<li class="breadcrumb-item"><Link<Route> to={Route::Registration { registration: entry.1.clone()}}>{entry.0.clone()}</Link<Route>></li>}
+                    })
+                    .collect::<Html>()
+            }
+                            </ol>
+                        </nav>
+                        // TODO FIXME this is dangerous
+
+                        { Html::from_html_unchecked(data.additional_information.join("\n").into()) }
+                        <h2 class="text-center">
+                            { "Submenus" }
+                        </h2>
+                        <ul class="list-group">
+                            {
+                data.submenus
+                    .iter()
+                    .map(|entry| {
+                        yew::html!{<Link<Route> to={Route::Registration { registration: entry.1.clone()}} classes="list-group-item list-group-item-action">{ format!("{}", entry.0) }</Link<Route>>}
+                    })
+                    .collect::<Html>()
+            }
+                        </ul>
+                        <h2 class="text-center">
+                            { "Modules and courses" }
+                        </h2>
+                        <ul class="list-group">
+                            {
+                data.entries
+                    .iter()
+                    .map(|entry| {
+                        let module = entry.module.as_ref();
+                        yew::html!{
                                 <li class="list-group-item">
                                     <div class="d-flex w-100 justify-content-between">
                                         <h5 class="mb-1"><Link<Route> to={Route::ModuleDetails { module: module.unwrap().url.clone() }}>{ format!("Modul {} {}", module.map(|module| module.id.clone()).unwrap_or_default(), module.map(|module| module.name.clone()).unwrap_or_default())}</Link<Route>></h5>
@@ -122,9 +144,11 @@ pub fn registration<TucanType: Tucan + 'static>(AnmeldungRequestProps { registra
                                     </ul>
                                 </li>
                             }
-                        }).collect::<Html>() }
-                </ul>
-                </div>
+                    })
+                    .collect::<Html>()
+            }
+                        </ul>
+                    </div>
         }
     })
 }
