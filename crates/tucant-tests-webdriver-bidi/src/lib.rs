ccef94ed3fc0852f52e2511343d69ead4035a57f
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2025-04-08 23:22:26 +0200

    wip

--- crates/tucant-tests-webdriver-bidi/src/lib.rs
+++ crates/tucant-tests-webdriver-bidi/src/lib.rs
@@ -1,5 +1,6 @@
 #[cfg(test)]
 mod tests {
+    use tokio::sync::OnceCell;
     use webdriverbidi::{
         remote::{
             EmptyParams,
@@ -8,6 +9,12 @@ mod tests {
         session::WebDriverBiDiSession,
         webdriver::capabilities::CapabilitiesRequest,
     };
+
+    static SESSION: OnceCell<WebDriverBiDiSession> = OnceCell::const_new();
+
+    async fn get_session() -> WebDriverBiDiSession {
+        SESSION.get_or_init(async || setup_session().await.unwrap()).await
+    }
 
     async fn setup_session() -> anyhow::Result<WebDriverBiDiSession> {
         let capabilities = CapabilitiesRequest::default();
@@ -24,7 +31,26 @@ mod tests {
 
     #[tokio::test]
     async fn it_works() -> anyhow::Result<()> {
-        let mut session = setup_session().await?;
+        let mut session = get_session().await;
+        let user_context = session.browser_create_user_context(EmptyParams::new()).await?;
+        let browsing_context = session
+            .browsing_context_create(CreateParameters {
+                create_type: CreateType::Window,
+                user_context: Some(user_context.user_context),
+                reference_context: None,
+                background: None,
+            })
+            .await?;
+        navigate(&mut session, browsing_context.context.clone(), "https://google.de".to_owned()).await?;
+        session.browsing_context_close(CloseParameters { context: browsing_context.context, prompt_unload: None }).await?;
+        session.close().await?;
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn it_works2() -> anyhow::Result<()> {
+        let mut session = get_session().await;
         let user_context = session.browser_create_user_context(EmptyParams::new()).await?;
         let browsing_context = session
             .browsing_context_create(CreateParameters {
