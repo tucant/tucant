c80351d020e9fe2b199f4e385ddafe10b353d9fd
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2025-04-08 23:12:21 +0200

    now we can run this multiple times

--- crates/tucant-tests-webdriver-bidi/src/lib.rs
+++ crates/tucant-tests-webdriver-bidi/src/lib.rs
@@ -3,13 +3,13 @@ mod tests {
     use webdriverbidi::{
         remote::{
             EmptyParams,
-            browsing_context::{CreateParameters, CreateType},
+            browsing_context::{CloseParameters, CreateParameters, CreateType},
         },
         session::WebDriverBiDiSession,
         webdriver::capabilities::CapabilitiesRequest,
     };
 
-    async fn setup_session() -> Result<WebDriverBiDiSession, Box<dyn std::error::Error>> {
+    async fn setup_session() -> anyhow::Result<WebDriverBiDiSession> {
         let capabilities = CapabilitiesRequest::default();
         let mut session = WebDriverBiDiSession::new("localhost".to_owned(), 4444, capabilities);
         session.start().await?;
@@ -17,7 +17,7 @@ mod tests {
     }
 
     #[tokio::test]
-    async fn it_works() -> Result<(), Box<dyn std::error::Error>> {
+    async fn it_works() -> anyhow::Result<()> {
         let mut session = setup_session().await?;
         let user_context = session.browser_create_user_context(EmptyParams::new()).await?;
         let browsing_context = session
@@ -28,6 +28,8 @@ mod tests {
                 background: None,
             })
             .await?;
+        session.browsing_context_close(CloseParameters { context: browsing_context.context, prompt_unload: None }).await?;
+        session.close().await?;
 
         Ok(())
     }
