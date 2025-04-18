2722629e823735cef965c2f84c1cce9e78f81cf9
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2025-04-18 15:28:06 +0200

    Fix login state handling

--- crates/tucant-yew/src/common.rs
+++ crates/tucant-yew/src/common.rs
@@ -41,6 +41,10 @@ pub fn use_data_loader<TucanType: Tucan + 'static, I: Clone + PartialEq + 'stati
                             }
                         }
                         Err(error) => {
+                            log::error!("{}", error);
+                            if let TucanError::Timeout | TucanError::AccessDenied = error {
+                                current_session_handle.set(None);
+                            }
                             data.set(Err(error.to_string()));
                             loading.set(false);
                         }
@@ -53,19 +57,19 @@ pub fn use_data_loader<TucanType: Tucan + 'static, I: Clone + PartialEq + 'stati
     }
 
     let reload = {
-        let current_session = current_session_handle.clone();
+        let current_session_handle = current_session_handle.clone();
         let course_details = request.clone();
         let data = data.clone();
         let loading = loading.clone();
-        let current_session = current_session.clone();
         let tucan = tucan.clone();
         Callback::from(move |_e: MouseEvent| {
-            if let Some(current_session) = (*current_session).to_owned() {
+            if let Some(current_session) = (*current_session_handle).to_owned() {
                 loading.set(true);
                 let course_details = course_details.clone();
                 let data = data.clone();
                 let tucan = tucan.clone();
                 let loading = loading.clone();
+                let current_session_handle = current_session_handle.clone();
                 spawn_local(async move {
                     match handler(tucan.clone(), current_session.clone(), RevalidationStrategy { max_age: 0, invalidate_dependents: Some(true) }, course_details.clone()).await {
                         Ok(response) => {
@@ -73,6 +77,10 @@ pub fn use_data_loader<TucanType: Tucan + 'static, I: Clone + PartialEq + 'stati
                             loading.set(false);
                         }
                         Err(error) => {
+                            log::error!("{}", error);
+                            if let TucanError::Timeout | TucanError::AccessDenied = error {
+                                current_session_handle.set(None);
+                            }
                             data.set(Err(error.to_string()));
                             loading.set(false);
                         }
@@ -98,6 +106,7 @@ pub fn use_data_loader<TucanType: Tucan + 'static, I: Clone + PartialEq + 'stati
                             <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
                         </svg>
                         <div>
+                            { "Data loading error: " }
                             { error }
                         </div>
                     </div>
