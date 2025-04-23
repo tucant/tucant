ca53d7a9bd94573108817eec72e521a33cdbb0fd
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2025-04-23 16:30:47 +0200

    Fixes

--- tucant-extension/eslint.config.mjs
+++ tucant-extension/eslint.config.mjs
@@ -18,6 +18,7 @@ export default tseslint.config({
                 },
                 globals: {
                     ...globals.browser,
+                    ...globals.webextensions,
                 },
             },
         },
