fc670afbda8dab3736aaaa63197bafd6e0d3262a
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2025-04-23 16:29:31 +0200

    wip

--- tucant-extension/eslint.config.mjs
+++ tucant-extension/eslint.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 
+import globals from "globals";
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
@@ -7,12 +8,16 @@ export default tseslint.config({
     ignores: ["bootstrap.bundle.min.js"],
     extends: [
         eslint.configs.recommended,
-        tseslint.configs.recommendedTypeChecked,
+        tseslint.configs.strictTypeChecked,
+        tseslint.configs.stylisticTypeChecked,
         {
             languageOptions: {
                 parserOptions: {
                     projectService: true,
                     tsconfigRootDir: import.meta.dirname,
+                },
+                globals: {
+                    ...globals.browser,
                 },
             },
         },
