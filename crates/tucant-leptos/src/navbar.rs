e579504932b1c40e07ee8b46747c07ed509f6f9d
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2025-05-16 22:45:42 +0200

    does not start?

--- crates/tucant-leptos/src/navbar.rs
+++ crates/tucant-leptos/src/navbar.rs
@@ -3,10 +3,11 @@ use log::error;
 use std::ops::Deref;
 use tucant_types::{LoginResponse, RevalidationStrategy, Tucan, TucanError};
 
-use crate::{login_component::LoginComponent, navbar_logged_out::NavbarLoggedOut};
+use crate::{login_component::LoginComponent, navbar_logged_in::NavbarLoggedIn, navbar_logged_out::NavbarLoggedOut};
 
 #[component]
 pub fn Navbar(set_session: WriteSignal<Option<LoginResponse>>) -> impl IntoView {
+    let session = use_context::<ReadSignal<Option<LoginResponse>>>().unwrap();
     view! {
         <>
             <nav class="navbar navbar-expand-xl bg-body-tertiary">
@@ -27,11 +28,15 @@ pub fn Navbar(set_session: WriteSignal<Option<LoginResponse>>) -> impl IntoView 
                     </button>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav me-auto mb-2 mb-xl-0">
-
-                            <NavbarLoggedOut />
-                        // }
+                        {
+                            move || if let Some(session) = session.get() {
+                                view! { <NavbarLoggedIn current_session=session data=None /> }.into_any()
+                            } else {
+                                view! { <NavbarLoggedOut /> }.into_any()
+                            }
+                        }
                         </ul>
-                        <LoginComponent />
+                        <LoginComponent set_session=set_session />
 
                     </div>
                 </div>
