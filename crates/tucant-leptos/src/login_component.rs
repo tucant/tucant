c9cbb2f0359bd57793818a7e77942127ea973c5d
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2025-05-16 15:08:22 +0200

    wip

--- crates/tucant-leptos/src/login_component.rs
+++ crates/tucant-leptos/src/login_component.rs
@@ -1,83 +1,48 @@
 use std::marker::PhantomData;
 
-use leptos::prelude::*;
-use tucant_types::Tucan;
+use leptos::{html::Input, prelude::*};
+use tucant_types::{LoginRequest, Tucan};
 
 use crate::rc_tucan_type::RcTucanType;
 
 #[component]
 pub fn LoginComponent() -> impl IntoView {
-    /*let tucan: RcTucanType<dyn Tucan> = use_context().expect("no ctx found");
+    let username_ref = NodeRef::<Input>::new();
+    let password_ref = NodeRef::<Input>::new();
 
-    let username_value_handle = use_state(String::default);
+    let add_todo_action = Action::new(|(username, password): &(String, String)| async move {
+        let response = tucan.0.login(LoginRequest { username: username.to_owned(), password: password.to_owned() }).await.unwrap();
 
-    let on_username_change = {
-        let username_value_handle = username_value_handle.clone();
-
-        Callback::from(move |e: Event| {
-            username_value_handle.set(e.target_dyn_into::<HtmlInputElement>().unwrap().value());
-        })
-    };
-
-    let password_value_handle = use_state(String::default);
-
-    let on_password_change = {
-        let password_value_handle = password_value_handle.clone();
-
-        Callback::from(move |e: Event| {
-            password_value_handle.set(e.target_dyn_into::<HtmlInputElement>().unwrap().value());
-        })
-    };
-
-    let current_session = use_context::<UseStateHandle<Option<LoginResponse>>>().expect("no ctx found");
-
-    let on_submit = {
-        let username_value_handle = username_value_handle.clone();
-        let password_value_handle = password_value_handle.clone();
-        let current_session = current_session.clone();
-
-        Callback::from(move |e: SubmitEvent| {
-            e.prevent_default();
-            let username = (*username_value_handle).clone();
-            let password = (*password_value_handle).clone();
-            let current_session = current_session.clone();
-            password_value_handle.set("".to_owned());
-
-            let tucan = tucan.clone();
-
-            spawn_local(async move {
-                let response = tucan.0.login(LoginRequest { username, password }).await.unwrap();
-
-                #[cfg(feature = "direct")]
-                web_extensions_sys::chrome()
-                    .cookies()
-                    .set(web_extensions_sys::SetCookieDetails {
-                        name: Some("id".to_owned()),
-                        partition_key: None,
-                        store_id: None,
-                        url: "https://www.tucan.tu-darmstadt.de/scripts/".to_owned(),
-                        domain: None,
-                        path: None,
-                        value: Some(response.id.to_string()),
-                        expiration_date: None,
-                        http_only: None,
-                        secure: Some(true),
-                        same_site: None,
-                    })
-                    .await;
-
-                current_session.set(Some(response.clone()));
+        #[cfg(feature = "direct")]
+        web_extensions_sys::chrome()
+            .cookies()
+            .set(web_extensions_sys::SetCookieDetails {
+                name: Some("id".to_owned()),
+                partition_key: None,
+                store_id: None,
+                url: "https://www.tucan.tu-darmstadt.de/scripts/".to_owned(),
+                domain: None,
+                path: None,
+                value: Some(response.id.to_string()),
+                expiration_date: None,
+                http_only: None,
+                secure: Some(true),
+                same_site: None,
             })
-        })
-    };*/
+            .await;
 
-    // onsubmit={on_submit}
-    // onchange={on_username_change} value={(*username_value_handle).clone()}
-    // onchange={on_password_change} value={(*password_value_handle).clone()}
+        current_session.set(Some(response.clone()));
+    });
+
     view! {
-        <form class="d-flex">
-            <input id="login-username" required=true class="form-control me-2" type="username" placeholder="TU-ID" aria-label="TU-ID" autocomplete="current-username" />
-            <input id="login-password" required=true class="form-control me-2" type="password" placeholder="Password" aria-label="Password" autocomplete="current-password" />
+        <form on:submit=move |ev| {
+            ev.prevent_default();
+            let username = username_ref.get().unwrap();
+            let password = password_ref.get().unwrap();
+            add_todo_action.dispatch(username.value());
+        } class="d-flex">
+            <input node_ref=username_ref id="login-username" required=true class="form-control me-2" type="username" placeholder="TU-ID" aria-label="TU-ID" autocomplete="current-username" />
+            <input node_ref=password_ref id="login-password" required=true class="form-control me-2" type="password" placeholder="Password" aria-label="Password" autocomplete="current-password" />
             <button class="btn btn-outline-success" type="submit" id="login-button">{ "Login" }</button>
         </form>
     }
