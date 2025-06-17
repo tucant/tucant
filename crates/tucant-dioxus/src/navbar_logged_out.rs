0353ac9861107bb7b6d405ef7a466c0bbdfb777d
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2025-06-17 22:56:04 +0200

    wip

--- crates/tucant-dioxus/src/navbar_logged_out.rs
+++ crates/tucant-dioxus/src/navbar_logged_out.rs
@@ -15,13 +15,15 @@ pub fn NavbarLoggedOut() -> Element {
                 a { class: "nav-link dropdown-toggle", href:"#", role:"button", "data-bs-toggle": "dropdown", "aria-expanded": "false",
                     "VV" 
                 }
-                ul class:"dropdown-menu" {
+                ul { class:"dropdown-menu"
                     li {
-                        <Link<Route> to={Route::Vorlesungsverzeichnis {
-                            vv: ActionRequest::parse("-AN07PBvMn59bWIkwI5kPIrV6ttS-nQO52gY48WnmIDWTv9PQsRceJIEekBMsiG7XrGxJxL6WmWMRCgv6ZdqcqJvgDTJ41d1yHBN12FkxT2-2R1XLasNa7As0AF4mdh2AohuT~wrzHUbQsFAkJJF23tlDnGaVBwg3B7S2UW-GrR0DSb24IOCR8EhR1~A__")
-                        }} classes={classes!("dropdown-item", "bg-success-subtle")}>
-                            { "Vorlesungsverzeichnis" }
-                        </Link<Route>>
+                        Link {
+                            to: Route::Vorlesungsverzeichnis {
+                                vv: ActionRequest::parse("-AN07PBvMn59bWIkwI5kPIrV6ttS-nQO52gY48WnmIDWTv9PQsRceJIEekBMsiG7XrGxJxL6WmWMRCgv6ZdqcqJvgDTJ41d1yHBN12FkxT2-2R1XLasNa7As0AF4mdh2AohuT~wrzHUbQsFAkJJF23tlDnGaVBwg3B7S2UW-GrR0DSb24IOCR8EhR1~A__")
+                            },
+                            class: "dropdown-item bg-success-subtle",
+                            "Vorlesungsverzeichnis"
+                        }
                     }
                     li {
                         hr { class: "dropdown-divider" }
@@ -37,25 +39,29 @@ pub fn NavbarLoggedOut() -> Element {
                         }
                     }
                     li {
-                        <Link<Route> to={Route::Vorlesungsverzeichnis {
-                            vv: ActionRequest::parse("-AEv3p-ixRiFiH8xo86RrKK9uf72StPaSd2hoHwg~OCNTRSPUhnJzdfm2ekgyicwVEYv0sluY-Xc02k-Ok9YzmHJMN~8ZC5XqBZRJWAgaUOXj0O99z7w1fzIT-9l2n08Ivm2cZlRYRW-OLNzyzfekzeBlJcHKHIyvLHyZM1Gw7U0RN428Qtt79KAuiKQ__")
-                        }} classes={classes!("dropdown-item", "bg-success-subtle")}>
-                            { "Aktuell - Sommersemester 2025" }
-                        </Link<Route>>
+                        Link {
+                            to: Route::Vorlesungsverzeichnis {
+                                vv: ActionRequest::parse("-AEv3p-ixRiFiH8xo86RrKK9uf72StPaSd2hoHwg~OCNTRSPUhnJzdfm2ekgyicwVEYv0sluY-Xc02k-Ok9YzmHJMN~8ZC5XqBZRJWAgaUOXj0O99z7w1fzIT-9l2n08Ivm2cZlRYRW-OLNzyzfekzeBlJcHKHIyvLHyZM1Gw7U0RN428Qtt79KAuiKQ__")
+                            },
+                            class: "dropdown-item bg-success-subtle",
+                            "Aktuell - Sommersemester 2025"
+                        }
                     }
                     li {
-                        <Link<Route> to={Route::Vorlesungsverzeichnis {
-                            vv: ActionRequest::parse("-AV49BnEx08lkzBuUkpL1OiPOiy3h-IQL20ziRJFpOsEY2DC1~UykqG1eQ8bvz68jeZWo2btN4wItUnwQLaiKvRjVeU-HLKg-t83hCL4AmQ3H2Cn-as8NQ7ITOtoJXIQoEFvGuq6fgf7itKqWJGnHUuKsH5k3PgoK~dcpAqk7jG2vRTR7jTf6iDg8GoQ__")
-                        }} classes={classes!("dropdown-item", "bg-success-subtle")}>
-                            { "Vorlesungsverzeichnis des WiSe 2024/25" }
-                        </Link<Route>>
+                        Link {
+                            to: Route::Vorlesungsverzeichnis {
+                                vv: ActionRequest::parse("-AV49BnEx08lkzBuUkpL1OiPOiy3h-IQL20ziRJFpOsEY2DC1~UykqG1eQ8bvz68jeZWo2btN4wItUnwQLaiKvRjVeU-HLKg-t83hCL4AmQ3H2Cn-as8NQ7ITOtoJXIQoEFvGuq6fgf7itKqWJGnHUuKsH5k3PgoK~dcpAqk7jG2vRTR7jTf6iDg8GoQ__")
+                            }, class:"dropdown-item bg-success-subtle",
+                            "Vorlesungsverzeichnis des WiSe 2024/25"
+                        }
                     }
                     li {
-                        <Link<Route> to={Route::Vorlesungsverzeichnis {
-                            vv: ActionRequest::parse("-Avn07o4KNB3FseLBaQAymKF-c26tMa37alo49WaWmPNAs6c~eB~9eUKwhY254n8VlPObojuspLhwsEVitzdGci5tybqHGcovSsTvIbYZMNs1-6N8WKN6ieWGsv4ScDLXw~hThHy7imvWnzEqAY-6kee5lmdm8GhDxStGxy5AwNpGJW73ftfmIvUZlgQ__")
-                        }} classes={classes!("dropdown-item", "bg-success-subtle")}>
-                            { "Vorlesungsverzeichnis des SoSe2024" }
-                        </Link<Route>>
+                        Link {
+                            to: Route::Vorlesungsverzeichnis {
+                                vv: ActionRequest::parse("-Avn07o4KNB3FseLBaQAymKF-c26tMa37alo49WaWmPNAs6c~eB~9eUKwhY254n8VlPObojuspLhwsEVitzdGci5tybqHGcovSsTvIbYZMNs1-6N8WKN6ieWGsv4ScDLXw~hThHy7imvWnzEqAY-6kee5lmdm8GhDxStGxy5AwNpGJW73ftfmIvUZlgQ__")
+                            }, class: "dropdown-item bg-success-subtle",
+                            "Vorlesungsverzeichnis des SoSe2024"
+                        }
                     }
                     li {
                         a { class:"dropdown-item", href:"https://www.tucan.tu-darmstadt.de/scripts/mgrqispi.dll?APPNAME=CampusNet&PRGNAME=EXTERNALPAGES&ARGUMENTS=-N000000000000001,-N000463,-Avvarchivstart%2Ehtml",
