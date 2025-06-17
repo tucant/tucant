445ebf9f849ec06642cdf98a819fce4d743ebc93
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2025-06-17 22:52:28 +0200

    wip

--- crates/tucant-dioxus/src/navbar_logged_out.rs
+++ crates/tucant-dioxus/src/navbar_logged_out.rs
@@ -31,68 +31,68 @@ pub fn NavbarLoggedOut() -> Element {
                             "Lehrveranstaltungssuche"
                         }
                     }
-                    <li>
-                        <a class="dropdown-item" href="https://www.tucan.tu-darmstadt.de/scripts/mgrqispi.dll?APPNAME=CampusNet&PRGNAME=SEARCHROOM&ARGUMENTS=-N000000000000001,-N000385,">
-                            { "Raumsuche" }
-                        </a>
-                    </li>
-                    <li>
+                    li {
+                        a { class:"dropdown-item", href:"https://www.tucan.tu-darmstadt.de/scripts/mgrqispi.dll?APPNAME=CampusNet&PRGNAME=SEARCHROOM&ARGUMENTS=-N000000000000001,-N000385,",
+                            "Raumsuche"
+                        }
+                    }
+                    li {
                         <Link<Route> to={Route::Vorlesungsverzeichnis {
                             vv: ActionRequest::parse("-AEv3p-ixRiFiH8xo86RrKK9uf72StPaSd2hoHwg~OCNTRSPUhnJzdfm2ekgyicwVEYv0sluY-Xc02k-Ok9YzmHJMN~8ZC5XqBZRJWAgaUOXj0O99z7w1fzIT-9l2n08Ivm2cZlRYRW-OLNzyzfekzeBlJcHKHIyvLHyZM1Gw7U0RN428Qtt79KAuiKQ__")
                         }} classes={classes!("dropdown-item", "bg-success-subtle")}>
                             { "Aktuell - Sommersemester 2025" }
                         </Link<Route>>
-                    </li>
-                    <li>
+                    }
+                    li {
                         <Link<Route> to={Route::Vorlesungsverzeichnis {
                             vv: ActionRequest::parse("-AV49BnEx08lkzBuUkpL1OiPOiy3h-IQL20ziRJFpOsEY2DC1~UykqG1eQ8bvz68jeZWo2btN4wItUnwQLaiKvRjVeU-HLKg-t83hCL4AmQ3H2Cn-as8NQ7ITOtoJXIQoEFvGuq6fgf7itKqWJGnHUuKsH5k3PgoK~dcpAqk7jG2vRTR7jTf6iDg8GoQ__")
                         }} classes={classes!("dropdown-item", "bg-success-subtle")}>
                             { "Vorlesungsverzeichnis des WiSe 2024/25" }
                         </Link<Route>>
-                    </li>
-                    <li>
+                    }
+                    li {
                         <Link<Route> to={Route::Vorlesungsverzeichnis {
                             vv: ActionRequest::parse("-Avn07o4KNB3FseLBaQAymKF-c26tMa37alo49WaWmPNAs6c~eB~9eUKwhY254n8VlPObojuspLhwsEVitzdGci5tybqHGcovSsTvIbYZMNs1-6N8WKN6ieWGsv4ScDLXw~hThHy7imvWnzEqAY-6kee5lmdm8GhDxStGxy5AwNpGJW73ftfmIvUZlgQ__")
                         }} classes={classes!("dropdown-item", "bg-success-subtle")}>
                             { "Vorlesungsverzeichnis des SoSe2024" }
                         </Link<Route>>
-                    </li>
-                    <li>
-                        <a class="dropdown-item" href="https://www.tucan.tu-darmstadt.de/scripts/mgrqispi.dll?APPNAME=CampusNet&PRGNAME=EXTERNALPAGES&ARGUMENTS=-N000000000000001,-N000463,-Avvarchivstart%2Ehtml">
-                            { "Archiv" }
-                        </a>
-                    </li>
-                </ul>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    { "TUCaN-Account" }
-                </a>
-                <ul class="dropdown-menu">
-                    <li>
-                        <a class="dropdown-item" href="https://www.tucan.tu-darmstadt.de/scripts/mgrqispi.dll?APPNAME=CampusNet&PRGNAME=EXTERNALPAGES&ARGUMENTS=-N000000000000001,-N000410,-Atucan%5Faccount%2Ehtml">
-                            { "TUCaN-Account" }
-                        </a>
-                    </li>
-                    <li>
-                        <hr class="dropdown-divider" />
-                    </li>
-                    <li>
-                        <a class="dropdown-item" href="https://www.tucan.tu-darmstadt.de/scripts/mgrqispi.dll?APPNAME=CampusNet&PRGNAME=CREATEACCOUNT&ARGUMENTS=-N000000000000001,-N000425,">
-                            { "Account anlegen" }
-                        </a>
-                    </li>
-                    <li>
-                        <a class="dropdown-item" href="https://www.tucan.tu-darmstadt.de/scripts/mgrqispi.dll?APPNAME=CampusNet&PRGNAME=LOSTPASS&ARGUMENTS=-N000000000000001,-N000426,-A">
-                            { "Passwort vergessen (nur für Bewerber/innen!)" }
-                        </a>
-                    </li>
-                </ul>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="https://www.tucan.tu-darmstadt.de/scripts/mgrqispi.dll?APPNAME=CampusNet&PRGNAME=EXTERNALPAGES&ARGUMENTS=-N000000000000001,-N000340,-Ahilfe%2Ehtml">
-                    { "Hilfe" }
-                </a>
-            </li>
+                    }
+                    li {
+                        a { class:"dropdown-item", href:"https://www.tucan.tu-darmstadt.de/scripts/mgrqispi.dll?APPNAME=CampusNet&PRGNAME=EXTERNALPAGES&ARGUMENTS=-N000000000000001,-N000463,-Avvarchivstart%2Ehtml",
+                            "Archiv"
+                        }
+                    }
+                }
+            }
+            li { class:"nav-item dropdown",
+                a { class: "nav-link dropdown-toggle", href: "#", role:"button", "data-bs-toggle":"dropdown", "aria-expanded": "false",
+                    "TUCaN-Account"
+                }
+                ul { class: "dropdown-menu",
+                    li {
+                        a { class: "dropdown-item", href: "https://www.tucan.tu-darmstadt.de/scripts/mgrqispi.dll?APPNAME=CampusNet&PRGNAME=EXTERNALPAGES&ARGUMENTS=-N000000000000001,-N000410,-Atucan%5Faccount%2Ehtml",
+                            "TUCaN-Account"
+                        }
+                    }
+                    li {
+                        hr { class: "dropdown-divider" }
+                    }
+                    li {
+                        a { class: "dropdown-item", href: "https://www.tucan.tu-darmstadt.de/scripts/mgrqispi.dll?APPNAME=CampusNet&PRGNAME=CREATEACCOUNT&ARGUMENTS=-N000000000000001,-N000425,",
+                            "Account anlegen"
+                        }
+                    }
+                    li {
+                        a { class:"dropdown-item", href:"https://www.tucan.tu-darmstadt.de/scripts/mgrqispi.dll?APPNAME=CampusNet&PRGNAME=LOSTPASS&ARGUMENTS=-N000000000000001,-N000426,-A",
+                            "Passwort vergessen (nur für Bewerber/innen!)"
+                        }
+                    }
+                }
+            }
+            li { class: "nav-item",
+                a { class:"nav-link", href:"https://www.tucan.tu-darmstadt.de/scripts/mgrqispi.dll?APPNAME=CampusNet&PRGNAME=EXTERNALPAGES&ARGUMENTS=-N000000000000001,-N000340,-Ahilfe%2Ehtml",
+                    "Hilfe"
+                }
+            }
     }
 }
