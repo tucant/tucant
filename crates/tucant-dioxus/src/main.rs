51a136cd3d290016efb295652deac38563283848
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2025-06-20 23:02:50 +0200

    Fix

--- crates/tucant-dioxus/src/main.rs
+++ crates/tucant-dioxus/src/main.rs
@@ -2,7 +2,7 @@ use std::rc::Rc;
 
 use dioxus::prelude::*;
 use tucant_dioxus::{api_server::ApiServerTucan, navbar::Navbar, rc_tucan_type::RcTucanType, Route};
-use tucant_types::Tucan;
+use tucant_types::{DynTucan, Tucan};
 
 const BOOTSTRAP_CSS: Asset = asset!("/assets/bootstrap.min.css");
 const BOOTSTRAP_JS: Asset = asset!("/assets/bootstrap.bundle.min.js");
@@ -13,7 +13,7 @@ fn main() {
 
 #[component]
 fn App() -> Element {
-    use_context_provider(|| RcTucanType(Rc::new(ApiServerTucan::new())));
+    use_context_provider(|| DynTucan::new_rc(ApiServerTucan::new()));
 
     rsx! {
         document::Link { rel: "stylesheet", href: BOOTSTRAP_CSS }
