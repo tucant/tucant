e92968dad8bd3b5945f1683d68390c308da41c14
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2025-06-21 15:43:26 +0200

    wip

--- crates/tucant-dioxus/src/common.rs
+++ crates/tucant-dioxus/src/common.rs
@@ -37,7 +37,7 @@ fn use_data_loader<I: Clone + PartialEq + 'static, O: Clone + 'static>(authentic
             let data = data.clone();
             let tucan = tucan.clone();
             let current_session_handle = current_session_handle.to_owned();
-            spawn_local(async move {
+            spawn(async move {
                 match handler(tucan.clone(), current_session_handle(), RevalidationStrategy { max_age: cache_age_seconds, invalidate_dependents: Some(true) }, request.clone()).await {
                     Ok(response) => {
                         data.set(Ok(Some(response)));
@@ -67,7 +67,7 @@ fn use_data_loader<I: Clone + PartialEq + 'static, O: Clone + 'static>(authentic
                         loading.set(false);
                     }
                 }
-            })
+            });
         });
     }
 
@@ -88,7 +88,7 @@ fn use_data_loader<I: Clone + PartialEq + 'static, O: Clone + 'static>(authentic
             let tucan = tucan.clone();
             let loading = loading.clone();
             let current_session_handle = current_session_handle.clone();
-            spawn_local(async move {
+            spawn(async move {
                 match handler(tucan.clone(), current_session_handle(), RevalidationStrategy { max_age: 0, invalidate_dependents: Some(true) }, course_details.clone()).await {
                     Ok(response) => {
                         data.set(Ok(Some(response)));
@@ -111,7 +111,7 @@ fn use_data_loader<I: Clone + PartialEq + 'static, O: Clone + 'static>(authentic
                         loading.set(false);
                     }
                 }
-            })
+            });
         })
     };
 
@@ -119,39 +119,39 @@ fn use_data_loader<I: Clone + PartialEq + 'static, O: Clone + 'static>(authentic
         Ok(data) => data,
         Err(error) => {
             return rsx! {
-                <div class="container">
-                    <div class="alert alert-danger d-flex align-items-center mt-2" role="alert">
+                div { class:"container",
+                    div { class: "alert alert-danger d-flex align-items-center mt-2", role: "alert",
                         // https://github.com/twbs/icons
                         // The MIT License (MIT)
                         // Copyright (c) 2019-2024 The Bootstrap Authors
 
-                        <svg xmlns="http://www.w3.org/2000/svg" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" width="16" height="16" viewBox="0 0 16 16" role="img" aria-label="Error:">
-                            <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
-                        </svg>
-                        <div>
+                        svg { xmlns: "http://www.w3.org/2000/svg", class: "bi bi-exclamation-triangle-fill flex-shrink-0 me-2", width: "16", height: "16", view_box: "0 0 16 16", role: "img", "aria-label": "Error:",
+                            path { d: "M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" }
+                        }
+                        div {
                             { "Data loading error: " }
                             { error }
-                        </div>
-                    </div>
-                </div>
+                        }
+                    }
+                }
             };
         }
     };
 
     rsx! {
-        <div class="container">
-            if *loading {
-                <div style="z-index: 10000" class="position-fixed top-50 start-50 translate-middle">
-                    <div class="spinner-grow" role="status">
-                        <span class="visually-hidden">
-                            { "Loading..." }
-                        </span>
-                    </div>
-                </div>
+        div { class: "container",
+            if loading() {
+                div { style: "z-index: 10000", class: "position-fixed top-50 start-50 translate-middle",
+                    div { class: "spinner-grow", role: "status",
+                        span { class: "visually-hidden",
+                            "Loading..."
+                        }
+                    }
+                }
             }
             if let Some(course) = data {
                 { render(course.to_owned(), reload) }
             }
-        </div>
+        }
     }
 }
