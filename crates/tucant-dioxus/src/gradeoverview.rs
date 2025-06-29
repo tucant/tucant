cfcb6a772c0e17673fa866353ffbbb14b060990d
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2025-06-29 12:42:04 +0200

    Fixes

--- crates/tucant-dioxus/src/gradeoverview.rs
+++ crates/tucant-dioxus/src/gradeoverview.rs
@@ -35,17 +35,23 @@ pub fn GradeOverview(gradeoverview: ReadOnlySignal<GradeOverviewRequest>) -> Ele
                             .x_label_area_size(35)
                             .y_label_area_size(40)
                             .margin(5)
-                            .caption("Histogram Test", ("sans-serif", 50.0))
-                            .build_cartesian_2d((0usize..15usize).into_segmented(), 0u32..100u32)?;
+                            .caption("Grade distribution", ("sans-serif", 50.0))
+                            .build_cartesian_2d((0usize..grades.columns.len()-1).into_segmented(), 0u32..100u32)?;
 
                         chart
                             .configure_mesh()
                             .disable_x_mesh()
                             .bold_line_style(WHITE.mix(0.3))
                             .y_desc("Count")
-                            .x_desc("Bucket")
+                            .x_desc("Grade")
+                            .x_label_formatter(&|i| grades.columns[match i {
+                                SegmentValue::Exact(i) => unreachable!(),
+                                SegmentValue::CenterOf(i) => *i,
+                                SegmentValue::Last => unreachable!(),
+                            }].0.clone())
                             .axis_desc_style(("sans-serif", 15))
                             .draw()?;
+
 
                         chart.draw_series(
                             Histogram::vertical(&chart)
