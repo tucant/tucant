caf8bc0b05e97709c55caac04659bf2cf58bdd2c
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2025-07-05 17:21:27 +0200

    wip

---
+++ crates/tucant-planning/src/bin/leistungsspiegel/main.rs
@@ -0,0 +1,40 @@
+use std::future::Future;
+use std::panic::AssertUnwindSafe;
+use std::sync::Arc;
+use std::sync::atomic::{AtomicU64, Ordering};
+
+use futures_util::stream::FuturesUnordered;
+use futures_util::{FutureExt, StreamExt};
+use tucan_connector::TucanConnector;
+use tucant_types::coursedetails::CourseDetailsRequest;
+use tucant_types::registration::{AnmeldungRequest, RegistrationState};
+use tucant_types::{LoginRequest, RevalidationStrategy, Tucan};
+use tucant_types::{LoginResponse, TucanError};
+
+fn main() -> Result<(), TucanError> {
+    dotenvy::dotenv().unwrap();
+    tokio::runtime::Builder::new_current_thread().enable_all().build().unwrap().block_on(async_main())
+}
+
+async fn async_main() -> Result<(), TucanError> {
+    let tucan = TucanConnector::new().await?;
+
+    /*let login_response = LoginResponse {
+        id: std::env::var("SESSION_ID").unwrap().parse().unwrap(),
+        cookie_cnsc: std::env::var("SESSION_KEY").unwrap(),
+    };*/
+
+    let login_response = tucan
+        .login(LoginRequest {
+            username: std::env::var("TUCAN_USERNAME").expect("env variable TUCAN_USERNAME missing"),
+            password: std::env::var("TUCAN_PASSWORD").expect("env variable TUCAN_PASSWORD missing"),
+        })
+        .await
+        .unwrap();
+
+    let course_of_studies = tucan.student_result(&login_response, RevalidationStrategy::cache(), 0).await.unwrap();
+    let bachelor = course_of_studies.course_of_study.iter().find(|v| v.name == "B.Sc. Informatik (2015)").unwrap().value.parse().unwrap();
+    let student_result = tucan.student_result(&login_response, RevalidationStrategy::cache(), bachelor).await.unwrap();
+    println!("{:?}", student_result.level0);
+    Ok(())
+}
