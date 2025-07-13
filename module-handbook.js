9a48d3c39feeec112bc8daa92613923e4214b543
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2025-07-13 23:05:44 +0200

    wip

--- module-handbook.js
+++ module-handbook.js
@@ -1,5 +1,12 @@
 import { getDocument, OPS } from "pdfjs-dist/legacy/build/pdf.mjs";
 
+const DrawOPS = {
+    moveTo: 0,
+    lineTo: 1,
+    curveTo: 2,
+    closePath: 3,
+};
+
 const OPS_INVERTED = Object.fromEntries(
     Object.entries(OPS).map(([key, value]) => [value, key])
 );
@@ -9,7 +16,7 @@ const OPS_INVERTED = Object.fromEntries(
 const document = await getDocument("/home/moritz/Downloads/2023_05_11_MHB_MSC_INF.pdf").promise
 //console.log(document)
 
-// https://github.com/mozilla/pdf.js/blob/master/examples/text-only/pdf2svg.mjs
+// https://github.com/mozilla/pdf.js/blob/master/src/display/api.js
 
 for (let i = 1; i <= document.numPages; i++) {
     const page = await document.getPage(i)
@@ -21,6 +28,36 @@ for (let i = 1; i <= document.numPages; i++) {
         const args = opList.argsArray[i];
 
         const opName = OPS_INVERTED[fnId];
-        console.log(`Operation: ${opName}`, args);
+        // console.log(`Operation: ${opName}`, args);
+        // https://github.com/mozilla/pdf.js/blob/e0783cd07557134798e1fc882b043376bc8b8b6e/src/display/canvas.js#L1421
+        if (opName === "constructPath") {
+            let [op, data, minMax] = args;
+            let [path] = data;
+            for (let i = 0, ii = path.length; i < ii;) {
+                switch (path[i++]) {
+                    case DrawOPS.moveTo:
+                        console.log(`moveTo ${path[i++]}, ${path[i++]}`);
+                        break;
+                    case DrawOPS.lineTo:
+                        console.log(`lineTo ${path[i++]}, ${path[i++]}`);
+                        break;
+                    case DrawOPS.curveTo:
+                        onsole.log(`bezierCurveTo ${path[i++]},
+                            ${path[i++]},
+                            ${path[i++]},
+                            ${path[i++]},
+                            ${path[i++]},
+                            ${path[i++]}
+                        `);
+                        break;
+                    case DrawOPS.closePath:
+                        console.log(`closePath`);
+                        break;
+                    default:
+                        warn(`Unrecognized drawing path operator: ${path[i - 1]}`);
+                        break;
+                }
+            }
+        }
     }
 }
