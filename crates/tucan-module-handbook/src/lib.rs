f11a5130b9e3dde4415a4db7891c0fa5b63e26ff
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2025-08-03 17:49:31 +0200

    wip

--- crates/tucan-module-handbook/src/lib.rs
+++
@@ -1,6 +0,0 @@
-use pdfium_render::prelude::Pdfium;
-
-// maybe we can steal from https://github.com/Spxg/sqlite-wasm-rs
-pub fn extract_modules() {
-    let pdfium = Pdfium::default();
-}
--- crates/tucant-dioxus/src/main.rs
+++ crates/tucant-dioxus/src/main.rs
@@ -2,7 +2,6 @@ use std::{collections::HashMap, panic};
 
 use dioxus::prelude::*;
 use log::warn;
-use tucan_module_handbook::extract_modules;
 use tucant_dioxus::{RcTucanType, Route};
 use tucant_types::LoginResponse;
 use wasm_bindgen::prelude::*;
@@ -47,8 +46,6 @@ pub async fn main() {
     }));
     #[cfg(feature = "web")]
     console_log::init().unwrap();
-
-    extract_modules();
 
     let launcher = dioxus::LaunchBuilder::new();
 
