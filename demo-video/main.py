0202acaf98cdfdd5736d21defcb0732bc4f9a871
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2025-08-05 22:13:32 +0200

    works

--- demo-video/main.py
+++ demo-video/main.py
@@ -1,12 +1,28 @@
 #!/usr/bin/python3
 from dogtail.tree import root
 
+# set firefox as always on top window?
+
 firefox = root.application("Firefox")
 
 navigation = firefox.child("Navigation", "tool bar")
 
-firefox_button = navigation.child("Firefox", "button")
-firefox_button.click()
+#firefox_button = navigation.child("Firefox", "button")
+#firefox_button.click()
 
-extensions_button = firefox.child("Extensions and themes", "button")
-extensions_button.click()
+# https://tucant.github.io/tucant/
+
+#extensions_button = firefox.child("Extensions and themes", "button")
+#extensions_button.click()
+
+
+
+urlbar_input = navigation.child(identifier="urlbar-input")
+#urlbar_input.doActionNamed("activate")
+urlbar_input.click()
+urlbar_input.typeText("https://tucant.github.io/tucant/")
+urlbar_input.click()
+
+print(urlbar_input.dump())
+
+# .text = "https://tucant.github.io/tucant/"
