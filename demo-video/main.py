7cc0fba7a701de23566f88f7254d66b9c9fe0c1e
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2025-08-12 23:30:59 +0200

    wip

--- demo-video/main.py
+++ demo-video/main.py
@@ -29,6 +29,22 @@ config.searchShowingOnly = True
 
 record_state = "OBS_WEBSOCKET_OUTPUT_STOPPED"
 
+# https://github.com/canopyai/Orpheus-TTS
+# https://github.com/hexgrad/kokoro
+# https://github.com/idiap/coqui-ai-TTS
+# https://github.com/nari-labs/dia
+# https://github.com/2noise/ChatTTS
+# https://github.com/myshell-ai/MeloTTS
+
+# https://www.bentoml.com/blog/exploring-the-world-of-open-source-text-to-speech-models
+
+# pip install ChatTTS
+# pip install coqui-tts
+
+# dia
+
+# kokoro looks good
+
 model = ChatterboxTTS.from_pretrained(device="cpu")
 
 def on_record_state_changed(data):
