2abe9940141a4201a2d5f172c62a2c7ce088685c
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2025-08-16 23:17:51 +0200

    wip

--- crates/tucant-planning/src/examples/fetcher/main.rs
+++ crates/tucant-planning/src/examples/fetcher/main.rs
@@ -15,6 +15,7 @@ use serde::{Deserialize, Serialize};
 use tokio::fs::File;
 use tokio::io::{AsyncSeekExt, AsyncWriteExt as _};
 use tucan_connector::TucanConnector;
+use tucant_planning::{compress, recursive_anmeldung};
 use tucant_types::coursedetails::CourseDetailsRequest;
 use tucant_types::moduledetails::ModuleDetailsRequest;
 use tucant_types::registration::{
@@ -31,23 +32,6 @@ fn main() -> Result<(), TucanError> {
         .build()
         .unwrap()
         .block_on(async_main())
-}
-
-async fn compress(in_data: &[u8]) -> std::io::Result<Vec<u8>> {
-    let mut encoder = async_compression::tokio::write::BrotliEncoder::with_quality(
-        Vec::new(),
-        async_compression::Level::Best,
-    );
-    encoder.write_all(in_data).await?;
-    encoder.shutdown().await?;
-    Ok(encoder.into_inner())
-}
-
-async fn decompress(in_data: &[u8]) -> std::io::Result<Vec<u8>> {
-    let mut decoder = async_compression::tokio::write::BrotliDecoder::new(Vec::new());
-    decoder.write_all(in_data).await?;
-    decoder.shutdown().await?;
-    Ok(decoder.into_inner())
 }
 
 async fn async_main() -> Result<(), TucanError> {
@@ -90,35 +74,4 @@ async fn async_main() -> Result<(), TucanError> {
     }
 
     Ok(())
-}
-
-#[expect(clippy::manual_async_fn)]
-fn recursive_anmeldung<'a, 'b>(
-    tucan: &'a TucanConnector,
-    login_response: &'b LoginResponse,
-    anmeldung_request: AnmeldungRequest,
-) -> impl Future<Output = Vec<AnmeldungResponse>> + Send + use<'a, 'b> {
-    async move {
-        let anmeldung_response = tucan
-            .anmeldung(
-                login_response.clone(),
-                RevalidationStrategy::cache(),
-                anmeldung_request.clone(),
-            )
-            .await
-            .unwrap();
-
-        let results: FuturesUnordered<_> = anmeldung_response
-            .submenus
-            .iter()
-            .map(|entry| {
-                async { recursive_anmeldung(tucan, login_response, entry.1.clone()).await }.boxed()
-            })
-            .collect();
-        let results = results.collect::<Vec<Vec<AnmeldungResponse>>>().await;
-
-        std::iter::once(anmeldung_response)
-            .chain(results.into_iter().flatten())
-            .collect()
-    }
 }
