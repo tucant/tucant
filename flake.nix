852770086aa8a628128df6072812dc62dee1da33
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2025-08-29 23:11:59 +0200

    bump

--- flake.nix
+++ flake.nix
@@ -39,7 +39,7 @@
             owner = "mohe2015";
             repo = "dioxus";
             rev = "c5ca0c8576825ea2b6478ac3087d2f0d778f9ff6";
-            hash = "";
+            hash = "sha256-Uy5Ug3bu8Reae6i8UpT2/tV0yjAfubO44oFs0EYN8Rc=";
           };
           doCheck = false;
           strictDeps = true;
