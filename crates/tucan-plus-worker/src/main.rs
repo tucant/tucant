659c1f5245c4db0fe2fcfbb228edfd8b4fd8a751
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2025-09-20 16:13:10 +0200

    wip

--- crates/tucan-plus-worker/src/main.rs
+++ crates/tucan-plus-worker/src/main.rs
@@ -1,6 +1,16 @@
+use log::info;
 use wasm_bindgen::prelude::*;
+use web_sys::MessageEvent;
 
 #[wasm_bindgen(main)]
 fn main() {
-    println!("Hello, world!");
+    console_log::init().unwrap();
+
+    let global = js_sys::global().unchecked_into::<web_sys::DedicatedWorkerGlobalScope>();
+    let closure: Closure<dyn Fn(MessageEvent)> = Closure::new(|event: MessageEvent| {
+        info!("Got message");
+    });
+    global
+        .add_event_listener_with_callback("message", closure.as_ref().unchecked_ref())
+        .unwrap();
 }
