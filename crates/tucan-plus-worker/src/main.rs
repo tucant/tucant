46f9c20738c4ba8c2f9c2f54381bc7825cc234d9
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2025-09-20 16:22:13 +0200

    wip

--- crates/tucan-plus-worker/src/main.rs
+++ crates/tucan-plus-worker/src/main.rs
@@ -1,16 +1,37 @@
+use std::time::Duration;
+
 use log::info;
 use wasm_bindgen::prelude::*;
 use web_sys::MessageEvent;
 
+pub async fn sleep(duration: Duration) {
+    let mut cb = |resolve: js_sys::Function, _reject: js_sys::Function| {
+        let global = js_sys::global().unchecked_into::<web_sys::DedicatedWorkerGlobalScope>();
+        global
+            .set_timeout_with_callback_and_timeout_and_arguments_0(
+                &resolve,
+                duration.as_millis().try_into().unwrap(),
+            )
+            .unwrap();
+    };
+
+    let p = js_sys::Promise::new(&mut cb);
+
+    wasm_bindgen_futures::JsFuture::from(p).await.unwrap();
+}
+
 #[wasm_bindgen(main)]
-fn main() {
+async fn main() {
     console_log::init().unwrap();
 
     let global = js_sys::global().unchecked_into::<web_sys::DedicatedWorkerGlobalScope>();
-    let closure: Closure<dyn Fn(MessageEvent)> = Closure::new(|event: MessageEvent| {
+    let closure: Closure<dyn Fn(MessageEvent)> = Closure::new(move |event: MessageEvent| {
+        let global = js_sys::global().unchecked_into::<web_sys::DedicatedWorkerGlobalScope>();
+        global.post_message(&JsValue::from_str("Response")).unwrap();
         info!("Got message");
     });
     global
         .add_event_listener_with_callback("message", closure.as_ref().unchecked_ref())
         .unwrap();
+    sleep(Duration::from_secs(100000)).await;
 }
