d12c0b1609acf079ffe554a750e36a25f001d837
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2025-09-21 19:41:10 +0200

    wip

--- crates/tucan-plus-dioxus/src/main.rs
+++ crates/tucan-plus-dioxus/src/main.rs
@@ -1,11 +1,12 @@
 use std::panic;
 
 use dioxus::prelude::*;
+use js_sys::Function;
 use log::info;
 use tucan_plus_dioxus::{Anonymize, BOOTSTRAP_JS, BOOTSTRAP_PATCH_JS, Route};
 use tucan_types::LoginResponse;
 use wasm_bindgen::prelude::*;
-use web_sys::{MessageEvent, Worker, WorkerOptions, WorkerType};
+use web_sys::{AddEventListenerOptions, MessageEvent, Worker, WorkerOptions, WorkerType};
 
 #[wasm_bindgen]
 extern "C" {
@@ -26,27 +27,56 @@ extern "C" {
 // https://github.com/tauri-apps/wry
 // https://github.com/tauri-apps/tao/blob/5ac00b57ad3f5c5c7135dde626cb90bc1ad469dc/src/platform_impl/android/ndk_glue.rs#L236
 
+pub async fn wait_for_worker() {
+    let mut cb = |resolve: js_sys::Function, reject: js_sys::Function| {
+        let options = WorkerOptions::new();
+        options.set_type(WorkerType::Module);
+        let worker = Worker::new_with_options("/assets/worker-helper/worker.js", &options).unwrap();
+        let message_closure: Option<Closure<dyn Fn(MessageEvent)>> = None;
+        let error_closure: Closure<dyn Fn(_)> = Closure::new(move |event: web_sys::Event| {
+            info!("error {event:?}");
+            worker.remove_event_listener_with_callback(
+                "message",
+                message_closure.unwrap().as_ref().unchecked_ref(),
+            );
+            reject.call0(&JsValue::NULL).unwrap();
+        });
+        message_closure = Some(Closure::new(move |event: MessageEvent| {
+            info!("{:?}", event.data());
+            worker.remove_event_listener_with_callback(
+                "error",
+                error_closure.as_ref().unchecked_ref(),
+            );
+            resolve.call0(&JsValue::NULL).unwrap();
+        }));
+        let options = AddEventListenerOptions::new();
+        options.set_once(true);
+        worker
+            .add_event_listener_with_callback_and_add_event_listener_options(
+                "error",
+                error_closure.as_ref().unchecked_ref(),
+                &options,
+            )
+            .unwrap();
+        worker
+            .add_event_listener_with_callback_and_add_event_listener_options(
+                "message",
+                message_closure.unwrap().as_ref().unchecked_ref(),
+                &options,
+            )
+            .unwrap();
+        error_closure.forget();
+        message_closure.unwrap().forget();
+    };
+
+    let p = js_sys::Promise::new(&mut cb);
+
+    wasm_bindgen_futures::JsFuture::from(p).await.unwrap();
+}
+
 #[cfg_attr(target_arch = "wasm32", wasm_bindgen(main))]
 #[cfg_attr(not(target_arch = "wasm32"), tokio::main)]
 pub async fn main() {
-    let options = WorkerOptions::new();
-    options.set_type(WorkerType::Module);
-    let worker = Worker::new_with_options("/assets/worker-helper/worker.js", &options).unwrap();
-    let error_closure: Closure<dyn Fn(_)> = Closure::new(move |event: web_sys::Event| {
-        info!("error {event:?}");
-    });
-    let message_closure: Closure<dyn Fn(_)> = Closure::new(move |event: MessageEvent| {
-        info!("{:?}", event.data());
-    });
-    worker
-        .add_event_listener_with_callback("error", error_closure.as_ref().unchecked_ref())
-        .unwrap();
-    worker
-        .add_event_listener_with_callback("message", message_closure.as_ref().unchecked_ref())
-        .unwrap();
-    error_closure.forget();
-    message_closure.forget();
-
     // From https://github.com/rustwasm/console_error_panic_hook, licensed under MIT and Apache 2.0
     #[cfg(feature = "web")]
     panic::set_hook(Box::new(|info| {
