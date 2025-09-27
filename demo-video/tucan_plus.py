f185f72453a38cae13853c57afea9f335987f7fe
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2025-09-27 22:03:34 +0200

    wip

--- demo-video/tucan_plus.py
+++ demo-video/tucan_plus.py
@@ -7,3 +7,6 @@ def tucan_plus_cli():
     urlbar_input.keyCombo("<ctrl><a>")
     urlbar_input.typeText("https://tucan-plus.github.io/tucan-plus/")
     urlbar_input.keyCombo("<enter>")
+
+if __name__ == "__main__":
+    tucan_plus_cli()
